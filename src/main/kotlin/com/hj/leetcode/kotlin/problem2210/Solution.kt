8f46cd933dded85176df0c8e12834d3dc36615e9
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-08 14:22:23 +0800

    Update local variable name in problem 2210 solution;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2210/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2210/Solution.kt
@@ -12,11 +12,11 @@ class Solution {
      */
     fun countHillValley(nums: IntArray): Int {
         var count = 0
-        var prevLevelChange = LevelChange.EQUAL
+        var prevLevelTrend = LevelChange.EQUAL
         for (index in 1..nums.lastIndex) {
             val currLevelChange = getLevelChange(nums, index)
-            if (isTurningPoint(prevLevelChange, currLevelChange)) count++
-            if (currLevelChange != LevelChange.EQUAL) prevLevelChange = currLevelChange
+            if (isTurningPoint(prevLevelTrend, currLevelChange)) count++
+            if (currLevelChange != LevelChange.EQUAL) prevLevelTrend = currLevelChange
         }
         return count
     }
@@ -29,10 +29,10 @@ class Solution {
         }
     }
 
-    private fun isTurningPoint(prevLevelChange: LevelChange, currLevelChange: LevelChange): Boolean {
+    private fun isTurningPoint(prevLevelTrend: LevelChange, currLevelChange: LevelChange): Boolean {
         return when (currLevelChange) {
-            LevelChange.INCREASE -> prevLevelChange == LevelChange.DECREASE
-            LevelChange.DECREASE -> prevLevelChange == LevelChange.INCREASE
+            LevelChange.INCREASE -> prevLevelTrend == LevelChange.DECREASE
+            LevelChange.DECREASE -> prevLevelTrend == LevelChange.INCREASE
             LevelChange.EQUAL -> false
         }
     }
