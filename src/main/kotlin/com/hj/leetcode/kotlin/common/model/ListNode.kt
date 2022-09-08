bb70c35a36f03927da2699ba03248b2a99a2564a
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-08 12:45:34 +0800

    Add data structures commonly used by LeetCode;

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/common/model/ListNode.kt
@@ -0,0 +1,5 @@
+package com.hj.leetcode.kotlin.common.model
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
---
+++ src/main/kotlin/com/hj/leetcode/kotlin/common/model/Node.kt
@@ -0,0 +1,5 @@
+package com.hj.leetcode.kotlin.common.model
+
+class Node(var `val`: Int) {
+    var children: List<Node?> = listOf()
+}
