4978144c37852568c8e2a1222274463305b88222
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-11 21:37:22 +0800

    Add solution to problem 15;

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ at [LeetCode](https://leetcode.com/hj-core/).
 |-----------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------------------------|---------|---------------|
 | [6. Zigzag Conversion](https://leetcode.com/problems/zigzag-conversion/)                                                    | [Solution](src/main/kotlin/com/hj/leetcode/kotlin/problem6/Solution.kt)    | -       | 2022-09-11    |
 | [7. Reverse Integer](https://leetcode.com/problems/reverse-integer/)                                                        | [Solution](src/main/kotlin/com/hj/leetcode/kotlin/problem7/Solution.kt)    | -       | 2022-09-11    |
+| [15. 3Sum](https://leetcode.com/problems/3sum/)                                                                             | [Solution](src/main/kotlin/com/hj/leetcode/kotlin/problem15/Solution.kt)   | -       | 2022-09-11    |
 | [1996. The Number of Weak Characters in the Game](https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/) | [Solution](src/main/kotlin/com/hj/leetcode/kotlin/problem1996/Solution.kt) | -       | 2022-09-09    |
 
 ### Easy
---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem15/Solution.kt
@@ -0,0 +1,65 @@
+package com.hj.leetcode.kotlin.problem15
+
+/**
+ * LeetCode page: [15. 3Sum](https://leetcode.com/problems/3sum/);
+ */
+class Solution {
+    private var sortedNumber = intArrayOf()
+
+    /* Complexity:
+     * Time O(N^2) and Space O(N) where N is the size of nums;
+     */
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        resetState()
+        updateSortedNumber(nums)
+        return getValidTriplets()
+    }
+
+    private fun resetState() {
+        sortedNumber = intArrayOf()
+    }
+
+    private fun updateSortedNumber(numbers: IntArray) {
+        sortedNumber = numbers
+            .clone()
+            .apply { sort() }
+    }
+
+    private fun getValidTriplets(): List<List<Int>> {
+        if (sortedNumber.first() > 0 || sortedNumber.last() < 0) return emptyList()
+
+        val leftBound = getLeftBoundIndex()
+        val rightBound = getRightBoundIndex()
+
+        val validTriplets = hashSetOf<List<Int>>()
+        val availableThirdNumbers = hashSetOf<Int>().also { it.add(sortedNumber[leftBound]) }
+        for (firstIndex in leftBound + 1 until rightBound) {
+            val firstNumber = sortedNumber[firstIndex]
+            for (secondIndex in firstIndex + 1..rightBound) {
+                val secondNumber = sortedNumber[secondIndex]
+                val target = -(firstNumber + secondNumber)
+                if (target in availableThirdNumbers) validTriplets.add(listOf(firstNumber, secondNumber, target))
+            }
+            availableThirdNumbers.add(sortedNumber[firstIndex])
+        }
+
+        return validTriplets.toList()
+    }
+
+    private fun getLeftBoundIndex(): Int {
+        val maxTwoSum = sortedNumber
+            .lastIndex
+            .let { sortedNumber[it] + sortedNumber[it - 1] }
+        return sortedNumber
+            .binarySearch(-maxTwoSum, 0, sortedNumber.lastIndex - 1)
+            .let { biIndex -> if (biIndex < 0) -(biIndex + 1) else biIndex }
+    }
+
+    private fun getRightBoundIndex(): Int {
+        val minTwoSum = sortedNumber[0] + sortedNumber[1]
+        return sortedNumber
+            .binarySearch(-minTwoSum, 2, sortedNumber.size)
+            .let { biIndex -> if (biIndex < 0) -(biIndex + 1) else biIndex }
+            .coerceAtMost(sortedNumber.lastIndex)
+    }
+}
