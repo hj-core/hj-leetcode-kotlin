af1d8f78f06515aabc65a8002d857f6a22c18a76
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-13 01:52:09 +0800

    Update problem 15 solution that refactor the implementation;
    - Decide to eliminate the state variable so that all function become pure function and no concurrency concern;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem15/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem15/Solution.kt
@@ -4,62 +4,52 @@ package com.hj.leetcode.kotlin.problem15
  * LeetCode page: [15. 3Sum](https://leetcode.com/problems/3sum/);
  */
 class Solution {
-    private var sortedNumber = intArrayOf()
-
     /* Complexity:
      * Time O(N^2) and Space O(N) where N is the size of nums;
      */
     fun threeSum(nums: IntArray): List<List<Int>> {
-        updateSortedNumber(nums)
-        val validTriplets = getValidTriplets()
-        clearState()
-        return validTriplets
+        val sortedNumbers = getSortedNumbers(nums)
+        return getValidTriplets(sortedNumbers)
     }
 
-    private fun updateSortedNumber(numbers: IntArray) {
-        sortedNumber = numbers
-            .clone()
-            .apply { sort() }
+    private fun getSortedNumbers(numbers: IntArray): IntArray {
+        return numbers.clone().apply { sort() }
     }
 
-    private fun getValidTriplets(): List<List<Int>> {
-        if (sortedNumber.first() > 0 || sortedNumber.last() < 0) return emptyList()
+    private fun getValidTriplets(sortedNumbers: IntArray): List<List<Int>> {
+        if (sortedNumbers.first() > 0 || sortedNumbers.last() < 0) return emptyList()
 
-        val leftBound = getLeftBoundIndex()
-        val rightBound = getRightBoundIndex()
+        val leftBound = getLeftBoundIndex(sortedNumbers)
+        val rightBound = getRightBoundIndex(sortedNumbers)
 
         val validTriplets = hashSetOf<List<Int>>()
-        val availableThirdNumbers = hashSetOf<Int>().also { it.add(sortedNumber[leftBound]) }
+        val availableThirdNumbers = hashSetOf<Int>().also { it.add(sortedNumbers[leftBound]) }
         for (firstIndex in leftBound + 1 until rightBound) {
-            val firstNumber = sortedNumber[firstIndex]
+            val firstNumber = sortedNumbers[firstIndex]
             for (secondIndex in firstIndex + 1..rightBound) {
-                val secondNumber = sortedNumber[secondIndex]
+                val secondNumber = sortedNumbers[secondIndex]
                 val target = -(firstNumber + secondNumber)
                 if (target in availableThirdNumbers) validTriplets.add(listOf(firstNumber, secondNumber, target))
             }
-            availableThirdNumbers.add(sortedNumber[firstIndex])
+            availableThirdNumbers.add(sortedNumbers[firstIndex])
         }
         return validTriplets.toList()
     }
 
-    private fun getLeftBoundIndex(): Int {
-        val maxTwoSum = sortedNumber
+    private fun getLeftBoundIndex(sortedNumbers: IntArray): Int {
+        val maxTwoSum = sortedNumbers
             .lastIndex
-            .let { sortedNumber[it] + sortedNumber[it - 1] }
-        return sortedNumber
-            .binarySearch(-maxTwoSum, 0, sortedNumber.lastIndex - 1)
+            .let { sortedNumbers[it] + sortedNumbers[it - 1] }
+        return sortedNumbers
+            .binarySearch(-maxTwoSum, 0, sortedNumbers.lastIndex - 1)
             .let { biIndex -> if (biIndex < 0) -(biIndex + 1) else biIndex }
     }
 
-    private fun getRightBoundIndex(): Int {
-        val minTwoSum = sortedNumber[0] + sortedNumber[1]
-        return sortedNumber
-            .binarySearch(-minTwoSum, 2, sortedNumber.size)
+    private fun getRightBoundIndex(sortedNumbers: IntArray): Int {
+        val minTwoSum = sortedNumbers[0] + sortedNumbers[1]
+        return sortedNumbers
+            .binarySearch(-minTwoSum, 2, sortedNumbers.size)
             .let { biIndex -> if (biIndex < 0) -(biIndex + 1) else biIndex }
-            .coerceAtMost(sortedNumber.lastIndex)
-    }
-
-    private fun clearState() {
-        sortedNumber = intArrayOf()
+            .coerceAtMost(sortedNumbers.lastIndex)
     }
 }
