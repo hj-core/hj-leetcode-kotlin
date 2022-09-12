9c5217aa370395206fdde48819026f8b9493ca88
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-12 22:21:49 +0800

    Update a bit the problem 1383 solution;
    - As an amendment to commit 46c1d7c3;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1383/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1383/Solution.kt
@@ -6,29 +6,25 @@ import java.util.*
  * LeetCode page: [1383. Maximum Performance of a Team](https://leetcode.com/problems/maximum-performance-of-a-team/);
  */
 class Solution {
-    private var agileSortedByEfficiency = listOf<Agile>()
-
     private data class Agile(val speed: Int, val efficiency: Int)
 
     /* Complexity:
      * Time O(NLogN) and Space O(N) where N equals n;
      */
     fun maxPerformance(n: Int, speed: IntArray, efficiency: IntArray, k: Int): Int {
-        updateAgileSortedByEfficiency(speed, efficiency)
-        val maxPerformance = getMaxPerformance(k)
-        clearStates()
-        return maxPerformance
+        val sortedAgile = getAgileSortedByEfficiency(speed, efficiency)
+        return getMaxPerformance(k, sortedAgile)
     }
 
-    private fun updateAgileSortedByEfficiency(speed: IntArray, efficiency: IntArray) {
-        agileSortedByEfficiency = speed
+    private fun getAgileSortedByEfficiency(speed: IntArray, efficiency: IntArray): List<Agile> {
+        return speed
             .asSequence()
             .zip(efficiency.asSequence()) { spd, eff -> Agile(spd, eff) }
             .sortedBy { agile -> agile.efficiency }
             .toList()
     }
 
-    private fun getMaxPerformance(maxTeamSize: Int): Int {
+    private fun getMaxPerformance(maxTeamSize: Int, agileSortedByEfficiency: List<Agile>): Int {
         var maxPerformance = 0L
         val teamSpeedMinPq = PriorityQueue<Int>()
         var maxTeamSpeedSum = 0L
@@ -47,9 +43,5 @@ class Solution {
     private fun Long.toOutputFormat(): Int {
         val requiredModulo = 1_000_000_007
         return (this % requiredModulo).toInt()
-    }
-
-    private fun clearStates() {
-        agileSortedByEfficiency = listOf()
     }
 }
