fb3149246ecb862755548f67ac3d860755442302
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-13 03:18:16 +0800

    Update problem 1201 solution that refactor the implementation;
    - Decide to eliminate the state variable so that all functions become pure function and no concurrency concern;
    
    - Last Modified in README.md is updated by commit af1d8f78 although not intend;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1201/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1201/Solution.kt
@@ -4,79 +4,63 @@ package com.hj.leetcode.kotlin.problem1201
  * LeetCode page: [1201. Ugly Number III](https://leetcode.com/problems/ugly-number-iii/);
  */
 class Solution {
-    private val divisors = longArrayOf(0L, 0L, 0L)
-    private val lcmBetweenDivisors = longArrayOf(0L, 0L, 0L)
-    private var lcm = 0L
-
     /* Complexity:
      * Time O(Log(NMK)) and Space O(1) where N, M, K equal a, b and c;
      */
     fun nthUglyNumber(n: Int, a: Int, b: Int, c: Int): Int {
-        updateDivisorsAndLcms(a, b, c)
-        val nthUgly = getNthUglyNumber(n)
-        clearStates()
-        return nthUgly
+        val divisors = Divisors(a.toLong(), b.toLong(), c.toLong())
+        return getNthUglyNumber(n, divisors)
     }
 
-    private fun updateDivisorsAndLcms(num1: Int, num2: Int, num3: Int) {
-        divisors[0] = num1.toLong()
-        divisors[1] = num2.toLong()
-        divisors[2] = num3.toLong()
-        lcmBetweenDivisors[0] = divisors[0].leastCommonMultiple(divisors[1])
-        lcmBetweenDivisors[1] = divisors[1].leastCommonMultiple(divisors[2])
-        lcmBetweenDivisors[2] = divisors[2].leastCommonMultiple(divisors[0])
-        lcm = lcmBetweenDivisors[0].leastCommonMultiple(divisors[2])
+    private data class Divisors(val d1: Long, val d2: Long, val d3: Long) {
+        val lcmD1D2 = d1.leastCommonMultiple(d2)
+        val lcmD2D3 = d2.leastCommonMultiple(d3)
+        val lcmD3D1 = d3.leastCommonMultiple(d1)
+        val lcmD1D2D3 = lcmD1D2.leastCommonMultiple(d3)
+
+        private fun Long.leastCommonMultiple(other: Long): Long {
+            require(this > 0L && other > 0L) { "Require positive inputs." }
+            val gcd = this.greatestCommonDivisor(other)
+            return this * other / gcd
+        }
+
+        private fun Long.greatestCommonDivisor(other: Long): Long {
+            require(this > 0L && other > 0L) { "Require positive inputs." }
+            val (smaller, larger) = if (this < other) this to other else other to this
+            return if (larger % smaller == 0L) smaller else euclideanGcdAlgorithm(smaller, larger)
+        }
+
+        private tailrec fun euclideanGcdAlgorithm(smaller: Long, larger: Long): Long =
+            if (smaller == 0L) larger else euclideanGcdAlgorithm(larger % smaller, smaller)
     }
 
-    private fun Long.leastCommonMultiple(other: Long): Long {
-        require(this > 0L && other > 0L) { "Require positive inputs." }
-        val gcd = this.greatestCommonDivisor(other)
-        return this * other / gcd
-    }
-
-    private fun Long.greatestCommonDivisor(other: Long): Long {
-        require(this > 0L && other > 0L) { "Require positive inputs." }
-        val (smaller, larger) = if (this < other) this to other else other to this
-        return if (larger % smaller == 0L) smaller else processEuclideanGcdAlgorithm(smaller, larger)
-    }
-
-    private tailrec fun processEuclideanGcdAlgorithm(smaller: Long, larger: Long): Long =
-        if (smaller == 0L) larger else processEuclideanGcdAlgorithm(larger % smaller, smaller)
-
-    private fun getNthUglyNumber(n: Int): Int {
-        val numberOfUglyWithinLcm = getNumberOfUglyWithin(lcm)
-        var nThUgly = (n / numberOfUglyWithinLcm) * lcm
+    private fun getNthUglyNumber(n: Int, divisors: Divisors): Int {
+        val numberOfUglyWithinLcm = getNumberOfUglyWithin(divisors.lcmD1D2D3, divisors)
+        var nThUgly = (n / numberOfUglyWithinLcm) * divisors.lcmD1D2D3
         val rem = n % numberOfUglyWithinLcm
         if (rem != 0L) {
-            val remThUgly = getNthUglyNumberWithinLcm(rem.toInt())
+            val remThUgly = getNthUglyNumberWithinLcm(rem.toInt(), divisors)
             nThUgly += remThUgly
         }
         return nThUgly.toInt()
     }
 
-    private fun getNumberOfUglyWithin(value: Long): Long {
-        val naiveCount = divisors.let { value / it[0] + value / it[1] + value / it[2] }
-        val duplication = -value / lcm +
-                value / lcmBetweenDivisors[0] +
-                value / lcmBetweenDivisors[1] +
-                value / lcmBetweenDivisors[2]
+    private fun getNumberOfUglyWithin(value: Long, divisors: Divisors): Long {
+        val naiveCount =
+            divisors.run { value / d1  + value / d2 + value / d3 }
+        val duplication =
+            divisors.run { -value / lcmD1D2D3 + value / lcmD1D2 + value / lcmD2D3 + value / lcmD3D1 }
         return naiveCount - duplication
     }
 
-    private fun getNthUglyNumberWithinLcm(n: Int): Long {
+    private fun getNthUglyNumberWithinLcm(n: Int, divisors: Divisors): Long {
         var leftBound = 1L
-        var rightBound = lcm
+        var rightBound = divisors.lcmD1D2D3
         while (leftBound <= rightBound) {
             val midValue = (leftBound + rightBound) shr 1
-            val numberOfUglyAtMid = getNumberOfUglyWithin(midValue)
+            val numberOfUglyAtMid = getNumberOfUglyWithin(midValue, divisors)
             if (numberOfUglyAtMid >= n) rightBound = midValue - 1 else leftBound = midValue + 1
         }
         return leftBound
-    }
-
-    private fun clearStates() {
-        divisors.fill(0L)
-        lcmBetweenDivisors.fill(0L)
-        lcm = 0L
     }
 }
