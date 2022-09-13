457398e8793111c7b7af674c3de987ceee7433f7
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-14 00:53:11 +0800

    Update problem 2248 : refactor local functions;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2248/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2248/Solution.kt
@@ -8,24 +8,21 @@ class Solution {
      * Time O(N+M) and Aux_Space O(M) where N is the flat size of nums and M is the size of value range;
      */
     fun intersection(nums: Array<IntArray>): List<Int> {
+        val numFrequency = getNumberFrequency(nums)
+        return getSortedNumbersOfSpecificFrequency(numFrequency, nums.size)
+    }
+
+    private fun getNumberFrequency(numbers: Array<IntArray>): IntArray {
         val numFrequency = IntArray(1001) // size determined by constraint of value range;
-        updateFrequency(numFrequency, nums)
-        return getNumberWithFrequency(numFrequency, nums.size)
-    }
-
-    private fun updateFrequency(numFrequency: IntArray, numbers: Array<IntArray>) {
         for (array in numbers) {
-            updateFrequency(numFrequency, array)
+            for (num in array) {
+                numFrequency[num]++
+            }
         }
+        return numFrequency
     }
 
-    private fun updateFrequency(numFrequency: IntArray, numbers: IntArray) {
-        for (num in numbers) {
-            numFrequency[num]++
-        }
-    }
-
-    private fun getNumberWithFrequency(numFrequency: IntArray, frequency: Int): List<Int> {
+    private fun getSortedNumbersOfSpecificFrequency(numFrequency: IntArray, frequency: Int): List<Int> {
         val matchedNumbers = mutableListOf<Int>()
         for (index in numFrequency.indices) {
             if (numFrequency[index] == frequency) matchedNumbers.add(index)
