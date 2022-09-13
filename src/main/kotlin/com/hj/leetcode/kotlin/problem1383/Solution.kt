09442986a38bdbedfd355f343cf5b2dabaab4333
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-13 23:47:44 +0800

    Update problem 1383 : local clean up;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1383/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1383/Solution.kt
@@ -6,31 +6,31 @@ import java.util.*
  * LeetCode page: [1383. Maximum Performance of a Team](https://leetcode.com/problems/maximum-performance-of-a-team/);
  */
 class Solution {
-    private data class Agile(val speed: Int, val efficiency: Int)
+    private data class Engineer(val speed: Int, val efficiency: Int)
 
     /* Complexity:
      * Time O(NLogN) and Space O(N) where N equals n;
      */
     fun maxPerformance(n: Int, speed: IntArray, efficiency: IntArray, k: Int): Int {
-        val sortedAgile = getAgileSortedByEfficiency(speed, efficiency)
-        return getMaxPerformance(k, sortedAgile)
+        val sortedEngineers = createEngineersSortedByEfficiency(speed, efficiency)
+        return findMaxPerformance(k, sortedEngineers)
     }
 
-    private fun getAgileSortedByEfficiency(speed: IntArray, efficiency: IntArray): List<Agile> {
+    private fun createEngineersSortedByEfficiency(speed: IntArray, efficiency: IntArray): List<Engineer> {
         return speed
             .asSequence()
-            .zip(efficiency.asSequence()) { spd, eff -> Agile(spd, eff) }
-            .sortedBy { agile -> agile.efficiency }
+            .zip(efficiency.asSequence()) { spd, eff -> Engineer(spd, eff) }
+            .sortedBy { engineer -> engineer.efficiency }
             .toList()
     }
 
-    private fun getMaxPerformance(maxTeamSize: Int, agileSortedByEfficiency: List<Agile>): Int {
+    private fun findMaxPerformance(maxTeamSize: Int, engineersSortedByEfficiency: List<Engineer>): Int {
         var maxPerformance = 0L
         val teamSpeedMinPq = PriorityQueue<Int>()
         var maxTeamSpeedSum = 0L
-        for (index in agileSortedByEfficiency.indices.reversed()) {
-            val currSpeed = agileSortedByEfficiency[index].speed
-            val currEfficiency = agileSortedByEfficiency[index].efficiency
+        for (index in engineersSortedByEfficiency.indices.reversed()) {
+            val currSpeed = engineersSortedByEfficiency[index].speed
+            val currEfficiency = engineersSortedByEfficiency[index].efficiency
             teamSpeedMinPq.offer(currSpeed)
             maxTeamSpeedSum += currSpeed
             if (teamSpeedMinPq.size > maxTeamSize) maxTeamSpeedSum -= teamSpeedMinPq.poll()
