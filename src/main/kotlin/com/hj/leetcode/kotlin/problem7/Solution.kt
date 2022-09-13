acbd809e6ee349f0ea789cbe90751e3b0338883b
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-14 00:37:32 +0800

    Update problem 7 : improve naming and function signatures;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem7/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem7/Solution.kt
@@ -11,16 +11,16 @@ class Solution {
      */
     fun reverse(x: Int): Int {
         val reversedDigits = getReversedDigits(x)
-        return getReversedNumber(x < 0, reversedDigits)
+        return getReversedNumber(reversedDigits, x < 0)
     }
 
     private fun getReversedDigits(number: Int): List<Int> {
         val reversedDigits = mutableListOf<Int>()
-        if (number == 0) reversedDigits.add(0) else updateReversedDigitsForNonZero(number, reversedDigits)
+        if (number == 0) reversedDigits.add(0) else updateReversedDigitsForNonZero(reversedDigits, number)
         return reversedDigits
     }
 
-    private fun updateReversedDigitsForNonZero(number: Int, reversedDigits: MutableList<Int>) {
+    private fun updateReversedDigitsForNonZero(reversedDigits: MutableList<Int>, number: Int) {
         require(number != 0)
         var num = Math.abs(number)
         while (num > 0) {
@@ -29,7 +29,7 @@ class Solution {
         }
     }
 
-    private fun getReversedNumber(isNegative: Boolean, reversedDigits: List<Int>): Int {
+    private fun getReversedNumber(reversedDigits: List<Int>, isNegative: Boolean): Int {
         if (isReversedOverflow(reversedDigits)) return 0
 
         val abs = reversedDigits.fold(0) { acc, i -> acc * 10 + i }
@@ -40,11 +40,11 @@ class Solution {
         return when {
             reversedDigits.size < digitsOfIntMax.size -> false
             reversedDigits.size > digitsOfIntMax.size -> throw IllegalStateException()
-            else -> checkOverflowOfSameNumberOfDigits(reversedDigits)
+            else -> checkOverflowForEqualNumberOfDigits(reversedDigits)
         }
     }
 
-    private fun checkOverflowOfSameNumberOfDigits(reversedDigits: List<Int>): Boolean {
+    private fun checkOverflowForEqualNumberOfDigits(reversedDigits: List<Int>): Boolean {
         for (index in reversedDigits.indices) {
             when {
                 reversedDigits[index] < digitsOfIntMax[index] -> return false
