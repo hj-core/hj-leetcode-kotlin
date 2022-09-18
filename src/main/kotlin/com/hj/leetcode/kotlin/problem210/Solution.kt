db8f3974657f99c7730b024852b8af21b0504527
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-18 21:58:30 +0800

    Add solution of problem 210;

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem210/Solution.kt
@@ -0,0 +1,76 @@
+package com.hj.leetcode.kotlin.problem210
+
+/**
+ * LeetCode page: [210. Course Schedule II](https://leetcode.com/problems/course-schedule-ii/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(V+E) and Space O(V+E) where V is numCourses and E is size of prerequisites;
+     */
+    fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+        val graph = createPrerequisiteToCourses(numCourses, prerequisites)
+        val count = createCourseToNumPendingPrerequisite(numCourses, prerequisites)
+        return findPathToFinishAllCourses(numCourses, count, graph)
+    }
+
+    private fun createPrerequisiteToCourses(
+        numCourses: Int,
+        prerequisites: Array<IntArray>
+    ): List<List<Int>> {
+        val graph = List(numCourses) { mutableListOf<Int>() }
+        for (dependency in prerequisites) {
+            val course = dependency[0]
+            val prerequisite = dependency[1]
+            graph[prerequisite].add(course)
+        }
+        return graph
+    }
+
+    private fun createCourseToNumPendingPrerequisite(
+        numCourses: Int,
+        prerequisites: Array<IntArray>
+    ): IntArray {
+        val count = IntArray(numCourses)
+        for (dependency in prerequisites) {
+            val course = dependency[0]
+            count[course]++
+        }
+        return count
+    }
+
+    private fun findPathToFinishAllCourses(
+        numCourses: Int,
+        courseToNumPendingPrerequisite: IntArray,
+        prerequisiteToCourses: List<List<Int>>
+    ): IntArray {
+        val path = IntArray(numCourses)
+
+        var indexNextCourse = 0
+        for (course in courseToNumPendingPrerequisite.indices) {
+            val noPrerequisite = courseToNumPendingPrerequisite[course] == 0
+            if (noPrerequisite) {
+                path[indexNextCourse] = course
+                indexNextCourse++
+            }
+        }
+
+        var indexPrevCourse = 0
+        while (indexPrevCourse < indexNextCourse) {
+            val completedCourse = path[indexPrevCourse]
+            val coursesRequiringIt = prerequisiteToCourses[completedCourse]
+
+            for (course in coursesRequiringIt) {
+                courseToNumPendingPrerequisite[course]--
+                val canBeCompleted = courseToNumPendingPrerequisite[course] == 0
+                if (canBeCompleted) {
+                    path[indexNextCourse] = course
+                    indexNextCourse++
+                }
+            }
+            indexPrevCourse++
+        }
+
+        val pathFound = indexNextCourse == path.size
+        return if (pathFound) path else intArrayOf()
+    }
+}
