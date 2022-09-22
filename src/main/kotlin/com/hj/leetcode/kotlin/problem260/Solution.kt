6dc79a8ffcaab6a901eaaabda76b9f025f6231df
hj-core <88603338+hj-core@users.noreply.github.com>
2022-09-22 23:26:13 +0800

    Add solution of problem 260;

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem260/Solution.kt
@@ -0,0 +1,30 @@
+package com.hj.leetcode.kotlin.problem260
+
+/**
+ * LeetCode page: [260. Single Number III](https://leetcode.com/problems/single-number-iii/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the size of nums;
+     */
+    fun singleNumber(nums: IntArray): IntArray {
+        val xorOfSingles = nums.reduce { acc, i -> acc xor i }
+        val maskForSeparation = xorOfSingles.getRightmostSetBit()
+
+        var single1 = 0
+        var single2 = 0
+        for (num in nums) {
+            val isGroup1 = num.hasBitMaskSet(maskForSeparation)
+            if (isGroup1) {
+                single1 = single1 xor num
+            } else {
+                single2 = single2 xor num
+            }
+        }
+        return intArrayOf(single1, single2)
+    }
+
+    private fun Int.getRightmostSetBit(): Int = this - (this and (this - 1))
+
+    private fun Int.hasBitMaskSet(mask: Int): Boolean = this and mask == mask
+}
