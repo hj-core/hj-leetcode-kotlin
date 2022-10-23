4bf8191bacebc82ff20e15336236a41059dfc4a2
hj-core <88603338+hj-core@users.noreply.github.com>
2022-10-23 19:50:13 +0800

    Add solution of problem 645;

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem645/Solution.kt
@@ -0,0 +1,67 @@
+package com.hj.leetcode.kotlin.problem645
+
+/**
+ * LeetCode page: [645. Set Mismatch](https://leetcode.com/problems/set-mismatch/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the size of nums;
+     */
+    fun findErrorNums(nums: IntArray): IntArray {
+        val repetitionXorLoss = getRepetitionXorLoss(nums)
+
+        // bitmask that can distinguish between repetition and loss
+        val distinguishMask = repetitionXorLoss.rightmostBit()
+        val candidates = findCandidatesOfRepetitionAndLoss(nums, distinguishMask)
+        return determineRepetitionAndLoss(nums, candidates)
+    }
+
+    private fun getRepetitionXorLoss(nums: IntArray): Int {
+        var ans = 0
+
+        for (num in nums) {
+            ans = ans xor num
+        }
+
+        for (num in 1..nums.size) {
+            ans = ans xor num
+        }
+
+        return ans
+    }
+
+    private fun Int.rightmostBit() = this - (this and (this - 1))
+
+    private fun findCandidatesOfRepetitionAndLoss(nums: IntArray, distinguishMask: Int): Pair<Int, Int> {
+        var candidate1 = 0
+        var candidate2 = 0
+
+        for (num in nums) {
+            if (num and distinguishMask == 0) {
+                candidate1 = candidate1 xor num
+            } else {
+                candidate2 = candidate2 xor num
+            }
+        }
+
+        for (num in 1..nums.size) {
+            if (num and distinguishMask == 0) {
+                candidate1 = candidate1 xor num
+            } else {
+                candidate2 = candidate2 xor num
+            }
+        }
+
+        return Pair(candidate1, candidate2)
+    }
+
+    private fun determineRepetitionAndLoss(nums: IntArray, candidates: Pair<Int, Int>): IntArray {
+        val (candidate1, candidate2) = candidates
+
+        for (num in nums) {
+            if (num == candidate1) return intArrayOf(candidate1, candidate2)
+        }
+
+        return intArrayOf(candidate2, candidate1)
+    }
+}
