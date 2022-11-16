c860a6e4cb0452dd6ee6beb6e14ecda377d98742
hj-core <88603338+hj-core@users.noreply.github.com>
2022-11-16 22:32:14 +0800

    draft solution by full-BFS;

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem947/Solution.kt
@@ -0,0 +1,65 @@
+package com.hj.leetcode.kotlin.problem947
+
+/**
+ * LeetCode page: [947. Most Stones Removed with Same Row or Column](https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the size of stones;
+     */
+    fun removeStones(stones: Array<IntArray>): Int {
+        val rowGroups = groupStoneIndicesByRow(stones)
+        val columnGroups = groupStoneIndicesByColumn(stones)
+        val numConnectedComponents = findNumConnectedComponents(stones, rowGroups, columnGroups)
+        return stones.size - numConnectedComponents
+    }
+
+    private fun groupStoneIndicesByRow(stones: Array<IntArray>): Map<Int, List<Int>> {
+        return stones.indices.groupBy { index -> stones[index][0] }
+    }
+
+    private fun groupStoneIndicesByColumn(stones: Array<IntArray>): Map<Int, List<Int>> {
+        return stones.indices.groupBy { index -> stones[index][1] }
+    }
+
+    private fun findNumConnectedComponents(
+        stones: Array<IntArray>,
+        rowGroups: Map<Int, List<Int>>,
+        columnGroups: Map<Int, List<Int>>
+    ): Int {
+        var numConnectedComponents = 0
+        val visited = BooleanArray(stones.size)
+
+        for (index in stones.indices) {
+            if (visited[index]) continue
+
+            numConnectedComponents++
+            visited[index] = true
+            val queue = ArrayDeque<Int>().apply { addLast(index) }
+
+            while (queue.isNotEmpty()) {
+                val first = queue.removeFirst()
+                val stone = stones[first]
+                val (row, column) = stone
+
+                val sameRowIndices = rowGroups[row] ?: emptyList()
+                for (sameRowIndex in sameRowIndices) {
+                    if (!visited[sameRowIndex]) {
+                        queue.addLast(sameRowIndex)
+                        visited[sameRowIndex] = true
+                    }
+                }
+
+                val sameColumnIndices = columnGroups[column] ?: emptyList()
+                for (sameColumnIndex in sameColumnIndices) {
+                    if (!visited[sameColumnIndex]) {
+                        queue.addLast(sameColumnIndex)
+                        visited[sameColumnIndex] = true
+                    }
+                }
+            }
+        }
+
+        return numConnectedComponents
+    }
+}
