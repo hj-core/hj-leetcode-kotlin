8e045599164e329b317c7337bf9ac3a9c555d194
hj-core <88603338+hj-core@users.noreply.github.com>
2022-12-04 12:10:10 +0800

    Add solution to problem 2256;

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2256/Solution.kt
@@ -0,0 +1,34 @@
+package com.hj.leetcode.kotlin.problem2256
+
+/**
+ * LeetCode page: [2256. Minimum Average Difference](https://leetcode.com/problems/minimum-average-difference/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(|nums|) and Space O(1);
+     */
+    fun minimumAverageDifference(nums: IntArray): Int {
+        val totalSum = nums.fold(0L) { acc, i -> acc + i }
+        var leftSum = 0L
+        var leftSize = 0
+        var minAbsAverageDiff = Long.MAX_VALUE
+        var indexOfMin = -1
+
+        for ((index, num) in nums.withIndex()) {
+            leftSum += num
+            leftSize++
+            val leftAverage = leftSum / leftSize
+
+            val rightSum = totalSum - leftSum
+            val rightSize = nums.size - leftSize
+            val rightAverage = if (rightSize == 0) 0 else rightSum / rightSize
+
+            val absAverageDiff = Math.abs(leftAverage - rightAverage)
+            if (absAverageDiff < minAbsAverageDiff) {
+                minAbsAverageDiff = absAverageDiff
+                indexOfMin = index
+            }
+        }
+        return indexOfMin
+    }
+}
