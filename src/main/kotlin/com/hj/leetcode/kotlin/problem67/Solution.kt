6b91d337abfaa7c4284562d1c50517738f65c5b4
hj-core <88603338+hj-core@users.noreply.github.com>
2023-02-14 23:04:26 +0800

    Add solution to problem 67;

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem67/Solution.kt
@@ -0,0 +1,35 @@
+package com.hj.leetcode.kotlin.problem67
+
+/**
+ * LeetCode page: [67. Add Binary](https://leetcode.com/problems/add-binary/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(L) and Space O(L) where L is the longer length of a and b;
+     */
+    fun addBinary(a: String, b: String): String {
+        val (large, small) = if (a.length > b.length) a to b else b to a
+        val reversedRes = StringBuilder()
+        var carry = 0
+
+        for (i in large.indices) {
+            val bit1 = large.let { it[it.lastIndex - i].digitToInt() }
+            val bit2 = small.let { it.getOrNull(it.lastIndex - i)?.digitToInt() } ?: 0
+            val sum = bit1 + bit2 + carry
+            reversedRes.append(sum and 1)
+            carry = if (sum < 2) 0 else 1
+        }
+        if (carry == 1) reversedRes.append(carry)
+
+        return reversedRes.run {
+            reverse()
+            toString()
+        }
+    }
+
+    private fun Char.digitToInt(): Int {
+        val res = Character.digit(this.toInt(), 10)
+        if (res < 0) throw IllegalArgumentException("Char $this is not a decimal digit")
+        return res
+    }
+}
