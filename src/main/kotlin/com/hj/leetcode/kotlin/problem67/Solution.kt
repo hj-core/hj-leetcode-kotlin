37f7eabeb9f759473217c830ac60491db58595ac
hj-core <88603338+hj-core@users.noreply.github.com>
2023-02-15 11:23:10 +0800

    Update problem 67 : Rename local variables for clarity;

--- src/main/kotlin/com/hj/leetcode/kotlin/problem67/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem67/Solution.kt
@@ -8,13 +8,13 @@ class Solution {
      * Time O(L) and Space O(L) where L is the longer length of a and b;
      */
     fun addBinary(a: String, b: String): String {
-        val (large, small) = if (a.length > b.length) a to b else b to a
+        val (long, short) = if (a.length > b.length) a to b else b to a
         val reversedRes = StringBuilder()
         var carry = 0
 
-        for (i in large.indices) {
-            val bit1 = large.let { it[it.lastIndex - i].digitToInt() }
-            val bit2 = small.let { it.getOrNull(it.lastIndex - i)?.digitToInt() } ?: 0
+        for (i in long.indices) {
+            val bit1 = long.let { it[it.lastIndex - i].digitToInt() }
+            val bit2 = short.let { it.getOrNull(it.lastIndex - i)?.digitToInt() } ?: 0
             val sum = bit1 + bit2 + carry
             reversedRes.append(sum and 1)
             carry = if (sum < 2) 0 else 1
