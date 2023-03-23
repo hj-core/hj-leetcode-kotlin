9028aaecf9b1e190bc7f4673f1931568d8831b4d
hj-core <88603338+hj-core@users.noreply.github.com>
2023-03-23 19:29:32 +0800

    Add solutions to problem 1319 (#7)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1319/Solution2.kt
@@ -0,0 +1,61 @@
+package com.hj.leetcode.kotlin.problem1319
+
+/**
+ * LeetCode page: [1319. Number of Operations to Make Network Connected](https://leetcode.com/problems/number-of-operations-to-make-network-connected/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(E) and Space O(n) where E is the size of connections;
+     */
+    fun makeConnected(n: Int, connections: Array<IntArray>): Int {
+        val numCables = connections.size
+        if (numCables < n - 1) return -1
+
+        val computerUnionFind = UnionFind(n, connections)
+        val numConnectedComponents = numConnectedComponents(computerUnionFind)
+        return numConnectedComponents - 1
+    }
+
+    private class UnionFind(numComputers: Int, connections: Array<IntArray>) {
+
+        private val parent = IntArray(numComputers) { it }
+        private val rank = IntArray(numComputers)
+
+        init {
+            for ((aComputer, bComputer) in connections) {
+                union(aComputer, bComputer)
+            }
+        }
+
+        private fun union(aComputer: Int, bComputer: Int) {
+            val aParent = find(aComputer)
+            val bParent = find(bComputer)
+            if (aParent == bParent) return
+
+            val aRank = rank[aComputer]
+            val bRank = rank[bComputer]
+            when {
+                aRank < bRank -> parent[aParent] = bParent
+                aRank > bRank -> parent[bParent] = aParent
+                else -> {
+                    parent[aParent] = bParent
+                    rank[bParent]++
+                }
+            }
+        }
+
+        private fun find(computer: Int): Int {
+            val isRoot = parent[computer] == computer
+            return if (isRoot) computer else find(parent[computer])
+        }
+
+        fun numUnions(): Int {
+            val computers = parent.indices
+            return computers.count { it == parent[it] }
+        }
+    }
+
+    private fun numConnectedComponents(computerUnionFind: UnionFind): Int {
+        return computerUnionFind.numUnions()
+    }
+}
