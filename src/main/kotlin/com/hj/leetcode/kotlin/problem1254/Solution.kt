ae05450d10861ce30527cda8a78467fa7927d5df
hj-core <88603338+hj-core@users.noreply.github.com>
2023-04-07 17:04:28 +0800

    Update problem1254 (#27)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1254/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1254/Solution.kt
@@ -22,33 +22,32 @@ class Solution {
         val numRows = grid.size
         val numColumns = grid[0].size
         val visited = Array(numRows) { BooleanArray(numColumns) }
+
         for (row in grid.indices) {
             for (column in grid[row].indices) {
                 val cell = Cell(row, column)
-                if (cell.isNotUnvisitedLand(grid, visited)) {
-                    continue
+                if (cell.isUnvisitedLand(grid, visited)) {
+                    var isClosedIsland = true
+                    visitAllConnectedUnvisitedLands(cell, grid, visited) { isOnBoundary ->
+                        if (isOnBoundary) isClosedIsland = false
+                    }
+                    onEachIsland(isClosedIsland)
                 }
-
-                var isClosedIsland = true
-                visitAllConnectedUnvisitedLands(cell, grid, visited) { isOnBoundary ->
-                    if (isOnBoundary) isClosedIsland = false
-                }
-                onEachIsland(isClosedIsland)
             }
         }
     }
 
     private data class Cell(val row: Int, val column: Int)
 
-    private fun Cell.isNotUnvisitedLand(grid: Array<IntArray>, visited: Array<BooleanArray>): Boolean {
-        return isOutOfBounds(grid) || isWater(grid[row][column]) || visited[row][column]
+    private fun Cell.isUnvisitedLand(grid: Array<IntArray>, visited: Array<BooleanArray>): Boolean {
+        return isWithinBounds(grid) && isLand(grid[row][column]) && !visited[row][column]
     }
 
-    private fun Cell.isOutOfBounds(grid: Array<IntArray>): Boolean {
-        return row !in grid.indices || column !in grid[row].indices
+    private fun Cell.isWithinBounds(grid: Array<IntArray>): Boolean {
+        return row in grid.indices && column in grid[row].indices
     }
 
-    private fun isWater(gridValue: Int): Boolean = gridValue == 1
+    private fun isLand(gridValue: Int): Boolean = gridValue == 0
 
     private fun visitAllConnectedUnvisitedLands(
         source: Cell,
@@ -56,18 +55,16 @@ class Solution {
         visited: Array<BooleanArray>,
         onEachLand: (isOnBoundary: Boolean) -> Unit
     ) {
-        if (source.isNotUnvisitedLand(grid, visited)) {
-            return
+        if (source.isUnvisitedLand(grid, visited)) {
+            onEachLand(source.isOnBoundary(grid))
+            val (row, column) = source
+            visited[row][column] = true
+
+            visitAllConnectedUnvisitedLands(Cell(row + 1, column), grid, visited, onEachLand)
+            visitAllConnectedUnvisitedLands(Cell(row - 1, column), grid, visited, onEachLand)
+            visitAllConnectedUnvisitedLands(Cell(row, column + 1), grid, visited, onEachLand)
+            visitAllConnectedUnvisitedLands(Cell(row, column - 1), grid, visited, onEachLand)
         }
-
-        onEachLand(source.isOnBoundary(grid))
-        val (row, column) = source
-        visited[row][column] = true
-
-        visitAllConnectedUnvisitedLands(Cell(row + 1, column), grid, visited, onEachLand)
-        visitAllConnectedUnvisitedLands(Cell(row - 1, column), grid, visited, onEachLand)
-        visitAllConnectedUnvisitedLands(Cell(row, column + 1), grid, visited, onEachLand)
-        visitAllConnectedUnvisitedLands(Cell(row, column - 1), grid, visited, onEachLand)
     }
 
     private fun Cell.isOnBoundary(grid: Array<IntArray>): Boolean {
