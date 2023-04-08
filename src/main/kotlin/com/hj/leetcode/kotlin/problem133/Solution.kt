6667f6ac03ac4f60ff1058c505bc320bb89fc2f1
hj-core <88603338+hj-core@users.noreply.github.com>
2023-04-08 16:01:14 +0800

    Add solutions to problem 133

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem133/Solution.kt
@@ -0,0 +1,33 @@
+package com.hj.leetcode.kotlin.problem133
+
+/**
+ * LeetCode page: [133. Clone Graph](https://leetcode.com/problems/clone-graph/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(V+E) and Space O(V) where V and E are the number of nodes and edges in node;
+     */
+    fun cloneGraph(node: Node?): Node? {
+        if (node == null) return null
+        return cloneGraph(node, hashMapOf())
+    }
+
+    private fun cloneGraph(node: Node, nodeClone: MutableMap<Node, Node>): Node {
+        if (nodeClone[node] != null) {
+            return nodeClone[node]!!
+        }
+
+        val clone = nodeClone.computeIfAbsent(node) { Node(node.`val`) }
+        for (neighbor in node.neighbors) {
+            if (neighbor != null) {
+                val neighborClone = cloneGraph(neighbor, nodeClone)
+                clone.neighbors.add(neighborClone)
+            }
+        }
+        return clone
+    }
+}
+
+class Node(var `val`: Int) {
+    var neighbors: ArrayList<Node?> = ArrayList()
+}
