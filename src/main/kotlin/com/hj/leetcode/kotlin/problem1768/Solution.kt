f5082b5db5df938b2056c797a2e3097b90fec6e3
hj-core <88603338+hj-core@users.noreply.github.com>
2023-04-18 10:24:14 +0800

    Add a solution to problem 1768

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1768/Solution.kt
@@ -0,0 +1,25 @@
+package com.hj.leetcode.kotlin.problem1768
+
+/**
+ * LeetCode page: [1768. Merge Strings Alternately](https://leetcode.com/problems/merge-strings-alternately/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(M+N) and Space O(1) where M and N are the length of word1 and word2;
+     */
+    fun mergeAlternately(word1: String, word2: String): String {
+        val (short, long) =
+            if (word1.length < word2.length) word1 to word2 else word2 to word1
+        val result = StringBuilder()
+
+        for (index in short.indices) {
+            result
+                .append(word1[index])
+                .append(word2[index])
+        }
+        for (index in short.length until long.length) {
+            result.append(long[index])
+        }
+        return result.toString()
+    }
+}
