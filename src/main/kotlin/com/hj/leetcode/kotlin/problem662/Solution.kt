ee42098a2b3fb6c03a891a29833d7d26f8b8f949
hj-core <88603338+hj-core@users.noreply.github.com>
2023-04-20 18:16:48 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem662/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem662/Solution.kt
@@ -13,29 +13,46 @@ class Solution {
         if (root == null) return 0
 
         var maxWidth = 0
+        // nodeIndex[node] ::= index of node in level order
         val nodeIndex = hashMapOf<TreeNode, Int>()
         val bfsQueue = ArrayDeque<TreeNode>()
 
-        nodeIndex[root] = 1
+        nodeIndex[root] = 0
         bfsQueue.addLast(root)
         while (bfsQueue.isNotEmpty()) {
-            val levelMaxWidth =
-                checkNotNull(nodeIndex[bfsQueue.last()]) - checkNotNull(nodeIndex[bfsQueue.first()]) + 1
-            if (maxWidth < levelMaxWidth) maxWidth = levelMaxWidth
-
-            repeat(bfsQueue.size) {
-                val popped = bfsQueue.removeFirst()
-                val indexPopped = checkNotNull(nodeIndex[popped])
-                popped.left?.let {
-                    nodeIndex[it] = indexPopped * 2
-                    bfsQueue.addLast(it)
-                }
-                popped.right?.let {
-                    nodeIndex[it] = indexPopped * 2 + 1
-                    bfsQueue.addLast(it)
-                }
+            val levelMaxWidth = levelMaxWidth(bfsQueue, nodeIndex)
+            if (maxWidth < levelMaxWidth) {
+                maxWidth = levelMaxWidth
             }
+            updateNextLevel(bfsQueue, nodeIndex)
         }
         return maxWidth
+    }
+
+    private fun levelMaxWidth(
+        bfsQueue: ArrayDeque<TreeNode>,
+        nodeIndex: Map<TreeNode, Int>
+    ): Int {
+        val leftmostIndex = checkNotNull(nodeIndex[bfsQueue.first()])
+        val rightmostIndex = checkNotNull(nodeIndex[bfsQueue.last()])
+        return rightmostIndex - leftmostIndex + 1
+    }
+
+    private fun updateNextLevel(
+        bfsQueue: ArrayDeque<TreeNode>,
+        nodeIndex: MutableMap<TreeNode, Int>
+    ) {
+        repeat(bfsQueue.size) {
+            val node = bfsQueue.removeFirst()
+            val index = checkNotNull(nodeIndex[node])
+            node.left?.let {
+                nodeIndex[it] = index * 2 + 1
+                bfsQueue.addLast(it)
+            }
+            node.right?.let {
+                nodeIndex[it] = index * 2 + 2
+                bfsQueue.addLast(it)
+            }
+        }
     }
 }
