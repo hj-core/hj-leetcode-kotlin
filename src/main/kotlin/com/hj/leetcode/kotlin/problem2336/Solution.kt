95edbcfc5a366035725a58a8f7dbbedb57685dfe
hj-core <88603338+hj-core@users.noreply.github.com>
2023-04-25 21:10:27 +0800

    Add a solution to problem 2336 (#43)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2336/Solution.kt
@@ -0,0 +1,32 @@
+package com.hj.leetcode.kotlin.problem2336
+
+/**
+ * LeetCode page: [2336. Smallest Number in Infinite Set](https://leetcode.com/problems/smallest-number-in-infinite-set/);
+ */
+class SmallestInfiniteSet() {
+
+    private var largestPopped = 0
+    private var sortedAddedBack = sortedSetOf<Int>()
+
+    /* Complexity for N calls:
+     * Time O(NLogN) and Space O(1);
+     */
+    fun popSmallest(): Int {
+        return if (sortedAddedBack.isEmpty()) {
+            largestPopped++
+            largestPopped
+        } else {
+            checkNotNull(sortedAddedBack.pollFirst())
+        }
+    }
+
+    /* Complexity for N calls:
+     * Time O(NLogN) and Space O(N);
+     */
+    fun addBack(num: Int) {
+        val isPopped = num <= largestPopped
+        if (isPopped) {
+            sortedAddedBack.add(num)
+        }
+    }
+}
