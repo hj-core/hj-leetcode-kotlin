ec7a3d2fe646d317132f994b79bbd14be495bff9
hj-core <88603338+hj-core@users.noreply.github.com>
2023-04-27 14:21:15 +0800

    Improve comment

--- src/main/kotlin/com/hj/leetcode/kotlin/problem319/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem319/Solution.kt
@@ -8,8 +8,8 @@ class Solution {
      * Time O(1) and Space O(1);
      */
     fun bulbSwitch(n: Int): Int {
-        /* After n rounds, only those bulbs at perfect squares positions are on due to
-         * their odd number of factors
+        /* After n rounds, each position is toggled its number of factors times, so only the
+         * positions of perfect squares are turned on due to their odd number of factors.
          */
         return Math.sqrt(n.toDouble()).toInt()
     }
