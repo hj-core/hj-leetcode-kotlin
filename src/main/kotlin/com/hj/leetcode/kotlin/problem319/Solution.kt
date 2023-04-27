f93fb8df21904633e803238baa3b3f08cea707af
hj-core <88603338+hj-core@users.noreply.github.com>
2023-04-27 13:07:41 +0800

    Add complexity analysis

--- src/main/kotlin/com/hj/leetcode/kotlin/problem319/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem319/Solution.kt
@@ -4,6 +4,9 @@ package com.hj.leetcode.kotlin.problem319
  * LeetCode page: [319. Bulb Switcher](https://leetcode.com/problems/bulb-switcher/);
  */
 class Solution {
+    /* Complexity:
+     * Time O(1) and Space O(1);
+     */
     fun bulbSwitch(n: Int): Int {
         /* After n rounds, only those bulbs at perfect squares positions are on due to
          * their odd number of factors
