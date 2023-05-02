a03e49d8b7af7e982193e840fd952bc649bd7eda
hj-core <88603338+hj-core@users.noreply.github.com>
2023-05-02 11:16:36 +0800

    Add a solution to problem 1822

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1822/Solution.kt
@@ -0,0 +1,21 @@
+package com.hj.leetcode.kotlin.problem1822
+
+/**
+ * LeetCode page: [1822. Sign of the Product of an Array](https://leetcode.com/problems/sign-of-the-product-of-an-array/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the size of nums;
+     */
+    fun arraySign(nums: IntArray): Int {
+        val hasZero = nums.any { it == 0 }
+        if (hasZero) {
+            return 0
+        }
+
+        val numNegatives = nums.count { it < 0 }
+        return if (numNegatives.isOdd()) -1 else 1
+    }
+
+    private fun Int.isOdd(): Boolean = this and 1 == 1
+}
