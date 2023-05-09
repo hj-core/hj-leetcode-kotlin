8296839b24186f9b1eb930e6203aa6d550c4dddf
hj-core <88603338+hj-core@users.noreply.github.com>
2023-05-09 15:09:06 +0800

    Refactor the solution

--- src/main/kotlin/com/hj/leetcode/kotlin/problem54/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem54/Solution.kt
@@ -5,76 +5,81 @@ package com.hj.leetcode.kotlin.problem54
  */
 class Solution {
     /* Complexity:
-     * Time O(MN) and Space O(MN) where M and N are the rows and columns of matrix;
+     * Time O(N) and Auxiliary Space O(1) where N is the number of elements in matrix;
      */
     fun spiralOrder(matrix: Array<IntArray>): List<Int> {
-        return getElementsSortedBySpiralOrder(matrix)
-    }
-
-    private tailrec fun getElementsSortedBySpiralOrder(
-        matrix: Array<IntArray>,
-        currShift: Int = 0,
-        accElements: MutableList<Int> = mutableListOf()
-    ): List<Int> {
-        val bottomRow = matrix.lastIndex - currShift
-        val rightColumn = matrix[0].lastIndex - currShift
-
-        val shouldComplete = currShift > bottomRow || currShift > rightColumn
-        if (shouldComplete) return accElements
-
-        when (currShift) {
-            bottomRow -> addElementsSingleRowCase(currShift, matrix, accElements)
-            rightColumn -> addElementsSingleColumnCase(currShift, matrix, accElements)
-            else -> addElementsInCurrentSpiralLayer(currShift, matrix, accElements)
+        val result = mutableListOf<Int>()
+        matrix.spiralTraversal { element ->
+            result.add(element)
         }
-
-        return getElementsSortedBySpiralOrder(matrix, currShift + 1, accElements)
+        return result
     }
 
-    private fun addElementsSingleRowCase(
-        currShift: Int,
-        matrix: Array<IntArray>,
-        container: MutableList<Int>
+    private tailrec fun Array<IntArray>.spiralTraversal(
+        fromLayer: Int = 0,
+        onEachElement: (element: Int) -> Unit
     ) {
-        val rightColumn = matrix[0].lastIndex - currShift
-        for (column in currShift..rightColumn) {
-            container.add(matrix[currShift][column])
+        require(fromLayer >= 0)
+
+        // Check if the traversal is ended
+        if (fromLayer > lastSpiralLayer()) {
+            return
         }
+
+        // Traversal the spiral layer at fromLayer
+        spiralTraversalAt(fromLayer, onEachElement)
+
+        // Recursive call for next layer
+        spiralTraversal(fromLayer + 1, onEachElement)
     }
 
-    private fun addElementsSingleColumnCase(
-        currShift: Int,
-        matrix: Array<IntArray>,
-        container: MutableList<Int>
-    ) {
-        val bottomRow = matrix.lastIndex - currShift
-        for (row in currShift..bottomRow) {
-            container.add(matrix[row][currShift])
-        }
+    private fun Array<IntArray>.lastSpiralLayer(): Int {
+        val indexLastRow = this.lastIndex
+        val indexLastColumn = this[0].lastIndex
+        val minLastIndex = minOf(indexLastRow, indexLastColumn)
+        return minLastIndex / 2
     }
 
-    private fun addElementsInCurrentSpiralLayer(
-        currShift: Int,
-        matrix: Array<IntArray>,
-        container: MutableList<Int>
+    private fun Array<IntArray>.spiralTraversalAt(
+        layer: Int,
+        onEachElement: (element: Int) -> Unit
     ) {
-        val bottomIndex = matrix.lastIndex - currShift
-        val rightIndex = matrix[0].lastIndex - currShift
+        require(layer >= 0)
 
-        for (column in currShift..rightIndex) {
-            container.add(matrix[currShift][column])
+        val indexLastRow = this.lastIndex
+        val indexLastColumn = this[0].lastIndex
+        val top = layer
+        val bottom = indexLastRow - layer
+        val left = layer
+        val right = indexLastColumn - layer
+
+        check(top <= bottom)
+        check(left <= right)
+
+        // Traversal the top row
+        for (column in left..right) {
+            onEachElement(this[top][column])
         }
 
-        for (row in currShift + 1 until bottomIndex) {
-            container.add(matrix[row][rightIndex])
+        // Traversal the right column, except its first and last elements
+        for (row in top + 1 until bottom) {
+            onEachElement(this[row][right])
         }
 
-        for (column in rightIndex downTo currShift) {
-            container.add(matrix[bottomIndex][column])
+        // If bottom and top are different, traversal the bottom row in reversed order
+        if (bottom != top) {
+            for (column in right downTo left) {
+                onEachElement(this[bottom][column])
+            }
         }
 
-        for (row in bottomIndex - 1 downTo currShift + 1) {
-            container.add(matrix[row][currShift])
+        /* If left and right are different, traversal the left column in reversed order, except the
+         * first and last elements.
+         */
+        if (left != right) {
+            for (row in bottom - 1 downTo top + 1) {
+                onEachElement(this[row][layer])
+            }
         }
     }
 }
