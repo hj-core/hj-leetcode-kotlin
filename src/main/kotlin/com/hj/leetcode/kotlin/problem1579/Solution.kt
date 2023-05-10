e872170817ef621ecc7f041892d403409818ab98
hj-core <88603338+hj-core@users.noreply.github.com>
2023-05-10 09:36:42 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1579/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1579/Solution.kt
@@ -17,9 +17,9 @@ class Solution {
         }
 
         /* The idea is to construct the spanning tree for Alice and Bob respectively.
-         * During the construction, UnionFind is used to determine whether the two nodes of an edge
+         * During the construction, UnionFind is used in determining whether the two nodes of an edge
          * are already in the same connected component.
-         * If it is the case, we know the edge can be removed.
+         * If this is the case, we know the edge can be removed.
          */
         var numRemovedEdges = 0
         val alice = UnionFind(n + 1) // additional size accounts for the unused label 0
@@ -33,8 +33,8 @@ class Solution {
             // Try to union the two nodes of the edge for both Alice and Bob
             val isRequiredEdge = alice.union(u, v)
             bob.union(u, v)
-            /* If the union is no-op, the two nodes have already been connected, so the edge can be removed,
-             * and we increase the number of removed edge by 1
+            /* If the union is no-op, the two nodes have already been connected, so the edge can be
+             * removed, and we increase the number of removed edge by 1
              */
             if (!isRequiredEdge) {
                 numRemovedEdges++
@@ -48,8 +48,8 @@ class Solution {
             }
             // Try to union the two nodes of the edge for Alice
             val isRequiredEdge = alice.union(u, v)
-            /* If the union is no-op, the two nodes have already been connected, so the edge can be removed,
-             * and we increase the number of removed edge by 1
+            /* If the union is no-op, the two nodes have already been connected, so the edge can be
+             * removed, and we increase the number of removed edge by 1
              */
             if (!isRequiredEdge) {
                 numRemovedEdges++
@@ -70,8 +70,8 @@ class Solution {
             }
             // Try to union the two nodes of edge for Bob
             val isRequiredEdge = bob.union(u, v)
-            /* If the union is no-op, the two nodes have already been connected, so the edge can be removed,
-             * and we increase the number of removed edge by 1
+            /* If the union is no-op, the two nodes have already been connected, so the edge can be
+             * removed, and we increase the number of removed edge by 1
              */
             if (!isRequiredEdge) {
                 numRemovedEdges++
