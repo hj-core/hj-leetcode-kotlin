87103aff2e7c525adc593fdf6e6671c60ba4b05f
hj-core <88603338+hj-core@users.noreply.github.com>
2023-05-22 21:44:26 +0800

    Refactor solution

--- src/main/kotlin/com/hj/leetcode/kotlin/problem934/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem934/Solution.kt
@@ -5,103 +5,116 @@ package com.hj.leetcode.kotlin.problem934
  */
 class Solution {
     /* Complexity:
-     * Time O(N^2) and Space O(N^2) where N is the size of grid;
+     * Time O(N) and Space O(N) where N is the number of cells in grid;
      */
     fun shortestBridge(grid: Array<IntArray>): Int {
-        val visited = List(grid.size) { BooleanArray(grid.size) }
-        var surrounding = findCellsAroundIslandAndUpdateVisited(visited, grid)
+        val visited = Array(grid.size) { BooleanArray(grid[it].size) }
+        val currentLayer = waterSurroundingIsland(firstLand(grid), grid, visited)
 
-        var shortest = 0
-        while (surrounding.isNotEmpty()) {
-            val reachSecondIsland = surrounding.any { grid[it.row][it.column] == 1 }
-            if (reachSecondIsland) return shortest
+        /* Search for the second island, layer by layer, and return the distance
+         * when it is found.
+         */
+        var result = 1
+        while (currentLayer.isNotEmpty()) {
+            repeat(currentLayer.size) {
+                val water = currentLayer.removeFirst()
+                for (cell in water.adjacentCells(grid)) {
+                    if (cell.isVisited(visited)) {
+                        continue
+                    }
 
-            surrounding = findUnvisitedAroundCellsAndUpdateVisited(surrounding, visited, grid)
-            shortest++
+                    cell.setVisited(visited)
+                    when {
+                        cell.isLand(grid) -> return result
+                        cell.isWater(grid) -> currentLayer.addLast(cell)
+                        else -> throw NoWhenBranchMatchedException()
+                    }
+                }
+            }
+            result++
         }
-        throw IllegalStateException("Cannot reach second island.")
+        throw IllegalStateException("Can not reach the second island.")
     }
 
-    private fun findCellsAroundIslandAndUpdateVisited(
-        visited: List<BooleanArray>,
-        grid: Array<IntArray>
-    ): List<Cell> {
-        val seedLand = findSeedLand(grid)
-        val surrounding = mutableListOf<Cell>()
-        addCellsAroundIslandAndUpdateVisited(seedLand, visited, grid, surrounding)
-        return surrounding
-    }
+    /**
+     * Return the water that surrounds the island containing [seedLand].
+     * This function will set [visited] of all lands of the island and the surrounding water.
+     */
+    private fun waterSurroundingIsland(
+        seedLand: Cell,
+        grid: Array<IntArray>,
+        visited: Array<BooleanArray>
+    ): ArrayDeque<Cell> {
 
-    private fun findSeedLand(grid: Array<IntArray>): Cell {
-        for (row in grid.indices) {
-            for (column in grid[row].indices) {
-                val isLand = grid[row][column] == 1
-                if (isLand) return Cell(row, column)
+        require(seedLand.isLand(grid))
+
+        val result = ArrayDeque<Cell>()
+        val lands = ArrayDeque<Cell>()
+        lands.add(seedLand)
+        seedLand.setVisited(visited)
+
+        while (lands.isNotEmpty()) {
+            val land = lands.removeFirst()
+            for (cell in land.adjacentCells(grid)) {
+                if (cell.isVisited(visited)) {
+                    continue
+                }
+
+                cell.setVisited(visited)
+                when {
+                    cell.isLand(grid) -> lands.addLast(cell)
+                    cell.isWater(grid) -> result.addLast(cell)
+                    else -> throw NoWhenBranchMatchedException()
+                }
             }
         }
-        throw IllegalStateException("No land found.")
+        return result
+    }
+
+    private fun firstLand(grid: Array<IntArray>): Cell {
+        for (row in grid.indices) {
+            for (column in grid[row].indices) {
+                if (isLand(grid[row][column])) {
+                    return Cell(row, column)
+                }
+            }
+        }
+        throw IllegalStateException("Can not find any land.")
     }
 
     private data class Cell(val row: Int, val column: Int) {
-        val north get() = Cell(row - 1, column)
-        val south get() = Cell(row + 1, column)
-        val west get() = Cell(row, column - 1)
-        val east get() = Cell(row, column + 1)
-    }
 
-    private fun addCellsAroundIslandAndUpdateVisited(
-        seedLand: Cell,
-        visited: List<BooleanArray>,
-        grid: Array<IntArray>,
-        container: MutableList<Cell>
-    ) {
-        with(seedLand) {
-            val shouldSkip = this.isInvalid(grid) || visited[row][column]
-            if (shouldSkip) return
-
-            visited[row][column] = true
-
-            val isSurrounding = grid[row][column] == 0
-            if (isSurrounding) {
-                container.add(this)
-            } else {
-                addCellsAroundIslandAndUpdateVisited(north, visited, grid, container)
-                addCellsAroundIslandAndUpdateVisited(south, visited, grid, container)
-                addCellsAroundIslandAndUpdateVisited(west, visited, grid, container)
-                addCellsAroundIslandAndUpdateVisited(east, visited, grid, container)
-            }
+        fun adjacentCells(grid: Array<IntArray>): List<Cell> {
+            return listOf(
+                Cell(row - 1, column),
+                Cell(row + 1, column),
+                Cell(row, column + 1),
+                Cell(row, column - 1)
+            ).filter { it.inGrid(grid) }
         }
-    }
 
-    private fun Cell.isInvalid(grid: Array<IntArray>) = row !in grid.indices || column !in grid[row].indices
-
-    private fun findUnvisitedAroundCellsAndUpdateVisited(
-        cells: List<Cell>,
-        visited: List<BooleanArray>,
-        grid: Array<IntArray>
-    ): List<Cell> {
-        val newSurrounding = mutableListOf<Cell>()
-        for (cell in cells) {
-            addCellIfUnvisitedAndUpdateVisited(cell.north, visited, grid, newSurrounding)
-            addCellIfUnvisitedAndUpdateVisited(cell.south, visited, grid, newSurrounding)
-            addCellIfUnvisitedAndUpdateVisited(cell.west, visited, grid, newSurrounding)
-            addCellIfUnvisitedAndUpdateVisited(cell.east, visited, grid, newSurrounding)
+        fun inGrid(grid: Array<IntArray>): Boolean {
+            return row in grid.indices && column in grid[row].indices
         }
-        return newSurrounding
-    }
 
-    private fun addCellIfUnvisitedAndUpdateVisited(
-        cell: Cell,
-        visited: List<BooleanArray>,
-        grid: Array<IntArray>,
-        container: MutableList<Cell>
-    ) {
-        with(cell) {
-            val shouldSkip = cell.isInvalid(grid) || visited[row][column]
-            if (shouldSkip) return
+        fun isVisited(visited: Array<BooleanArray>): Boolean {
+            return visited[row][column]
+        }
 
-            container.add(this)
+        fun setVisited(visited: Array<BooleanArray>) {
             visited[row][column] = true
         }
+
+        fun value(grid: Array<IntArray>): Int {
+            return grid[row][column]
+        }
     }
+
+    private fun Cell.isLand(grid: Array<IntArray>): Boolean = isLand(value(grid))
+
+    private fun Cell.isWater(grid: Array<IntArray>): Boolean = isWater(value(grid))
+
+    private fun isLand(value: Int): Boolean = value == 1
+
+    private fun isWater(value: Int): Boolean = value == 0
 }
