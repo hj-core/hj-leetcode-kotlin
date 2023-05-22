e4826d73e465dafd8628b1a7f6d1b2e9c7088950
hj-core <88603338+hj-core@users.noreply.github.com>
2023-05-22 15:35:25 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem347/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem347/Solution.kt
@@ -8,15 +8,28 @@ class Solution {
      * Time O(N) and Space O(N) where N is the size of nums;
      */
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val result = IntArray(k)
         val numbersByFrequency = numbersGroupByFrequency(nums)
-        return topKFrequentNumbers(k, numbersByFrequency)
+        var numCollected = 0
+
+        // Collect the k elements by searching in a decreasing frequency order
+        search@ for (frequency in nums.size downTo 1) {
+            val numbers = numbersByFrequency[frequency] ?: continue
+            for (number in numbers) {
+                result[numCollected] = number
+                numCollected++
+
+                if (numCollected == k) {
+                    break@search
+                }
+            }
+        }
+        return result
     }
 
     private fun numbersGroupByFrequency(numbers: IntArray): Map<Int, List<Int>> {
         val result = hashMapOf<Int, MutableList<Int>>()
-        val numberFrequency = numberFrequency(numbers)
-
-        for ((number, frequency) in numberFrequency) {
+        for ((number, frequency) in numberFrequency(numbers)) {
             result
                 .computeIfAbsent(frequency) { mutableListOf() }
                 .add(number)
@@ -30,28 +43,5 @@ class Solution {
             result[number] = (result[number] ?: 0) + 1
         }
         return result
-    }
-
-    private fun topKFrequentNumbers(
-        k: Int,
-        numbersByFrequency: Map<Int, List<Int>>
-    ): IntArray {
-
-        val result = IntArray(k)
-        var numPicked = 0
-        val maxFrequency = numbersByFrequency.keys.max()!!
-
-        for (frequency in maxFrequency downTo 1) {
-            val numbers = numbersByFrequency[frequency] ?: continue
-            for (number in numbers) {
-                result[numPicked] = number
-                numPicked++
-
-                if (numPicked == k) {
-                    return result
-                }
-            }
-        }
-        throw IllegalStateException()
     }
 }
