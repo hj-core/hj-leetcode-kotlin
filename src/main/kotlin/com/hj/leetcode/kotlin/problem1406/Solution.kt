796ec6d4290c91250e89162b90772401479d7d49
hj-core <88603338+hj-core@users.noreply.github.com>
2023-05-27 22:38:17 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1406/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1406/Solution.kt
@@ -8,11 +8,23 @@ class Solution {
      * Time O(N) and Space O(1) where N is the size of stoneValue;
      */
     fun stoneGameIII(stoneValue: IntArray): String {
+        val (aliceScore, bobScore) = scores(stoneValue)
+        return gameResult(aliceScore, bobScore)
+    }
+
+    private fun scores(stoneValue: IntArray): List<Int> {
+        /* Store the three most recent stoneValue suffix results, initialize with the
+         * results of stoneValue[size:], stoneValue[size+1:] and stoneValue[size+2:].
+         */
         val suffixResults = ArrayDeque<Int>().apply {
             addLast(0)
             addLast(0)
             addLast(0)
         }
+
+        /* The goal is to compute the result of stoneValue[0:], which is the score of Alice.
+         * We solve the result of suffix in an order of decreasing suffix start.
+         */
         var suffixSum = 0
         for (suffixStart in stoneValue.lastIndex downTo 0) {
             suffixSum += stoneValue[suffixStart]
@@ -21,9 +33,10 @@ class Solution {
             suffixResults.removeLast()
         }
 
+        // Return the score of Alice and Bob
         val aliceScore = suffixResults.first()
         val bobScore = suffixSum - aliceScore
-        return gameResult(aliceScore, bobScore)
+        return listOf(aliceScore, bobScore)
     }
 
     private fun gameResult(aliceScore: Int, bobScore: Int): String = when {
