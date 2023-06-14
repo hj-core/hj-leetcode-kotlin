2208de1fd62017fc3ae35d966c9a2ca3d85edc4b
hj-core <88603338+hj-core@users.noreply.github.com>
2023-06-14 10:41:58 +0800

    Add a solution to problem 530

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem530/Solution.kt
@@ -0,0 +1,37 @@
+package com.hj.leetcode.kotlin.problem530
+
+import com.hj.leetcode.kotlin.common.model.TreeNode
+
+/**
+ * LeetCode page: [530. Minimum Absolute Difference in BST](https://leetcode.com/problems/minimum-absolute-difference-in-bst/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(H) where N and H are the number of nodes and height of root;
+     */
+    fun getMinimumDifference(root: TreeNode?): Int {
+        checkNotNull(root)
+        var result = initialGuessResult(root)
+        var previousValue: Int? = null
+
+        root.inOrderTraversal { nodeValue ->
+            previousValue?.let {
+                result = minOf(result, nodeValue - it)
+            }
+            previousValue = nodeValue
+        }
+        return result
+    }
+
+    private fun initialGuessResult(root: TreeNode): Int = when {
+        root.left != null -> root.`val` - checkNotNull(root.left).`val`
+        root.right != null -> checkNotNull(root.right).`val` - root.`val`
+        else -> throw IllegalArgumentException()
+    }
+
+    private fun TreeNode.inOrderTraversal(onEachNode: (value: Int) -> Unit) {
+        left?.inOrderTraversal(onEachNode)
+        onEachNode(`val`)
+        right?.inOrderTraversal(onEachNode)
+    }
+}
