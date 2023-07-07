9a45aa573bed9fdb106ba9cdc6cc9b8d611a3f79
hj-core <88603338+hj-core@users.noreply.github.com>
2023-07-07 18:44:15 +0800

    Add a solution to problem 2024

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2024/Solution.kt
@@ -0,0 +1,37 @@
+package com.hj.leetcode.kotlin.problem2024
+
+/**
+ * LeetCode page: [2024. Maximize the Confusion of an Exam](https://leetcode.com/problems/maximize-the-confusion-of-an-exam/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the length of answerKey;
+     */
+    fun maxConsecutiveAnswers(answerKey: String, k: Int): Int {
+
+        /* Find the earliest starting index of the sub-array ending at each index that
+         * can form a consecutive answer after performing the operation at most k times.
+         * The result will be the maximum length among those sub-arrays.
+         */
+        var result = 0
+        var start = 0
+        var numAnswerT = 0
+        var numAnswerF = 0
+
+        for ((end, answer) in answerKey.withIndex()) {
+            when (answer) {
+                'T' -> numAnswerT++
+                'F' -> numAnswerF++
+            }
+            while (numAnswerF > k && numAnswerT > k) {
+                when (answerKey[start]) {
+                    'T' -> numAnswerT--
+                    'F' -> numAnswerF--
+                }
+                start++
+            }
+            result = maxOf(result, end - start + 1)
+        }
+        return result
+    }
+}
