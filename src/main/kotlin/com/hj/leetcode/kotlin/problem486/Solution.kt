f4f71953f032679ec5692137bf6e398790893329
hj-core <88603338+hj-core@users.noreply.github.com>
2023-07-28 18:29:24 +0800

    Add a solution to problem 486

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem486/Solution.kt
@@ -0,0 +1,59 @@
+package com.hj.leetcode.kotlin.problem486
+
+/**
+ * LeetCode page: [486. Predict the Winner](https://leetcode.com/problems/predict-the-winner/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N^2) and Space O(N) where N is the size of nums;
+     */
+    fun PredictTheWinner(nums: IntArray): Boolean {
+        if (nums.size.isEven()) {
+            return true
+        }
+
+        val prefixSum = nums.prefixSum()
+        val player1Score = optimalScore(nums, nums.indices, prefixSum)
+        val player2Score = prefixSum.last() - player1Score
+        return player1Score >= player2Score
+    }
+
+    private fun Int.isEven(): Boolean = this and 1 == 0
+
+    private fun IntArray.prefixSum(): IntArray {
+        val result = this.clone()
+        for (index in 1 until result.size) {
+            result[index] += result[index - 1]
+        }
+        return result
+    }
+
+    private fun optimalScore(
+        nums: IntArray,
+        indexRange: IntRange,
+        prefixSum: IntArray,
+        memoization: MutableMap<IntRange, Int> = hashMapOf()
+    ): Int {
+        if (indexRange in memoization) {
+            return checkNotNull(memoization[indexRange])
+        }
+        if (indexRange.last == indexRange.first) {
+            return nums[indexRange.first]
+        }
+
+        val result = sumRange(indexRange, prefixSum) - minOf(
+            optimalScore(nums, indexRange.first until indexRange.last, prefixSum, memoization),
+            optimalScore(nums, (indexRange.first + 1)..indexRange.last, prefixSum, memoization)
+        )
+        memoization[indexRange] = result
+        return result
+    }
+
+    private fun sumRange(indexRange: IntRange, prefixSum: IntArray): Int {
+        return if (indexRange.first > 0) {
+            prefixSum[indexRange.last] - prefixSum[indexRange.first - 1]
+        } else {
+            prefixSum[indexRange.last]
+        }
+    }
+}
