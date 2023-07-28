f8902df8854e1669e76dfb92e65229a2720354cc
hj-core <88603338+hj-core@users.noreply.github.com>
2023-07-29 00:00:02 +0800

    Revise the solution
    - Different sub problem is used to simply the solution

--- src/main/kotlin/com/hj/leetcode/kotlin/problem486/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem486/Solution.kt
@@ -11,49 +11,29 @@ class Solution {
         if (nums.size.isEven()) {
             return true
         }
-
-        val prefixSum = nums.prefixSum()
-        val player1Score = optimalScore(nums, nums.indices, prefixSum)
-        val player2Score = prefixSum.last() - player1Score
-        return player1Score >= player2Score
+        return maxScoreLead(nums) >= 0
     }
 
     private fun Int.isEven(): Boolean = this and 1 == 0
 
-    private fun IntArray.prefixSum(): IntArray {
-        val result = this.clone()
-        for (index in 1 until result.size) {
-            result[index] += result[index - 1]
-        }
-        return result
-    }
-
-    private fun optimalScore(
+    private fun maxScoreLead(
         nums: IntArray,
-        indexRange: IntRange,
-        prefixSum: IntArray,
+        gameRange: IntRange = nums.indices,
         memoization: MutableMap<IntRange, Int> = hashMapOf()
     ): Int {
-        if (indexRange in memoization) {
-            return checkNotNull(memoization[indexRange])
+        if (gameRange in memoization) {
+            return checkNotNull(memoization[gameRange])
         }
-        if (indexRange.last == indexRange.first) {
-            return nums[indexRange.first]
-        }
-
-        val result = sumRange(indexRange, prefixSum) - minOf(
-            optimalScore(nums, indexRange.first until indexRange.last, prefixSum, memoization),
-            optimalScore(nums, (indexRange.first + 1)..indexRange.last, prefixSum, memoization)
-        )
-        memoization[indexRange] = result
-        return result
-    }
-
-    private fun sumRange(indexRange: IntRange, prefixSum: IntArray): Int {
-        return if (indexRange.first > 0) {
-            prefixSum[indexRange.last] - prefixSum[indexRange.first - 1]
-        } else {
-            prefixSum[indexRange.last]
+        with(gameRange) {
+            if (first == last) {
+                return nums[first]
+            }
+            val result = maxOf(
+                nums[first] - maxScoreLead(nums, (first + 1)..last, memoization),
+                nums[last] - maxScoreLead(nums, first until last, memoization)
+            )
+            memoization[gameRange] = result
+            return result
         }
     }
 }
