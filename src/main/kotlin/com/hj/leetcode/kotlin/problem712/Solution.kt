49d53c9a39ed9d307d13b61cb71ddd521878f51d
hj-core <88603338+hj-core@users.noreply.github.com>
2023-08-01 10:29:01 +0800

    Improve wording of comments

--- src/main/kotlin/com/hj/leetcode/kotlin/problem712/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem712/Solution.kt
@@ -11,19 +11,19 @@ class Solution {
         // dp[lengthPrefix1][lengthPrefix2] ::= minimumDeleteSum(s1[:lengthPrefix1], s2[:lengthPrefix2])
         val dp = Array(s1.length + 1) { IntArray(s2.length + 1) }
 
-        // Base cases that all s1[:lengthPrefix1] is deleted since s2[:0] is empty
+        // Base cases that all characters of s1[:lengthPrefix1] are deleted since s2[:0] is empty
         for (lengthPrefix1 in 1..s1.length) {
             dp[lengthPrefix1][0] = s1[lengthPrefix1 - 1].ascii() + dp[lengthPrefix1 - 1][0]
         }
-        // Base cases that all s2[:lengthPrefix2] is deleted since s1[:0] is empty
+        // Base cases that all characters of s2[:lengthPrefix2] are deleted since s1[:0] is empty
         for (lengthPrefix2 in 1..s2.length) {
             dp[0][lengthPrefix2] = s2[lengthPrefix2 - 1].ascii() + dp[0][lengthPrefix2 - 1]
         }
 
         for (lengthPrefix1 in 1..s1.length) {
             for (lengthPrefix2 in 1..s2.length) {
-                /* If s1[lengthPrefix1-1] equals s2[lengthPrefix2-1], there is a minimumDeleteSum case
-                 * that keeps both characters; otherwise, one of the two characters must be deleted.
+                /* If s1[lengthPrefix1-1] and s2[lengthPrefix2-1] is the same, there is a minimumDeleteSum
+                 * case that keeps both characters; otherwise, one of the two characters must be deleted.
                  */
                 dp[lengthPrefix1][lengthPrefix2] = if (s1[lengthPrefix1 - 1] == s2[lengthPrefix2 - 1]) {
                     dp[lengthPrefix1 - 1][lengthPrefix2 - 1]
