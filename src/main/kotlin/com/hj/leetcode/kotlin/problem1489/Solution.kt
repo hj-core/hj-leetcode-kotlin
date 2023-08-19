4ebb00b150d33202e5443066108c595828986b7b
hj-core <88603338+hj-core@users.noreply.github.com>
2023-08-19 18:12:24 +0800

    Add a DRAFT solution to problem 1489

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1489/Solution.kt
@@ -0,0 +1,140 @@
+package com.hj.leetcode.kotlin.problem1489
+
+/**
+ * LeetCode page: [1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree](https://leetcode.com/problems/find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/);
+ */
+class Solution {
+    fun findCriticalAndPseudoCriticalEdges(n: Int, edges: Array<IntArray>): List<List<Int>> {
+        // Sort edges according to its weight in non-descending order;
+        val sortedEdges = sortedEdges(edges)
+
+        // Find the weight of MST;
+        val mstWeight = mstWeight(n, sortedEdges)
+
+        /* Find all critical edges:
+         * If excluding an edge resulting in an increasing on the MST weight or make the
+         * graph no longer connected, then it is a critical edge;
+         */
+        val criticalEdges = hashSetOf<Edge>()
+
+        for (excludedEdge in sortedEdges) {
+            var newMstWeight = 0
+            val unionFind = UnionFind(n)
+
+            for (edge in sortedEdges) {
+                if (edge == excludedEdge) {
+                    continue
+                }
+
+                if (unionFind.union(edge.u, edge.v)) {
+                    newMstWeight += edge.weight
+                }
+            }
+
+            if (unionFind.numComponents > 1 || newMstWeight > mstWeight) {
+                criticalEdges.add(excludedEdge)
+            }
+        }
+
+        /* Find all non-critical edges:
+         * If including an edge resulting in an increasing on the MST weight, then it is
+         * a non-critical edge;
+         */
+        val nonCriticalEdges = hashSetOf<Edge>()
+
+        for (includedEdge in sortedEdges) {
+            if (includedEdge in criticalEdges) {
+                continue
+            }
+
+            var newMstWeight = 0
+            val unionFind = UnionFind(n)
+
+            newMstWeight += includedEdge.weight
+            unionFind.union(includedEdge.u, includedEdge.v)
+
+            for (edge in sortedEdges) {
+                if (unionFind.union(edge.u, edge.v)) {
+                    newMstWeight += edge.weight
+                }
+            }
+
+            if (newMstWeight > mstWeight) {
+                nonCriticalEdges.add(includedEdge)
+            }
+        }
+
+        // Find all pseudo-critical edges:
+        // If an edge is neither critical nor non-critical, then it is a pseudo-critical edge;
+
+        // Return the result;
+        val criticalEdgeIndices = mutableListOf<Int>()
+        val pseudoCriticalEdgeIndices = mutableListOf<Int>()
+
+        for (edge in sortedEdges) {
+            when (edge) {
+                in criticalEdges -> criticalEdgeIndices.add(edge.index)
+                in nonCriticalEdges -> {}
+                else -> pseudoCriticalEdgeIndices.add(edge.index)
+            }
+        }
+        return listOf(criticalEdgeIndices, pseudoCriticalEdgeIndices)
+    }
+
+    private fun sortedEdges(edges: Array<IntArray>): List<Edge> {
+        val result = edges.mapIndexedTo(mutableListOf()) { index, array ->
+            Edge(array[0], array[1], array[2], index)
+        }
+        result.sortBy { edge -> edge.weight }
+        return result
+    }
+
+    private data class Edge(val u: Int, val v: Int, val weight: Int, val index: Int)
+
+    private fun mstWeight(n: Int, sortedEdges: List<Edge>): Int {
+        var result = 0
+        val unionFind = UnionFind(n)
+
+        for (edge in sortedEdges) {
+            if (unionFind.union(edge.u, edge.v)) {
+                result += edge.weight
+            }
+        }
+        return result
+    }
+
+    private class UnionFind(size: Int) {
+
+        private val parents = IntArray(size) { it }
+        private val ranks = IntArray(size)
+        var numComponents = size
+            private set
+
+        fun union(x: Int, y: Int): Boolean {
+            val xRoot = find(x)
+            val yRoot = find(y)
+
+            if (xRoot == yRoot) {
+                return false
+            }
+
+            when {
+                ranks[xRoot] < ranks[yRoot] -> parents[xRoot] = yRoot
+                ranks[yRoot] < ranks[xRoot] -> parents[yRoot] = xRoot
+                else -> {
+                    parents[yRoot] = xRoot
+                    ranks[xRoot]++
+                }
+            }
+            numComponents--
+            return true
+        }
+
+        private fun find(x: Int): Int {
+            if (parents[x] != x) {
+                parents[x] = find(parents[x])
+            }
+            return parents[x]
+        }
+    }
+}
