3d19d13d828501db1a2f5f70a5609dbdc284de85
hj-core <88603338+hj-core@users.noreply.github.com>
2023-09-20 20:36:53 +0800

    Add solutions to problem 287 (#151)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem287/Solution.kt
@@ -0,0 +1,27 @@
+package com.hj.leetcode.kotlin.problem287
+
+/**
+ * LeetCode page: [287. Find the Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(NLogN) and Space O(1) where N is the size of nums;
+     */
+    fun findDuplicate(nums: IntArray): Int {
+        var left = 1
+        var right = nums.size - 1
+
+        while (left < right) {
+            val mid = left + (right - left) / 2
+            val isResultInLeftToMid =
+                mid - left + 1 < nums.count { it in left..mid }
+
+            if (isResultInLeftToMid) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return left
+    }
+}
