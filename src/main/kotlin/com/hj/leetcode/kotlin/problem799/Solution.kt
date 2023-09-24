332a7e779f1ab385e647aa9c6e4f84aee61c6729
hj-core <88603338+hj-core@users.noreply.github.com>
2023-09-24 14:24:40 +0800

    Improve space complexity

--- src/main/kotlin/com/hj/leetcode/kotlin/problem799/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem799/Solution.kt
@@ -5,11 +5,10 @@ package com.hj.leetcode.kotlin.problem799
  */
 class Solution {
     /* Complexity:
-     * Time O(query_row^2) and Space O(totalRows);
+     * Time O(query_row^2) and Space O(query_row);
      */
     fun champagneTower(poured: Int, query_row: Int, query_glass: Int): Double {
-        val totalRows = 100
-        val dp = DoubleArray(totalRows)
+        val dp = DoubleArray(query_row + 1)
         dp[0] = poured.toDouble()
 
         repeat(query_row) { previousRow ->
