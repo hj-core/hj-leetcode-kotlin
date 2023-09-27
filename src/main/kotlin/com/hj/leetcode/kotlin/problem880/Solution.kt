495ac9a113613665c61a6a0630c12b2b984e5179
hj-core <88603338+hj-core@users.noreply.github.com>
2023-09-27 10:24:01 +0800

    Add a DRAFT solution to problem 880

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem880/Solution.kt
@@ -0,0 +1,47 @@
+package com.hj.leetcode.kotlin.problem880
+
+/**
+ * LeetCode page: [880. Decoded String at Index](https://leetcode.com/problems/decoded-string-at-index/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the length of s;
+     */
+    fun decodeAtIndex(s: String, k: Int): String {
+        return charAtLength(s, s.lastIndex, decodedLength(s), k.toLong()).toString()
+    }
+
+    private fun charAtLength(
+        s: String,
+        index: Int,
+        decodedLength: Long,
+        targetLength: Long
+    ): Char {
+        if (s[index] in '2'..'9') {
+            val newDecodedLength = decodedLength / (s[index] - '0')
+            val newTargetLength = (targetLength % newDecodedLength).let {
+                if (it == 0L) newDecodedLength else it
+            }
+            return charAtLength(s, index - 1, newDecodedLength, newTargetLength)
+        }
+
+        if (targetLength == decodedLength) {
+            return s[index]
+        }
+
+        return charAtLength(s, index - 1, decodedLength - 1, targetLength)
+    }
+
+    private fun decodedLength(s: String): Long {
+        var result = 0L
+
+        for (char in s) {
+            if (char in '2'..'9') {
+                result *= char - '0'
+            } else {
+                result += 1
+            }
+        }
+        return result
+    }
+}
