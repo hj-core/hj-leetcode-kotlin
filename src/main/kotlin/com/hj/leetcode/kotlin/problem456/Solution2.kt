5962a26fe982de7eed0f7fe5013f2d2f31bb80e8
hj-core <88603338+hj-core@users.noreply.github.com>
2023-09-30 22:14:37 +0800

    Add a trick one-pass solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem456/Solution2.kt
@@ -0,0 +1,32 @@
+package com.hj.leetcode.kotlin.problem456
+
+/**
+ * LeetCode page: [456. 132 Pattern](https://leetcode.com/problems/132-pattern/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the size of nums;
+     */
+    fun find132pattern(nums: IntArray): Boolean {
+        val stack = mutableListOf<Int>() // monotonically decreasing
+        var two = Int.MIN_VALUE
+
+        for (index in nums.indices.reversed()) {
+            val potentialOne = nums[index]
+
+            /* It is guaranteed that there is a three in the stack which is
+             * greater than two, so if potentialOne is less than two, we find
+             * a valid one-three-two pattern.
+             */
+            if (potentialOne < two) {
+                return true
+            }
+
+            while (stack.isNotEmpty() && stack.last() < potentialOne) {
+                two = stack.removeAt(stack.lastIndex)
+            }
+            stack.add(potentialOne)
+        }
+        return false
+    }
+}
