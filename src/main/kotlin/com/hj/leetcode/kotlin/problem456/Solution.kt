c124f6a931524ad3bc4f4dfdd3b52dd4e56ad149
hj-core <88603338+hj-core@users.noreply.github.com>
2023-09-30 20:46:15 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem456/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem456/Solution.kt
@@ -8,22 +8,25 @@ class Solution {
      * Time O(N) and Space O(N) where N is the size of nums;
      */
     fun find132pattern(nums: IntArray): Boolean {
-        val prefixMin = nums.runningReduce { acc: Int, i: Int -> minOf(acc, i) }
-        val stack = mutableListOf<Int>()
+        val prefixMin = nums.runningReduce { acc, i -> minOf(acc, i) }
+        val twoStack = mutableListOf<Int>() // monotonically decreasing
 
         for (index in nums.lastIndex downTo 1) {
-            if (nums[index] <= prefixMin[index - 1]) {
+            val three = nums[index]
+            val one = prefixMin[index - 1]
+
+            if (three <= one) {
                 continue
             }
 
-            while (stack.isNotEmpty() && stack.last() <= prefixMin[index - 1]) {
-                stack.removeAt(stack.lastIndex)
+            while (twoStack.isNotEmpty() && twoStack.last() <= one) {
+                twoStack.removeAt(twoStack.lastIndex)
             }
 
-            if (stack.isNotEmpty() && stack.last() < nums[index]) {
+            if (twoStack.isNotEmpty() && twoStack.last() < three) {
                 return true
             }
-            stack.add(nums[index])
+            twoStack.add(three)
         }
         return false
     }
