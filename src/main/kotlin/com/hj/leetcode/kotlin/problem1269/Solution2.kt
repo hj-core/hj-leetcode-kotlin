ecb8ba251c1e5fda8238c83dd7f1f85956da2191
hj-core <88603338+hj-core@users.noreply.github.com>
2023-10-15 16:17:49 +0800

    Add a bottom-up solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1269/Solution2.kt
@@ -0,0 +1,30 @@
+package com.hj.leetcode.kotlin.problem1269
+
+/**
+ * LeetCode page: [1269. Number of Ways to Stay in the Same Place After Some Steps](https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(step^2) and Space O(step);
+     */
+    fun numWays(steps: Int, arrLen: Int): Int {
+        val modulo = 1_000_000_007
+        /* dp[i]@step_j::=
+         * number of ways back to index 0 from index_i using j steps
+         */
+        val dp = IntArray(minOf(arrLen, steps + 1))
+        dp[0] = 1 // base case dp[0]@step_0
+
+        for (numSteps in 1..steps) {
+            var waysFromLeft = 0
+            for (i in 0..minOf(numSteps, dp.lastIndex)) {
+                var ways = waysFromLeft
+                ways = (ways + dp[i]) % modulo
+                ways = (ways + dp.getOrElse(i + 1) { 0 }) % modulo
+                waysFromLeft = dp[i]
+                dp[i] = ways
+            }
+        }
+        return dp[0]
+    }
+}
