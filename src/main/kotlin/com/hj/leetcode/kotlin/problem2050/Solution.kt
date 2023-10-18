de14883993a3f9135462802b037ba6d6a68c718d
hj-core <88603338+hj-core@users.noreply.github.com>
2023-10-19 02:46:40 +0800

    Replace the solution by DP approach

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2050/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2050/Solution.kt
@@ -5,19 +5,20 @@ package com.hj.leetcode.kotlin.problem2050
  */
 class Solution {
     /* Complexity:
-     * Time O(n+E) and Space O(n+E) where E is the size of relations;
+     * Time O(n+E) and Space O(n+E);
      */
     fun minimumTime(n: Int, relations: Array<IntArray>, time: IntArray): Int {
-        // prerequisites[i]::= the prerequisites of course i
-        val prerequisites = prerequisites(n, relations)
-        // nextCourses[i]::= the courses with course i as a prerequisite
-        val nextCourses = nextCourses(n, relations)
+        // prerequisites[i]::= the prerequisites of the course i
+        val prerequisites = coursePrerequisites(n, relations)
+        // memoization[i]::= the earliest finish time of the course i
+        val memoization = hashMapOf<Int, Int>()
 
-        val sortedCourses = topologicallySortedCourses(n, prerequisites)
-        return earliestFinishTimes(n, time, nextCourses, sortedCourses).max()
+        return (1..n).maxOf { course ->
+            earliestFinishTime(course, time, prerequisites, memoization)
+        }
     }
 
-    private fun prerequisites(n: Int, relations: Array<IntArray>): List<List<Int>> {
+    private fun coursePrerequisites(n: Int, relations: Array<IntArray>): List<List<Int>> {
         val result = List(n + 1) { mutableListOf<Int>() }
         for ((prerequisite, course) in relations) {
             result[course].add(prerequisite)
@@ -25,67 +26,22 @@ class Solution {
         return result
     }
 
-    private fun nextCourses(n: Int, relations: Array<IntArray>): List<List<Int>> {
-        val result = List(n + 1) { mutableListOf<Int>() }
-        for ((prerequisite, course) in relations) {
-            result[prerequisite].add(course)
-        }
-        return result
-    }
-
-    private fun topologicallySortedCourses(n: Int, prerequisites: List<List<Int>>): List<Int> {
-        val result = ArrayDeque<Int>()
-        val visited = BooleanArray(n + 1)
-        for (course in 1..n) {
-            if (visited[course]) {
-                continue
-            }
-            visited[course] = true
-            dfs(course, prerequisites, visited) {
-                result.addFirst(it)
-            }
-        }
-        return result.toList()
-    }
-
-    private fun dfs(
+    private fun earliestFinishTime(
         course: Int,
-        prerequisites: List<List<Int>>,
-        visited: BooleanArray,
-        onCompletion: (course: Int) -> Unit,
-    ) {
-        check(visited[course])
-
-        for (prerequisite in prerequisites[course]) {
-            if (visited[prerequisite]) {
-                continue
-            }
-            visited[prerequisite] = true
-            dfs(prerequisite, prerequisites, visited, onCompletion)
-        }
-        onCompletion(course)
-    }
-
-    private fun earliestFinishTimes(
-        n: Int,
         time: IntArray,
-        nextCourses: List<List<Int>>,
-        sortedCourses: List<Int>, // In topological order
-    ): IntArray {
-        val result = IntArray(n + 1).apply {
-            for (i in 1..<size) {
-                this[i] = time[i - 1]
-            }
+        prerequisites: List<List<Int>>,
+        memoization: MutableMap<Int, Int>,
+    ): Int {
+        if (course in memoization) {
+            return checkNotNull(memoization[course])
         }
 
-        for (course in sortedCourses.asReversed()) {
-            for (nextCourse in nextCourses[course]) {
-                val finishTime = result[course] + time[nextCourse - 1]
-                if (result[nextCourse] < finishTime) {
-                    result[nextCourse] = finishTime
-                }
-            }
-        }
+        val earliestStartTime = prerequisites[course].maxOfOrNull {
+            earliestFinishTime(it, time, prerequisites, memoization)
+        } ?: 0
+
+        val result = earliestStartTime + time[course - 1]
+        memoization[course] = result
         return result
     }
 }
