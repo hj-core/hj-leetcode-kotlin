801bb72ecf5525c3e003e5351688569483ec868c
hj-core <88603338+hj-core@users.noreply.github.com>
2023-11-19 20:05:45 +0800

    Add a solution to problem 1887

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1887/Solution.kt
@@ -0,0 +1,23 @@
+package com.hj.leetcode.kotlin.problem1887
+
+/**
+ * LeetCode page: [1887. Reduction Operations to Make the Array Elements Equal](https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(NLogN) and Space O(N) where N is the size of nums;
+     */
+    fun reductionOperations(nums: IntArray): Int {
+        val sorted = nums.sorted()
+        var result = 0
+        var localRequiredOps = 0
+
+        for (index in 1..<sorted.size) {
+            if (sorted[index] != sorted[index - 1]) {
+                localRequiredOps++
+            }
+            result += localRequiredOps
+        }
+        return result
+    }
+}
