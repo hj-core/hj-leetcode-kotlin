b7ee52dc3d7cdf125205549af01127b5345cc7d7
hj-core <88603338+hj-core@users.noreply.github.com>
2023-12-02 00:02:44 +0800

    Refactor the customary iterator through sequence api

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1662/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1662/Solution.kt
@@ -9,8 +9,8 @@ class Solution {
      * word1 and word2, respectively;
      */
     fun arrayStringsAreEqual(word1: Array<String>, word2: Array<String>): Boolean {
-        val iterator1 = word1.flattenedIterator()
-        val iterator2 = word2.flattenedIterator()
+        val iterator1 = word1.asSequence().flatMap { it.asSequence() }.iterator()
+        val iterator2 = word2.asSequence().flatMap { it.asSequence() }.iterator()
 
         while (iterator1.hasNext() && iterator2.hasNext()) {
             if (iterator1.next() != iterator2.next()) {
@@ -18,21 +18,5 @@ class Solution {
             }
         }
         return iterator1.hasNext() == iterator2.hasNext()
-    }
-
-    private fun Array<String>.flattenedIterator(): Iterator<Char> = object : Iterator<Char> {
-        private var arrayIterator = this@flattenedIterator.iterator()
-        private var stringIterator: Iterator<Char>? = null
-
-        override fun hasNext(): Boolean {
-            return arrayIterator.hasNext() || (stringIterator?.hasNext() == true)
-        }
-
-        override fun next(): Char {
-            if (stringIterator?.hasNext() != true) {
-                stringIterator = arrayIterator.next().iterator()
-            }
-            return checkNotNull(stringIterator).next()
-        }
     }
 }
