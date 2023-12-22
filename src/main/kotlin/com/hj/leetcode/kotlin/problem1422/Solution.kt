04f6d565291403c9324df5f835599a2cc27bed9e
hj-core <88603338+hj-core@users.noreply.github.com>
2023-12-22 16:57:26 +0800

    Add solutions to problem1422 (#213)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1422/Solution.kt
@@ -0,0 +1,26 @@
+package com.hj.leetcode.kotlin.problem1422
+
+import kotlin.math.max
+
+/**
+ * LeetCode page: [1422. Maximum Score After Splitting a String](https://leetcode.com/problems/maximum-score-after-splitting-a-string/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the length of s;
+     */
+    fun maxScore(s: String): Int {
+        var leftZeros = 0
+        var rightOnes = s.count { it == '1' }
+        var result = -1
+        for (index in 0..<s.lastIndex) {
+            if (s[index] == '0') {
+                leftZeros++
+            } else {
+                rightOnes--
+            }
+            result = max(result, leftZeros + rightOnes)
+        }
+        return result
+    }
+}
---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1422/Solution2.kt
@@ -0,0 +1,29 @@
+package com.hj.leetcode.kotlin.problem1422
+
+import kotlin.math.max
+
+/**
+ * LeetCode page: [1422. Maximum Score After Splitting a String](https://leetcode.com/problems/maximum-score-after-splitting-a-string/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the length of s;
+     */
+    fun maxScore(s: String): Int {
+        var leftZeros = 0
+        var leftOnes = 0
+        var totalOnes = 0
+        var partialResult = -1
+        for (index in 0..<s.lastIndex) {
+            if (s[index] == '0') {
+                leftZeros++
+            } else {
+                leftOnes++
+                totalOnes++
+            }
+            partialResult = max(partialResult, leftZeros - leftOnes)
+        }
+        totalOnes += if (s.last() == '1') 1 else 0
+        return partialResult + totalOnes
+    }
+}
