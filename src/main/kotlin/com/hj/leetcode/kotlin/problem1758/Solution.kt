befda37d47a5d60fbf7880c29746a62b7269f8be
hj-core <88603338+hj-core@users.noreply.github.com>
2023-12-24 10:17:58 +0800

    compute pattern01 from pattern10

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1758/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1758/Solution.kt
@@ -11,13 +11,13 @@ class Solution {
      */
     fun minOperations(s: String): Int {
         var pattern10 = 0
-        var pattern01 = 0
         for ((index, c) in s.withIndex()) {
             val digit = c.digitToInt()
             val indicator = digit xor (index and 1)
             pattern10 += indicator
-            pattern01 += 1 - indicator
+            // pattern01 += 1 - indicator
         }
+        val pattern01 = s.length - pattern10
         return min(pattern10, pattern01)
     }
 }
