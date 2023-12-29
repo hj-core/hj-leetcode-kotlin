5b747f802d7d467817c382ffa7df41998a72ec7b
hj-core <88603338+hj-core@users.noreply.github.com>
2023-12-29 13:10:08 +0800

    Simplify the implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1335/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1335/Solution.kt
@@ -1,5 +1,8 @@
 package com.hj.leetcode.kotlin.problem1335
 
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * LeetCode page: [1335. Minimum Difficulty of a Job Schedule](https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/);
  *
@@ -7,53 +10,50 @@ package com.hj.leetcode.kotlin.problem1335
  */
 class Solution {
     /* Complexity:
-     * Time O(N^2 * d) and Space O(N) where N is the size of jobDifficulty;
+     * Time O(N^2 * d) and Space O(N * d) where N is the size of jobDifficulty;
      */
     fun minDifficulty(jobDifficulty: IntArray, d: Int): Int {
-        val noEnoughJobs = jobDifficulty.size < d
-        if (noEnoughJobs) return -1
-
-        val dp = containerOfSubArrayMinDifficultyPerEndIndex(jobDifficulty.size)
-        updateSubArrayMinDifficultyPerEndIndex(dp, d, jobDifficulty)
-
-        return dp[jobDifficulty.lastIndex]
-    }
-
-    private fun containerOfSubArrayMinDifficultyPerEndIndex(totalJobs: Int): IntArray {
-        return IntArray(totalJobs)
-    }
-
-    private fun updateSubArrayMinDifficultyPerEndIndex(container: IntArray, totalDays: Int, jobDifficulty: IntArray) {
-        updateForSingleDayCase(container, jobDifficulty)
-        furtherUpdateToTotalDays(container, jobDifficulty, totalDays)
-    }
-
-    private fun updateForSingleDayCase(container: IntArray, jobDifficulty: IntArray) {
-        container[0] = jobDifficulty[0]
-
-        for (index in 1..container.lastIndex) {
-            container[index] = maxOf(container[index - 1], jobDifficulty[index])
+        if (jobDifficulty.size < d) {
+            return -1
         }
+        return minDifficultyDp(jobDifficulty, 0, d, hashMapOf())
     }
 
-    private fun furtherUpdateToTotalDays(container: IntArray, jobDifficulty: IntArray, totalDays: Int) {
-        for (currDays in 1 until totalDays) {
-
-            for (endIndex in container.lastIndex downTo currDays) {
-                var newDayMaxDifficulty = jobDifficulty[endIndex]
-                var currDayPrevIndexMinDifficulty = container[endIndex - 1]
-                var minDifficulty = newDayMaxDifficulty + currDayPrevIndexMinDifficulty
-
-                for (index in endIndex - 1 downTo currDays) {
-                    newDayMaxDifficulty = maxOf(newDayMaxDifficulty, jobDifficulty[index])
-                    currDayPrevIndexMinDifficulty = container[index - 1]
-                    minDifficulty = minOf(minDifficulty, newDayMaxDifficulty + currDayPrevIndexMinDifficulty)
-                }
-
-                container[endIndex] = minDifficulty
-            }
-
-            container[currDays - 1] = -1
+    private fun minDifficultyDp(
+        jobDifficulty: IntArray,
+        start: Int,
+        d: Int,
+        memoization: MutableMap<DpState, Int>,
+    ): Int {
+        if (start + d > jobDifficulty.size) {
+            throw IllegalArgumentException()
         }
+
+        val state = DpState(start, d)
+        if (state in memoization) {
+            return checkNotNull(memoization[state])
+        }
+        if (d == 1) {
+            val result = (start..<jobDifficulty.size).maxOf { jobDifficulty[it] }
+            memoization[state] = result
+            return result
+        }
+
+        var result = Int.MAX_VALUE
+        var firstDayDifficulty = jobDifficulty[start]
+        for (firstDayEnd in start..<jobDifficulty.size - (d - 1)) {
+            firstDayDifficulty = max(firstDayDifficulty, jobDifficulty[firstDayEnd])
+            val currentMinDifficulty = firstDayDifficulty + minDifficultyDp(
+                jobDifficulty,
+                firstDayEnd + 1,
+                d - 1,
+                memoization
+            )
+            result = min(result, currentMinDifficulty)
+        }
+        memoization[state] = result
+        return result
     }
+
+    private data class DpState(val start: Int, val d: Int)
 }
