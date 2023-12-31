a4641e2bf62f1ed119889d79b11ad548ebce1051
hj-core <88603338+hj-core@users.noreply.github.com>
2023-12-31 12:54:15 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1624/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1624/Solution.kt
@@ -1,7 +1,5 @@
 package com.hj.leetcode.kotlin.problem1624
 
-import kotlin.math.max
-
 /**
  * LeetCode page: [1624. Largest Substring Between Two Equal Characters](https://leetcode.com/problems/largest-substring-between-two-equal-characters/);
  */
@@ -10,16 +8,17 @@ class Solution {
      * Time O(N) and Space O(1) where N is the length of s;
      */
     fun maxLengthBetweenEqualCharacters(s: String): Int {
-        val charFirstIndex = IntArray(26) { -1 }
-        var result = -1
+        val charsFirstIndex = IntArray(26) { -1 }
+        val charsLastIndex = IntArray(26) { -1 }
         for ((index, char) in s.withIndex()) {
-            val first = charFirstIndex[char - 'a']
-            if (first == -1) {
-                charFirstIndex[char - 'a'] = index
-            } else {
-                result = max(result, index - first - 1)
+            if (charsFirstIndex[char - 'a'] == -1) {
+                charsFirstIndex[char - 'a'] = index
             }
+            charsLastIndex[char - 'a'] = index
         }
-        return result
+
+        return charsLastIndex.indices.maxOf {
+            charsLastIndex[it] - charsFirstIndex[it] - 1
+        }
     }
 }
