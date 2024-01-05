41a72f70ab47903e61114360fafcf2a77cb6a187
hj-core <88603338+hj-core@users.noreply.github.com>
2024-01-05 21:13:49 +0800

    Add a solution to problem 300

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem300/Solution.kt
@@ -0,0 +1,30 @@
+package com.hj.leetcode.kotlin.problem300
+
+/**
+ * LeetCode page: [300. Longest Increasing Subsequence](https://leetcode.com/problems/longest-increasing-subsequence/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(NLogN) and Space O(N) where N is the size of nums;
+     */
+    fun lengthOfLIS(nums: IntArray): Int {
+        /* minTails[i]::= the minimum tail among the strictly increasing
+         * subsequences with a length of i+1;
+         */
+        val minTails = mutableListOf<Int>()
+        for (num in nums) {
+            val bsResult = minTails.binarySearch(num)
+            if (0 <= bsResult) {
+                continue
+            }
+
+            val insertionIndex = -(bsResult + 1)
+            if (insertionIndex == minTails.size) {
+                minTails.add(num)
+            } else {
+                minTails[insertionIndex] = num
+            }
+        }
+        return minTails.size
+    }
+}
