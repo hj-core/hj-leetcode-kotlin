6980e9ba95e1cf2580a0abb253ae1cac4cb2b9d8
hj-core <88603338+hj-core@users.noreply.github.com>
2024-01-19 21:44:32 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem931/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem931/Solution.kt
@@ -5,21 +5,22 @@ package com.hj.leetcode.kotlin.problem931
  */
 class Solution {
     /* Complexity:
-     * Time O(MN) and Space O(N) where M and N are the number of rows and columns of matrix;
+     * Time O(MN) and Space O(N) where M and N are the number of rows and
+     * columns of matrix;
      */
     fun minFallingPathSum(matrix: Array<IntArray>): Int {
         // dp[c]@r::= the minimum falling path sum to matrix[r][c];
         val dp = matrix[0].clone()
 
         for (r in 1..<matrix.size) {
-            var upperLeftDp = dp[0]
+            var leftLastDp = Int.MAX_VALUE
             for (c in matrix[r].indices) {
                 val result = matrix[r][c] + minOf(
-                    upperLeftDp,
+                    leftLastDp,
                     dp[c],
-                    dp.getOrElse(c + 1) { dp[c] }
+                    dp.getOrElse(c + 1) { Int.MAX_VALUE }
                 )
-                upperLeftDp = dp[c]
+                leftLastDp = dp[c]
                 dp[c] = result
             }
         }
