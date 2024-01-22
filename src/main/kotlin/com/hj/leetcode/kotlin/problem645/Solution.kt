2901c3bb099d0510fbb660e347bcbb6e3b5bfd42
hj-core <88603338+hj-core@users.noreply.github.com>
2024-01-22 20:43:44 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem645/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem645/Solution.kt
@@ -8,47 +8,26 @@ class Solution {
      * Time O(N) and Space O(1) where N is the size of nums;
      */
     fun findErrorNums(nums: IntArray): IntArray {
-        val repetitionXorLoss = getRepetitionXorLoss(nums)
-
-        // bitmask that can distinguish between repetition and loss
-        val distinguishMask = repetitionXorLoss.rightmostBit()
-        val candidates = findCandidatesOfRepetitionAndLoss(nums, distinguishMask)
-        return determineRepetitionAndLoss(nums, candidates)
+        val (a, b) = errorsUnordered(nums)
+        return if (a in nums) intArrayOf(a, b) else intArrayOf(b, a)
     }
 
-    private fun getRepetitionXorLoss(nums: IntArray): Int {
+    private fun errorsUnordered(nums: IntArray): Pair<Int, Int> {
+        val xorOfErrors = xorOfErrors(nums)
+        val bitMask = xorOfErrors.rightmostBit()
+        val xorMasked = { acc: Int, i: Int ->
+            if (i and bitMask == 0) acc xor i else acc
+        }
+        val aError = (nums.fold(0, xorMasked)
+                xor (1..nums.size).fold(0, xorMasked))
+        return Pair(aError, aError xor xorOfErrors)
+    }
+
+    private fun xorOfErrors(nums: IntArray): Int {
         val xorOperation = { a: Int, b: Int -> a xor b }
         return (nums.reduce(xorOperation)
                 xor (1..nums.size).reduce(xorOperation))
     }
 
     private fun Int.rightmostBit() = this - (this and (this - 1))
-
-    private fun findCandidatesOfRepetitionAndLoss(nums: IntArray, distinguishMask: Int): Pair<Int, Int> {
-        var candidate1 = 0
-        var candidate2 = 0
-
-        for (num in nums) {
-            if (num and distinguishMask == 0) {
-                candidate1 = candidate1 xor num
-            } else {
-                candidate2 = candidate2 xor num
-            }
-        }
-
-        for (num in 1..nums.size) {
-            if (num and distinguishMask == 0) {
-                candidate1 = candidate1 xor num
-            } else {
-                candidate2 = candidate2 xor num
-            }
-        }
-
-        return Pair(candidate1, candidate2)
-    }
-
-    private fun determineRepetitionAndLoss(nums: IntArray, candidates: Pair<Int, Int>): IntArray {
-        val (a, b) = candidates
-        return if (a in nums) intArrayOf(a, b) else intArrayOf(b, a)
-    }
 }
