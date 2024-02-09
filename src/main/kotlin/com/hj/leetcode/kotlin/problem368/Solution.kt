ef7214d2a1338677e757db5b02ca349212bb6d43
hj-core <88603338+hj-core@users.noreply.github.com>
2024-02-09 19:05:39 +0800

    Add a solution to problem 368

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem368/Solution.kt
@@ -0,0 +1,47 @@
+package com.hj.leetcode.kotlin.problem368
+
+/**
+ * LeetCode page: [368. Largest Divisible Subset](https://leetcode.com/problems/largest-divisible-subset/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N^2) and Space O(N) where N is the size of nums;
+     */
+    fun largestDivisibleSubset(nums: IntArray): List<Int> {
+        val sorted = nums.sorted()
+        /* dp[i]::= the (size, index in sorted of the second-largest element) of the
+         * largest divisible subset that uses the elements in sorted.slice(0..i) and
+         * includes the number sorted[i].
+         */
+        val dp = buildDp(sorted)
+
+        return buildResult(dp, sorted)
+    }
+
+    private fun buildDp(sorted: List<Int>): Array<Properties> {
+        val result = Array(sorted.size) { Properties(1, -1) }
+
+        for (i in result.indices) {
+            var j = i - 1
+            while (j + 2 > result[i].size) {
+                if (sorted[i] % sorted[j] == 0 && 1 + result[j].size > result[i].size) {
+                    result[i] = Properties(1 + result[j].size, j)
+                }
+                j -= 1
+            }
+        }
+        return result
+    }
+
+    private data class Properties(val size: Int, val prevIndex: Int)
+
+    private fun buildResult(dp: Array<Properties>, sorted: List<Int>): MutableList<Int> {
+        val result = mutableListOf<Int>()
+        var prevIndex = dp.indices.maxBy { dp[it].size }
+        while (prevIndex != -1) {
+            result.add(sorted[prevIndex])
+            prevIndex = dp[prevIndex].prevIndex
+        }
+        return result
+    }
+}
