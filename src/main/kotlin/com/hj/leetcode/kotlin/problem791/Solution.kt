1ce7090ae6e80570cf3036ccc6707f2dd5aaa2ef
hj-core <88603338+hj-core@users.noreply.github.com>
2024-03-12 17:23:48 +0800

    Add a solution to problem 791 (#292)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem791/Solution.kt
@@ -0,0 +1,27 @@
+package com.hj.leetcode.kotlin.problem791
+
+/**
+ * LeetCode page: [791. Custom Sort String](https://leetcode.com/problems/custom-sort-string/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(M+N) and Space O(N) where M is the length of order and
+     * N is the length of s;
+     */
+    fun customSortString(order: String, s: String): String {
+        val charFreq = s.groupingBy { it }.eachCountTo(hashMapOf())
+        val result = StringBuilder(s.length)
+
+        // Counting sort
+        for (char in order) {
+            charFreq[char]?.let {
+                repeat(it) { result.append(char) }
+                charFreq.remove(char)
+            }
+        }
+        for ((char, count) in charFreq) {
+            repeat(count) { result.append(char) }
+        }
+        return result.toString()
+    }
+}
