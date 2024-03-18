5453e557aa137310f169ea0893dbb245d1728f34
hj-core <88603338+hj-core@users.noreply.github.com>
2024-03-18 18:01:14 +0800

    Add another greedy solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem452/Solution2.kt
@@ -0,0 +1,34 @@
+package com.hj.leetcode.kotlin.problem452
+
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * LeetCode page: [452. Minimum Number of Arrows to Burst Balloons](https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(NLogN) and Space O(N) where N is the size of points;
+     */
+    fun findMinArrowShots(points: Array<IntArray>): Int {
+        val sortedRanges = points.sortedBy { it[0] }
+        var result = 0
+        var arrowRange = intArrayOf(0, -1)
+
+        for (range in sortedRanges) {
+            arrowRange = intersect(arrowRange, range)
+            if (arrowRange[1] < arrowRange[0]) {
+                result += 1
+                arrowRange = range
+            }
+        }
+        return result
+    }
+
+    private fun intersect(range1: IntArray, range2: IntArray): IntArray {
+        return intArrayOf(
+            max(range1[0], range2[0]),
+            min(range1[1], range2[1])
+        )
+    }
+}
