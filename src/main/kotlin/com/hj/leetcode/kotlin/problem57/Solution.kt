2a220cef44c555fa33a81b4fc108c1d6e3fc9da9
hj-core <88603338+hj-core@users.noreply.github.com>
2024-03-18 16:44:00 +0800

    Update problem 57: Simplify solution (#297)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem57/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem57/Solution.kt
@@ -1,5 +1,8 @@
 package com.hj.leetcode.kotlin.problem57
 
+import kotlin.math.max
+import kotlin.math.min
+
 /**
  * LeetCode page: [57. Insert Interval](https://leetcode.com/problems/insert-interval/);
  */
@@ -8,113 +11,67 @@ class Solution {
      * Time O(N) and Space O(N) where N is the size of intervals;
      */
     fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
-        if (intervals.isEmpty()) return arrayOf(newInterval)
-        val indexMergeStart = findIndexAtMergeStart(intervals, newInterval)
-        val indexMergeEnded = findIndexAtMergeEnded(intervals, newInterval)
-        return buildIntervalsAfterInsertion(intervals, newInterval, indexMergeStart, indexMergeEnded)
-    }
+        if (intervals.isEmpty()) {
+            return arrayOf(newInterval)
+        }
+        if (newInterval[1] < intervals[0][0]) {
+            return arrayOf(newInterval) + intervals
+        }
+        if (newInterval[0] > intervals.last()[1]) {
+            return intervals + arrayOf(newInterval)
+        }
 
-    private fun findIndexAtMergeStart(intervals: Array<IntArray>, newInterval: IntArray): Int {
-        val (newStart, newEnd) = newInterval
-
-        /* BinarySearch start of newInterval against the end of existing intervals such that
-         * end_i-1 < start_new <= end_i
-         */
-        val i = intervals
-            .binarySearchBy(newStart) { it[1] }
+        val reversed = intArrayOf(newInterval[1], newInterval[0])
+        // The insertion index of newInterval[0] among intervals.map { it[1] }
+        val iStart = intervals
+            .binarySearch(reversed, compareBy { it[1] })
+            .let { if (it < 0) -(it + 1) else it }
+        // The insertion index of newInterval[1] among intervals.map { it[0] }
+        val iEnd = intervals
+            .binarySearch(reversed, compareBy { it[0] })
             .let { if (it < 0) -(it + 1) else it }
 
-        return when (i) {
-            0 -> if (newEnd < intervals[0][0]) -1 else 0
-            else -> i
-        }
-    }
+        return when {
+            iEnd == intervals.size -> {
+                val result = Array(iStart + 1) { intArrayOf() }
 
-    private fun <T, K : Comparable<K>> Array<T>.binarySearchBy(
-        key: K,
-        fromIndex: Int = 0,
-        untilIndex: Int = size,
-        selector: (T) -> K
-    ): Int {
+                for (i in 0..<iStart) {
+                    result[i] = intervals[i]
+                }
+                val mergedStart = min(newInterval[0], intervals[iStart][0])
+                val mergedEnd = max(newInterval[1], intervals.last()[1])
+                result[iStart] = intArrayOf(mergedStart, mergedEnd)
+                result
+            }
 
-        var left = fromIndex
-        var right = untilIndex - 1
-        while (left <= right) {
-            val midIndex = (left + right) ushr 1
-            val midValue = this[midIndex]
-            val midKey = selector(midValue)
-            when {
-                key > midKey -> left = midIndex + 1
-                key < midKey -> right = midIndex - 1
-                else -> return midIndex
+            newInterval[1] < intervals[iEnd][0] -> {
+                val result = Array(iStart + 1 + intervals.size - iEnd) { intArrayOf() }
+
+                for (i in 0..<iStart) {
+                    result[i] = intervals[i]
+                }
+                val mergedStart = min(newInterval[0], intervals[iStart][0])
+                val mergedEnd = max(newInterval[1], intervals[iEnd - 1][1])
+                result[iStart] = intArrayOf(mergedStart, mergedEnd)
+                for (i in iStart + 1..<result.size) {
+                    result[i] = intervals[iEnd + i - (iStart + 1)]
+                }
+                result
+            }
+
+            else -> {
+                val result = Array(iStart + intervals.size - iEnd) { intArrayOf() }
+
+                for (i in 0..<iStart) {
+                    result[i] = intervals[i]
+                }
+                val mergedStart = min(newInterval[0], intervals[iStart][0])
+                result[iStart] = intArrayOf(mergedStart, intervals[iEnd][1])
+                for (i in iStart + 1..<result.size) {
+                    result[i] = intervals[iEnd + i - iStart]
+                }
+                result
             }
         }
-        return -left - 1
-    }
-
-    private fun findIndexAtMergeEnded(intervals: Array<IntArray>, newInterval: IntArray): Int {
-        val (newStart, newEnd) = newInterval
-
-        /* BinarySearch end of newInterval against the start of existing intervals such that
-         * start_i-1 < newEnd <= start_i
-         */
-        val i = intervals
-            .binarySearchBy(newEnd) { it[0] }
-            .let { if (it < 0) -(it + 1) else it }
-
-        return when (i) {
-            0 -> if (newEnd < intervals[0][0]) -1 else 0
-
-            intervals.size ->
-                if (newStart > intervals.last()[1]) intervals.size else intervals.size - 1
-
-            else -> if (intervals[i][0] == newEnd) i else i - 1
-        }
-    }
-
-    private fun buildIntervalsAfterInsertion(
-        intervals: Array<IntArray>,
-        newInterval: IntArray,
-        indexMergeStart: Int,
-        indexMergeEnded: Int
-    ): Array<IntArray> {
-
-        if (intervals.isEmpty()) return arrayOf(newInterval)
-        val revisedIntervals = mutableListOf<IntArray>()
-
-        for (index in 0 until indexMergeStart) {
-            revisedIntervals.add(intervals[index])
-        }
-
-        revisedIntervals.add(
-            getMergedInterval(intervals, newInterval, indexMergeStart, indexMergeEnded)
-        )
-
-        for (index in indexMergeEnded + 1 until intervals.size) {
-            revisedIntervals.add(intervals[index])
-        }
-        return revisedIntervals.toTypedArray()
-    }
-
-    private fun getMergedInterval(
-        intervals: Array<IntArray>,
-        newInterval: IntArray,
-        indexMergeStart: Int,
-        indexMergeEnded: Int
-    ): IntArray {
-
-        val (newStart, newEnd) = newInterval
-
-        val mergedStart = when (indexMergeStart) {
-            -1, intervals.size -> newStart
-            else -> minOf(newStart, intervals[indexMergeStart][0])
-        }
-
-        val mergedEnd = when (indexMergeEnded) {
-            -1, intervals.size -> newEnd
-            else -> maxOf(newEnd, intervals[indexMergeEnded][1])
-        }
-
-        return intArrayOf(mergedStart, mergedEnd)
     }
 }
