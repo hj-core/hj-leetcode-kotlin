0a2c0ee144c13005961c35e21b56ed8375bb4a96
hj-core <88603338+hj-core@users.noreply.github.com>
2024-03-26 17:57:40 +0800

    Add a solution to problem 41

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem41/Solution.kt
@@ -0,0 +1,33 @@
+package com.hj.leetcode.kotlin.problem41
+
+/**
+ * LeetCode page: [41. First Missing Positive](https://leetcode.com/problems/first-missing-positive/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Auxiliary Space O(1) where N is the size of nums;
+     */
+    fun firstMissingPositive(nums: IntArray): Int {
+        /* The result is within the range from 1 to n+1.
+         * Use nums[i] != i+1 to indicate the value i+1 is missing.
+         */
+        for (i in nums.indices) {
+            while (nums[i] in 1..nums.size
+                && nums[i] != nums[nums[i] - 1]
+            ) {
+                nums.swap(i, nums[i] - 1)
+            }
+        }
+
+        for ((i, value) in nums.withIndex()) {
+            if (value != i + 1) {
+                return i + 1
+            }
+        }
+        return nums.size + 1
+    }
+
+    private fun IntArray.swap(i: Int, j: Int) {
+        this[i] = this[j].also { this[j] = this[i] }
+    }
+}
