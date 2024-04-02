3dafb9e406c9a888589b09fe0b6463ed87ea7558
hj-core <88603338+hj-core@users.noreply.github.com>
2024-04-02 22:56:49 +0800

    Refactor by a new perspective of isomorphic

--- src/main/kotlin/com/hj/leetcode/kotlin/problem205/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem205/Solution.kt
@@ -5,25 +5,24 @@ package com.hj.leetcode.kotlin.problem205
  */
 class Solution {
     /* Complexity:
-     * Time O(N) and Space O(min(N,C)) where N is the length of s
-     * and C is the cardinality of the possible charset;
+     * Time O(N) and Space O(min(N, C)) where N is the length of s
+     * and C is the number of cardinality of charset;
      */
     fun isIsomorphic(s: String, t: String): Boolean {
         if (s.length != t.length) {
             return false
         }
 
-        val mappingS = hashMapOf<Char, Char>() // s[i] to t[i]
-        val mappingT = hashMapOf<Char, Char>() // t[i] to s[i]
+        // The last found index of a char
+        val sLastFound = hashMapOf<Char, Int>()
+        val tLastFound = hashMapOf<Char, Int>()
+
         for (i in s.indices) {
-            if (s[i] in mappingS && mappingS[s[i]] != t[i]) {
+            if (sLastFound[s[i]] != tLastFound[t[i]]) {
                 return false
             }
-            if (t[i] in mappingT && mappingT[t[i]] != s[i]) {
-                return false
-            }
-            mappingS[s[i]] = t[i]
-            mappingT[t[i]] = s[i]
+            sLastFound[s[i]] = i
+            tLastFound[t[i]] = i
         }
         return true
     }
