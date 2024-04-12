288e9bb9e3c69ef78a0db6f032ba4fbca56f878e
hj-core <88603338+hj-core@users.noreply.github.com>
2024-04-12 16:55:33 +0800

    Add a solution to problem 402 (#320)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem402/Solution.kt
@@ -0,0 +1,59 @@
+package com.hj.leetcode.kotlin.problem402
+
+/**
+ * LeetCode page: [402. Remove K Digits](https://leetcode.com/problems/remove-k-digits/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the length of num;
+     */
+    fun removeKdigits(num: String, k: Int): String {
+        var remainingK = k
+        var index = 0
+        val stack = mutableListOf<Char>()
+
+        // Do the removals
+        while (remainingK > 0) {
+            when {
+                stack.isEmpty() -> {
+                    check(index < num.length)
+                    stack.add(num[index])
+                    index++
+                }
+
+                index == num.length || num[index] < stack.last() -> {
+                    stack.removeLast()
+                    remainingK--
+                }
+
+                else -> {
+                    stack.add(num[index])
+                    index++
+                }
+            }
+        }
+
+        // Build the result
+        val result = StringBuilder()
+        val firstNonZero = stack.indexOfFirst { it != '0' }
+        if (firstNonZero != -1) {
+            for (i in firstNonZero..<stack.size) {
+                result.append(stack[i])
+            }
+        }
+
+        if (result.isEmpty()) {
+            while (index < num.length && num[index] == '0') {
+                index++
+            }
+        }
+        for (i in index..<num.length) {
+            result.append(num[i])
+        }
+
+        if (result.isEmpty()) {
+            result.append('0')
+        }
+        return result.toString()
+    }
+}
