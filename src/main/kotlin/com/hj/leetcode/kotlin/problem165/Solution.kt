c95aec369ad6a40924f7ff73163a59117f543599
hj-core <88603338+hj-core@users.noreply.github.com>
2024-05-03 22:48:38 +0800

    Update Problem 165: Eliminate Int segment list (#336)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem165/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem165/Solution.kt
@@ -11,12 +11,12 @@ class Solution {
      * and N is the length of version2;
      */
     fun compareVersion(version1: String, version2: String): Int {
-        val segments1 = version1.split(".").map { it.toInt() }
-        val segments2 = version2.split(".").map { it.toInt() }
+        val segments1 = version1.split(".")
+        val segments2 = version2.split(".")
 
         for (i in 0..<max(segments1.size, segments2.size)) {
-            val segment1 = segments1.getOrElse(i) { 0 }
-            val segment2 = segments2.getOrElse(i) { 0 }
+            val segment1 = segments1.getOrElse(i) { "0" }.toInt()
+            val segment2 = segments2.getOrElse(i) { "0" }.toInt()
             when {
                 segment1 > segment2 -> return 1
                 segment1 < segment2 -> return -1
