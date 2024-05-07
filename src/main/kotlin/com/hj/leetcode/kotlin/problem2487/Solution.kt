475478c1111724e55e10bc44965e58b91343f645
hj-core <88603338+hj-core@users.noreply.github.com>
2024-05-07 17:09:35 +0800

    Switch to monolithic stack approach

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2487/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2487/Solution.kt
@@ -14,26 +14,19 @@ class Solution {
             return null
         }
 
-        val list = head.toList()
-        var suffixMax = list.last().`val`
-        var result: ListNode? = null
-
-        for (node in list.asReversed()) {
-            if (node.`val` < suffixMax) {
-                continue
-            }
-            node.next = result
-            result = node
-            suffixMax = node.`val`
-        }
-        return result
-    }
-
-    private fun ListNode?.toList(): List<ListNode> = buildList {
-        var nodePtr = this@toList
+        val stack = mutableListOf<ListNode>()
+        var nodePtr = head
         while (nodePtr != null) {
-            add(nodePtr)
+            while (stack.isNotEmpty() && stack.last().`val` < nodePtr.`val`) {
+                stack.removeLast()
+            }
+            stack.add(nodePtr)
             nodePtr = nodePtr.next
         }
+
+        for (i in 0..<stack.lastIndex) {
+            stack[i].next = stack[i + 1]
+        }
+        return stack[0]
     }
 }
