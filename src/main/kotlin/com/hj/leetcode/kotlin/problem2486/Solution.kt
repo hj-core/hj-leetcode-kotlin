d9bf355ef216df44b5b4934971c8680557e78465
hj-core <88603338+hj-core@users.noreply.github.com>
2024-06-03 16:48:56 +0800

    Add a solution to problem 2486

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2486/Solution.kt
@@ -0,0 +1,22 @@
+package com.hj.leetcode.kotlin.problem2486
+
+/**
+ * LeetCode page: [2486. Append Characters to String to Make Subsequence](https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N+M) and Space O(1) where N is the length of t
+     * and M is the length of s;
+     */
+    fun appendCharacters(s: String, t: String): Int {
+        var sIndex = 0
+        var tIndex = 0
+        while (tIndex < t.length && sIndex < s.length) {
+            if (t[tIndex] == s[sIndex]) {
+                tIndex++
+            }
+            sIndex++
+        }
+        return t.length - tIndex
+    }
+}
