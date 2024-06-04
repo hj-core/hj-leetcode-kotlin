54fc993116b8488ca9dc9e0c5fce934b2e5804ba
hj-core <88603338+hj-core@users.noreply.github.com>
2024-06-04 16:50:22 +0800

    Add a solution to problem 409

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem409/Solution.kt
@@ -0,0 +1,20 @@
+package com.hj.leetcode.kotlin.problem409
+
+/**
+ * LeetCode page: [409. Longest Palindrome](https://leetcode.com/problems/longest-palindrome/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the length of s;
+     */
+    fun longestPalindrome(s: String): Int {
+        val countChars = s
+            .groupingBy { it }
+            .eachCount()
+
+        val numOldFreqChars = countChars
+            .values
+            .count { it and 1 != 0 }
+        return if (numOldFreqChars > 0) s.length - numOldFreqChars + 1 else s.length
+    }
+}
