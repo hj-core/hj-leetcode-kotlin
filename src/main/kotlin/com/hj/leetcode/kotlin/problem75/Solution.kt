5a53b6b649e23b4bd3b525c2379f2e2b84c744a7
hj-core <88603338+hj-core@users.noreply.github.com>
2024-06-13 17:42:25 +0800

    Update problem 75: New solution and improve readablity (#375)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem75/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem75/Solution.kt
@@ -4,41 +4,40 @@ package com.hj.leetcode.kotlin.problem75
  * LeetCode page: [75. Sort Colors](https://leetcode.com/problems/sort-colors/);
  */
 class Solution {
-    private val red = 0
-    private val white = 1
-    private val blue = 2
-
     /* Complexity:
      * Time O(N) and Space O(1) where N is the size of nums;
      */
     fun sortColors(nums: IntArray): Unit {
-        val numReds = pushTargetToStartOfRangeAndReturnNumTargets(
+        val red = 0
+        val white = 1
+
+        val numReds = pushTargetsToStartAndReturnCount(
+            colors = nums,
+            start = 0,
             targetColor = red,
-            effectiveRange = nums.indices,
-            colors = nums
         )
 
-        pushTargetToStartOfRangeAndReturnNumTargets(
-            targetColor = white,
-            effectiveRange = numReds..nums.lastIndex,
-            colors = nums
+        pushTargetsToStartAndReturnCount(
+            colors = nums,
+            start = numReds,
+            targetColor = white
         )
     }
 
-    private fun pushTargetToStartOfRangeAndReturnNumTargets(
+    private fun pushTargetsToStartAndReturnCount(
+        colors: IntArray,
+        start: Int,
         targetColor: Int,
-        effectiveRange: IntRange,
-        colors: IntArray
     ): Int {
-       var pendingIndex = effectiveRange.first
-       for (index in effectiveRange){
-           val color = colors[index]
-           if (color == targetColor) {
-               colors.swap(index, pendingIndex)
-               pendingIndex++
-           }
-       }
-        return pendingIndex
+        var swapIndex = start
+        for (index in start..<colors.size) {
+            val color = colors[index]
+            if (color == targetColor) {
+                colors.swap(index, swapIndex)
+                swapIndex++
+            }
+        }
+        return swapIndex
     }
 
     private fun IntArray.swap(index: Int, withIndex: Int) {
