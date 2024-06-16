3a475cb3dfbc764dcf24709bd1762640ab743901
hj-core <88603338+hj-core@users.noreply.github.com>
2024-06-16 17:38:36 +0800

    Add a solution to problem 330

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem330/Solution.kt
@@ -0,0 +1,28 @@
+package com.hj.leetcode.kotlin.problem330
+
+import kotlin.math.min
+
+/**
+ * LeetCode page: [330. Patching Array](https://leetcode.com/problems/patching-array/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(M+Log(n)) and Space O(1) where M is the size of nums;
+     */
+    fun minPatches(nums: IntArray, n: Int): Int {
+        var result = 0
+        var index = 0
+        var covered = 0
+
+        while (covered < n) {
+            if (index < nums.size && nums[index] <= covered + 1) {
+                covered += nums[index]
+                index++
+            } else {
+                covered += min(covered + 1, Int.MAX_VALUE - covered)
+                result++
+            }
+        }
+        return result
+    }
+}
