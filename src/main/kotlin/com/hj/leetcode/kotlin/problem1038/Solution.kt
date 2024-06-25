3e0f40cc20b827b23ad61eb287d673cd397784fe
hj-core <88603338+hj-core@users.noreply.github.com>
2024-06-25 17:43:52 +0800

    Add a solution to problem 1038

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1038/Solution.kt
@@ -0,0 +1,28 @@
+package com.hj.leetcode.kotlin.problem1038
+
+import com.hj.leetcode.kotlin.common.model.TreeNode
+
+/**
+ * LeetCode page: [1038. Binary Search Tree to Greater Sum Tree](https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(H) where N is the number of nodes in root
+     * and H is the height of root;
+     */
+    fun bstToGst(root: TreeNode?): TreeNode? {
+        bstToGst(root, intArrayOf(0))
+        return root
+    }
+
+    private fun bstToGst(root: TreeNode?, carry: IntArray) {
+        if (root == null) {
+            return
+        }
+        // Reversed inorder traversal
+        bstToGst(root.right, carry)
+        root.`val` += carry[0]
+        carry[0] = root.`val`
+        bstToGst(root.left, carry)
+    }
+}
