2994418d7529b41703de58b8cb129d1b518a3746
hj-core <88603338+hj-core@users.noreply.github.com>
2024-07-08 00:18:55 +0800

    Add solutions to problem 1518 (#395)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1518/Solution.kt
@@ -0,0 +1,22 @@
+package com.hj.leetcode.kotlin.problem1518
+
+/**
+ * LeetCode page: [1518. Water Bottles](https://leetcode.com/problems/water-bottles/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(LogN) and Space O(1) where N represents numBottles;
+     */
+    fun numWaterBottles(numBottles: Int, numExchange: Int): Int {
+        var result = 0
+        var remaining = numBottles
+        while (remaining >= numExchange) {
+            val numDrank = remaining - remaining % numExchange
+            result += numDrank
+            remaining -= numDrank
+            remaining += numDrank / numExchange
+        }
+        result += remaining
+        return result
+    }
+}
