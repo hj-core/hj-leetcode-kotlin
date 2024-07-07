bc43c70d804e3d42f219b3d79ef35790feaa5b9b
hj-core <88603338+hj-core@users.noreply.github.com>
2024-07-07 15:57:05 +0800

    Add a constant time solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1518/Solution2.kt
@@ -0,0 +1,19 @@
+package com.hj.leetcode.kotlin.problem1518
+
+/**
+ * LeetCode page: [1518. Water Bottles](https://leetcode.com/problems/water-bottles/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(1) and Space O(1);
+     */
+    fun numWaterBottles(numBottles: Int, numExchange: Int): Int {
+        /* The idea is to use one bottle to absorb every following (numExchange-1) bottles.
+         * By absorption, I mean that we drink that bottle together with the (numExchange-1)
+         * bottles and then exchange one bottle back.
+         */
+        val absorptionRounds = (numBottles - 1) / (numExchange - 1)
+        val remainingBottles = (numBottles - 1) % (numExchange - 1) + 1
+        return (absorptionRounds * numExchange) + remainingBottles
+    }
+}
