1ba210fc074dd7d556f03fef09e86d5771a78f7c
hj-core <88603338+hj-core@users.noreply.github.com>
2024-07-08 23:12:28 +0800

    Add a recursive solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1823/Solution2.kt
@@ -0,0 +1,26 @@
+package com.hj.leetcode.kotlin.problem1823
+
+/**
+ * LeetCode page: [1823. Find the Winner of the Circular Game](https://leetcode.com/problems/find-the-winner-of-the-circular-game/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(n) and Space O(n);
+     */
+    fun findTheWinner(n: Int, k: Int): Int {
+        /* The idea is to start the game with zero-based indices and after proceed the
+         * first removal, the rest players form a game with size n-1 and shifted indices.
+         *
+         * This recursive solution can be converted to an iterative solution which
+         * has O(1) space complexity.
+         */
+        return 1 + findTheWinnerZeroBased(n, k)
+    }
+
+    private fun findTheWinnerZeroBased(n: Int, k: Int): Int {
+        if (n == 1) {
+            return 0
+        }
+        return (findTheWinnerZeroBased(n - 1, k) + k) % n
+    }
+}
