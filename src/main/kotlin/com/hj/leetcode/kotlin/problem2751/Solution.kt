45f3b15500e372303c018e3bca569218cf4d42d3
hj-core <88603338+hj-core@users.noreply.github.com>
2024-07-13 23:10:21 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2751/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2751/Solution.kt
@@ -8,31 +8,18 @@ class Solution {
      * Time O(NLogN) and Space O(N) where N is the number of robots;
      */
     fun survivedRobotsHealths(positions: IntArray, healths: IntArray, directions: String): List<Int> {
-        val sortedRobots = positions.indices.mapTo(mutableListOf()) { index ->
-            Robot(index, positions[index], healths[index], directions[index])
-        }.apply {
-            sortBy { it.position }
-        }
+        val remainingRobots = robotsAtStart(positions, healths, directions)
+        collideAll(remainingRobots)
+        remainingRobots.sortBy { it.index }
+        return remainingRobots.map { it.health }
+    }
 
-        val robotStack = mutableListOf<Robot>()
-        for (robot in sortedRobots) {
-            var winner = robot
-            while (robotStack.isNotEmpty()
-                && robotStack.last().direction == 'R'
-                && winner.direction == 'L'
-                && winner.health > 0
-            ) {
-                val opponent = robotStack.removeLast()
-                winner = collide(opponent, winner)
-            }
-            if (winner.health > 0) {
-                robotStack.add(winner)
-            }
-        }
-
-        return robotStack
-            .apply { sortBy { it.index } }
-            .map { it.health }
+    private fun robotsAtStart(
+        positions: IntArray,
+        healths: IntArray,
+        directions: String,
+    ): MutableList<Robot> = positions.indices.mapTo(mutableListOf()) { index ->
+        Robot(index, positions[index], healths[index], directions[index])
     }
 
     private data class Robot(
@@ -42,19 +29,37 @@ class Solution {
         val direction: Char,
     )
 
-    private fun collide(leftRobot: Robot, rightRobot: Robot): Robot {
-        require(leftRobot.direction == 'R' && rightRobot.direction == 'L')
+    private fun collideAll(robots: MutableList<Robot>) {
+        robots.sortBy { it.position }
+        // Use robots in-place as stack and perform all collisions
+        var stackPtr = -1
+        for (robot in robots) {
+            var winner = robot
+            while (stackPtr >= 0
+                && robots[stackPtr].direction == 'R'
+                && winner.direction == 'L'
+            ) {
+                val challenger = robots[stackPtr]
+                stackPtr--
+                winner = collide(challenger, winner)
+            }
+            if (winner.health > 0) {
+                stackPtr++
+                robots[stackPtr] = winner
+            }
+        }
+        // Pop the robots above stackPtr
+        repeat(robots.lastIndex - stackPtr) {
+            robots.removeLast()
+        }
+    }
 
+    private fun collide(left: Robot, right: Robot): Robot {
+        require(left.direction == 'R' && right.direction == 'L')
         return when {
-            leftRobot.health < rightRobot.health -> {
-                rightRobot.copy(health = rightRobot.health - 1)
-            }
-
-            leftRobot.health > rightRobot.health -> {
-                leftRobot.copy(health = leftRobot.health - 1)
-            }
-
-            else -> leftRobot.copy(health = 0)
+            left.health > right.health -> left.copy(health = left.health - 1)
+            left.health < right.health -> right.copy(health = right.health - 1)
+            else -> left.copy(health = 0)
         }
     }
 }
