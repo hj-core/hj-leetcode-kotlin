7a4d7233be659972673cd7f970c33583d69538ac
hj-core <88603338+hj-core@users.noreply.github.com>
2024-08-01 21:01:04 +0800

    Add a solution to problem 2678

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2678/Solution.kt
@@ -0,0 +1,13 @@
+package com.hj.leetcode.kotlin.problem2678
+
+/**
+ * LeetCode page: [2678. Number of Senior Citizens](https://leetcode.com/problems/number-of-senior-citizens/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the size of details;
+     */
+    fun countSeniors(details: Array<String>): Int {
+        return details.count { it.substring(11..12) > "60" }
+    }
+}
