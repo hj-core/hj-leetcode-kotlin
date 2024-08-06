dcd27c52c1553b1d51923a1da2c1511c03b37216
hj-core <88603338+hj-core@users.noreply.github.com>
2024-08-06 22:12:03 +0800

    Improve IntArray.sumSubarraysWithSumNotGreaterThan(..)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1508/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1508/Solution2.kt
@@ -4,7 +4,7 @@ import kotlin.math.max
 
 class Solution2 {
     /* Complexity:
-     * Time O(nLogM) and Space O(n) where M is the sum of nums;
+     * Time O(nLogM) and Space O(1) where M is the sum of nums;
      */
     fun rangeSum(nums: IntArray, n: Int, left: Int, right: Int): Int {
         val modulo = 1_000_000_007
@@ -60,32 +60,30 @@ class Solution2 {
     }
 
     private fun IntArray.sumSubarraysWithSumNotGreaterThan(limit: Int, modulo: Int): Int {
+        /* Imaging a triangle shape stack of subarrays that consists of all the
+         * valid subarrays that end at a given end index.
+         *
+         * These subarrays are aligned to the right and the higher, the shorter.
+         */
+
         var result = 0
-        var rightMostEnd = 0
-        var subarraySum = 0
-        val rightmostEnds = ArrayDeque<Int>()
-        var sumRightmostEnds = 0
-        for (start in indices) {
-            require(this[start] >= 0) { "All numbers should be non-negative." }
-            rightMostEnd = max(start, rightMostEnd)
-            while (rightMostEnd < size && subarraySum + this[rightMostEnd] <= limit) {
-                subarraySum += this[rightMostEnd]
-                rightMostEnd += 1
+        var earliestStart = 0
+        var subarraySum = 0 // Sum of subarray from earliestStart to end (inclusive)
+        var stackSum = 0 // Sum of all valid subarrays that end at the given end
+
+        for (end in this.indices) {
+            // Extend the stack to include current end
+            subarraySum += this[end]
+            stackSum += this[end] * (end - earliestStart + 1)
+
+            // Adjust the stack in case the subarraySum exceeds the limit
+            while (subarraySum > limit) {
+                stackSum -= subarraySum // Pop the bottom layer of stack
+                subarraySum -= this[earliestStart]
+                earliestStart++
             }
 
-            rightmostEnds.addLast(rightMostEnd)
-            sumRightmostEnds += rightMostEnd
-            while (rightmostEnds.isNotEmpty() && rightmostEnds.first() <= start) {
-                sumRightmostEnds -= rightmostEnds.first()
-                rightmostEnds.removeFirst()
-            }
-            result += this[start] * (sumRightmostEnds - start * rightmostEnds.size)
-            result %= modulo
-
-            // Prepare subarraySum for next start
-            if (start < rightMostEnd) {
-                subarraySum -= this[start]
-            }
+            result = (result + stackSum) % modulo
         }
         return result
     }
