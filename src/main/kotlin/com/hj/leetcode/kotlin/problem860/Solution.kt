683fa9fe3f48ca1c8592a73eecaa5ceae784ec80
hj-core <88603338+hj-core@users.noreply.github.com>
2024-08-15 17:54:57 +0800

    Add a solution to problem 860

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem860/Solution.kt
@@ -0,0 +1,38 @@
+package com.hj.leetcode.kotlin.problem860
+
+/**
+ * LeetCode page: [860. Lemonade Change](https://leetcode.com/problems/lemonade-change/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the size of bills;
+     */
+    fun lemonadeChange(bills: IntArray): Boolean {
+        val availableCash = intArrayOf(0, 0, 0) // count of ($5, $10, $20)
+
+        for (bill in bills) {
+            when (bill) {
+                5 -> availableCash[0] += 1
+
+                10 -> {
+                    availableCash[1] += 1
+                    availableCash[0] -= 1
+                }
+
+                20 -> {
+                    var change = 15
+                    if (availableCash[1] > 0) {
+                        change -= 10
+                        availableCash[1] -= 1
+                    }
+                    availableCash[0] -= change / 5
+                }
+            }
+
+            if (availableCash[0] < 0) {
+                return false
+            }
+        }
+        return true
+    }
+}
