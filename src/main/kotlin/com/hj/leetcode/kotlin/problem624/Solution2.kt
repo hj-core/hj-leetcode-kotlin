9724d7ed8271d8f68f6178f44f3dd227f410fa13
hj-core <88603338+hj-core@users.noreply.github.com>
2024-08-16 23:55:25 +0800

    Add an one-pass solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem624/Solution2.kt
@@ -0,0 +1,32 @@
+package com.hj.leetcode.kotlin.problem624
+
+import kotlin.math.max
+import kotlin.math.min
+
+/**
+ * LeetCode page: [624. Maximum Distance in Arrays](https://leetcode.com/problems/maximum-distance-in-arrays/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the size of arrays;
+     */
+    fun maxDistance(arrays: List<List<Int>>): Int {
+        /* Pile up the arrays and connect the pairs that lead to the result.
+         * The line must be either tilted to the left or tilted to the right.
+         */
+        var result = 0
+        var minValue = arrays[0].first()
+        var maxValue = arrays[0].last()
+
+        for (i in 1..<arrays.size) {
+            result = maxOf(
+                result,
+                arrays[i].last() - minValue,
+                maxValue - arrays[i].first()
+            )
+            minValue = min(minValue, arrays[i].first())
+            maxValue = max(maxValue, arrays[i].last())
+        }
+        return result
+    }
+}
