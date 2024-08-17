f027ffe621e37fea428dce3a7fae41096f31481f
hj-core <88603338+hj-core@users.noreply.github.com>
2024-08-17 12:49:23 +0800

    Improve comment

--- src/main/kotlin/com/hj/leetcode/kotlin/problem624/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem624/Solution2.kt
@@ -21,8 +21,8 @@ class Solution2 {
         for (i in 1..<arrays.size) {
             result = maxOf(
                 result,
-                arrays[i].last() - minValue, // Tilt to the right
-                maxValue - arrays[i].first() // Tilt to the left
+                arrays[i].last() - minValue, // Tilt to the left
+                maxValue - arrays[i].first() // Tilt to the right
             )
             minValue = min(minValue, arrays[i].first())
             maxValue = max(maxValue, arrays[i].last())
