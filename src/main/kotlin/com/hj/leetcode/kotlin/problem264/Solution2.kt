41c77707f00c1207336ac0eb80e96aaa7a84acfd
hj-core <88603338+hj-core@users.noreply.github.com>
2024-08-18 23:43:29 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem264/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem264/Solution2.kt
@@ -5,40 +5,31 @@ package com.hj.leetcode.kotlin.problem264
  */
 class Solution2 {
     /* Complexity:
-     * Time O(N) and Space O(N) where N equals n;
+     * Time O(n) and Space O(n);
      */
     fun nthUglyNumber(n: Int): Int {
         val uglyNumbers = mutableListOf(1)
-
         var index2 = 0
-        var candidate2 = 2 * uglyNumbers[index2]
-
         var index3 = 0
-        var candidate3 = 3 * uglyNumbers[index3]
-
         var index5 = 0
-        var candidate5 = 5 * uglyNumbers[index5]
 
         while (uglyNumbers.size < n) {
+            val candidate2 = 2 * uglyNumbers[index2]
+            val candidate3 = 3 * uglyNumbers[index3]
+            val candidate5 = 5 * uglyNumbers[index5]
             val nextUgly = minOf(candidate2, candidate3, candidate5)
             uglyNumbers.add(nextUgly)
 
             if (candidate2 == nextUgly) {
                 index2++
-                candidate2 = 2 * uglyNumbers[index2]
             }
-
             if (candidate3 == nextUgly) {
                 index3++
-                candidate3 = 3 * uglyNumbers[index3]
             }
-
             if (candidate5 == nextUgly) {
                 index5++
-                candidate5 = 5 * uglyNumbers[index5]
             }
         }
-
         return uglyNumbers.last()
     }
 }
