47fee53c60bff4499b102759c9c937247e016638
hj-core <88603338+hj-core@users.noreply.github.com>
2024-08-19 12:43:05 +0800

    Refactor Solution2 to use Array

--- src/main/kotlin/com/hj/leetcode/kotlin/problem264/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem264/Solution2.kt
@@ -8,27 +8,22 @@ class Solution2 {
      * Time O(n) and Space O(n);
      */
     fun nthUglyNumber(n: Int): Int {
-        val uglyNumbers = mutableListOf(1L)
+        val uglyNumbers = LongArray(n)
+        uglyNumbers[0] = 1
         var index2 = 0
         var index3 = 0
         var index5 = 0
 
-        while (uglyNumbers.size < n) {
+        for (i in 1..<uglyNumbers.size) {
             val candidate2 = 2 * uglyNumbers[index2]
             val candidate3 = 3 * uglyNumbers[index3]
             val candidate5 = 5 * uglyNumbers[index5]
             val nextUgly = minOf(candidate2, candidate3, candidate5)
-            uglyNumbers.add(nextUgly)
+            uglyNumbers[i] = nextUgly
 
-            if (candidate2 == nextUgly) {
-                index2++
-            }
-            if (candidate3 == nextUgly) {
-                index3++
-            }
-            if (candidate5 == nextUgly) {
-                index5++
-            }
+            if (candidate2 == nextUgly) index2++
+            if (candidate3 == nextUgly) index3++
+            if (candidate5 == nextUgly) index5++
         }
         return uglyNumbers.last().toInt()
     }
