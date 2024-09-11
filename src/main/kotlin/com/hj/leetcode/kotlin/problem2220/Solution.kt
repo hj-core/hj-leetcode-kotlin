c54a049daad30d326255e5d4681b01eb551bf52e
hj-core <88603338+hj-core@users.noreply.github.com>
2024-09-11 18:16:05 +0800

    Refactor to use built-in functions

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2220/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2220/Solution.kt
@@ -7,23 +7,8 @@ class Solution {
     /* Complexity:
      * Time O(LogN) and Space O(1) where N is the max between start and goal;
      */
-    fun minBitFlips(start: Int, goal: Int): Int {
-        val sumOfDiffBits = start xor goal
-        return sumOfDiffBits.countOneBits()
-    }
-
-    private fun Int.countOneBits(): Int {
-        var count = 0
-        var currValue = this
-        while (currValue != 0) {
-            currValue = currValue.subtractLastBitOne()
-            count++
-        }
-        return count
-    }
-
-    private fun Int.subtractLastBitOne(): Int {
-        require(this >= 0)
-        return if (this == 0) 0 else this and (this - 1)
-    }
+    fun minBitFlips(
+        start: Int,
+        goal: Int,
+    ): Int = (start xor goal).countOneBits()
 }
