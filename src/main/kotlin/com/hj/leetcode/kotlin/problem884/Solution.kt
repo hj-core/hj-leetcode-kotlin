c192a0735092d9498bf34884cd9533a3b4d7c3cc
hj-core <88603338+hj-core@users.noreply.github.com>
2024-09-17 18:56:43 +0800

    Revise implementation
    New implementation collect all words first and then filter unique ones

--- src/main/kotlin/com/hj/leetcode/kotlin/problem884/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem884/Solution.kt
@@ -12,21 +12,15 @@ class Solution {
         s1: String,
         s2: String,
     ): Array<String> {
-        val wordCount1 = s1.split(" ").groupingBy { it }.eachCount()
-        val wordCount2 = s2.split(" ").groupingBy { it }.eachCountTo(mutableMapOf())
+        val wordCount = mutableMapOf<String, Int>()
+        s1.split(" ").groupingBy { it }.eachCountTo(wordCount)
+        s2.split(" ").groupingBy { it }.eachCountTo(wordCount)
 
-        val result = mutableListOf<String>()
-        for ((word, count) in wordCount1) {
-            when {
-                word in wordCount2 -> wordCount2.remove(word)
-                count == 1 -> result.add(word)
-            }
-        }
-        for ((word, count) in wordCount2) {
-            if (count == 1) {
-                result.add(word)
-            }
-        }
-        return result.toTypedArray()
+        return wordCount
+            .asSequence()
+            .filter { (_, count) -> count == 1 }
+            .map { (word, _) -> word }
+            .toList()
+            .toTypedArray()
     }
 }
