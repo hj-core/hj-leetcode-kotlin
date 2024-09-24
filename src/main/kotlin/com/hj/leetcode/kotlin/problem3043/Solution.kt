b91bc35d892d9eb7aad45b8219f0b6d79899ec37
hj-core <88603338+hj-core@users.noreply.github.com>
2024-09-24 18:58:55 +0800

    Add a solution to problem 3043

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3043/Solution.kt
@@ -0,0 +1,34 @@
+package com.hj.leetcode.kotlin.problem3043
+
+/**
+ * LeetCode page: [3043. Find the Length of the Longest Common Prefix](https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N+M) and Space O(N+M) where N is the total number of digits in arr1
+     * and M is the total number of digits in arr2.
+     */
+    fun longestCommonPrefix(
+        arr1: IntArray,
+        arr2: IntArray,
+    ): Int {
+        val prefixes1 = allPrefixes(arr1)
+        val prefixes2 = allPrefixes(arr2)
+        val maxCommon = prefixes1.asSequence().filter { it in prefixes2 }.maxOrNull()
+        return maxCommon?.toString()?.length ?: 0
+    }
+
+    private fun allPrefixes(arr: IntArray): Set<Int> {
+        val result = mutableSetOf<Int>()
+        for (num in arr) {
+            var iNum = num
+            while (iNum > 0) {
+                if (!result.add(iNum)) {
+                    break
+                }
+                iNum /= 10
+            }
+        }
+        return result
+    }
+}
