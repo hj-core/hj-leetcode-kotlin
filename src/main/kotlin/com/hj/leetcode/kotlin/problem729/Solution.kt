2dcdaba6796682360db78e67872e01fb7f0eb6e4
hj-core <88603338+hj-core@users.noreply.github.com>
2024-09-26 18:10:20 +0800

    Add a solution to problem 729

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem729/Solution.kt
@@ -0,0 +1,32 @@
+package com.hj.leetcode.kotlin.problem729
+
+import java.util.*
+
+/**
+ * LeetCode page: [729. My Calendar I](https://leetcode.com/problems/my-calendar-i/);
+ */
+class Solution
+
+class MyCalendar {
+    private data class Slot(
+        val start: Int,
+        val end: Int,
+    )
+
+    private val sortedSlots = TreeSet<Slot>(compareBy { it.start })
+
+    /* Complexity for N calls:
+     * Time O(NLogN) and Space O(N).
+     */
+    fun book(
+        start: Int,
+        end: Int,
+    ): Boolean {
+        val next = sortedSlots.ceiling(Slot(end, end)) ?: Slot(end, end)
+        val previous = sortedSlots.lower(next) ?: Slot(0, 0)
+        if (start < previous.end) {
+            return false
+        }
+        return sortedSlots.add(Slot(start, end))
+    }
+}
