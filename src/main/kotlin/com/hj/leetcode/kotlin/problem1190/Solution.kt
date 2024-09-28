eb9a8871adba01ea053a68fb42db4468141803c0
hj-core <88603338+hj-core@users.noreply.github.com>
2024-09-28 23:13:12 +0800

    Replace with a time O(N) implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1190/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1190/Solution.kt
@@ -5,33 +5,44 @@ package com.hj.leetcode.kotlin.problem1190
  */
 class Solution {
     /* Complexity:
-     * Time O(N^2) and Space O(N) where N is the length of s;
+     * Time O(N) and Space O(N) where N is the length of s;
      */
     fun reverseParentheses(s: String): String {
-        val result = mutableListOf<Char>()
-        val leftParentheses = mutableListOf<Int>() // Indices in result
-        for (char in s) {
-            when (char) {
+        val parenthesesIndices = parenthesesIndices(s)
+        val leftToRight = parenthesesIndices.associate { it.first to it.second }
+        val rightToLeft = parenthesesIndices.associate { it.second to it.first }
+        val builder = StringBuilder()
+        var i = 0
+        var direction = 1
+
+        while (builder.length < s.length - 2 * parenthesesIndices.size) {
+            when (s[i]) {
                 '(' -> {
-                    leftParentheses.add(result.size)
-                    result.add(char)
+                    i = checkNotNull(leftToRight[i])
+                    direction *= -1
                 }
 
                 ')' -> {
-                    // Reverse the chars up to the most recent left parentheses and pop it
-                    var left = leftParentheses.removeLast()
-                    var right = result.lastIndex
-                    while (left < right) {
-                        result[left] = result[right].also { result[right] = result[left] }
-                        left++
-                        right--
-                    }
-                    result.removeLast()
+                    i = checkNotNull(rightToLeft[i])
+                    direction *= -1
                 }
 
-                else -> result.add(char)
+                else -> builder.append(s[i])
+            }
+            i += direction
+        }
+        return builder.toString()
+    }
+
+    private fun parenthesesIndices(s: String): List<Pair<Int, Int>> {
+        val result = mutableListOf<Pair<Int, Int>>()
+        val lefts = mutableListOf<Int>() // Indices of pending left parentheses
+        for ((i, char) in s.withIndex()) {
+            when (char) {
+                '(' -> lefts.add(i)
+                ')' -> result.add(Pair(lefts.removeLast(), i))
             }
         }
-        return result.joinToString(separator = "")
+        return result
     }
 }
