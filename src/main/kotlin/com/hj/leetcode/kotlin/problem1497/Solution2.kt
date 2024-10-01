a1a38b83753294d83d2cc6ea8ba1bee120f187d7
hj-core <88603338+hj-core@users.noreply.github.com>
2024-10-01 19:16:14 +0800

    Add solution that count pairs on the fly

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1497/Solution2.kt
@@ -0,0 +1,29 @@
+package com.hj.leetcode.kotlin.problem1497
+
+/**
+ * LeetCode page: [1497. Check If Array Pairs Are Divisible by k](https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(N+k) and Space O(k) where N is the length of arr.
+     */
+    fun canArrange(
+        arr: IntArray,
+        k: Int,
+    ): Boolean {
+        var paired = 0
+        val countRemainder = mutableMapOf<Int, Int>()
+        for (num in arr) {
+            val remainder = num.mod(k)
+            val complement = if (remainder == 0) 0 else k - remainder
+            val existComplement = (countRemainder[complement] ?: 0) > 0
+            if (existComplement) {
+                countRemainder.compute(complement) { _, v -> checkNotNull(v) - 1 }
+                paired += 1
+            } else {
+                countRemainder.compute(remainder) { _, v -> 1 + (v ?: 0) }
+            }
+        }
+        return paired * 2 == arr.size
+    }
+}
