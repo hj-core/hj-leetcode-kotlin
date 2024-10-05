0bb03818f6a55eeedee49114f80fae6b03b304d8
hj-core <88603338+hj-core@users.noreply.github.com>
2024-10-05 18:20:14 +0800

    Update problem 567: Improve readability (#483)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem567/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem567/Solution.kt
@@ -5,44 +5,34 @@ package com.hj.leetcode.kotlin.problem567
  */
 class Solution {
     /* Complexity:
-     * Time O(|s2|) and Space O(1);
+     * Time O(KN) and Space O(K) where K is the size of char set (26 in this problem)
+     * and N is the length of s2.
      */
-    fun checkInclusion(s1: String, s2: String): Boolean {
-        if (s1.length > s2.length) return false
+    fun checkInclusion(
+        s1: String,
+        s2: String,
+    ): Boolean {
+        if (s2.length < s1.length) {
+            return false
+        }
+        val charCount = IntArray(26)
+        // Check the first window
+        for (i in s1.indices) {
+            charCount[s1[i] - 'a'] -= 1
+            charCount[s2[i] - 'a'] += 1
+        }
+        if (charCount.all { it == 0 }) {
+            return true
+        }
+        // Check the remaining windows
+        for (windowEnd in s1.length..<s2.length) {
+            charCount[s2[windowEnd] - 'a'] += 1
+            charCount[s2[windowEnd - s1.length] - 'a'] -= 1
 
-        val targetCount = countCharFrequency(s1)
-        val currWindowCount = countCharFrequency(s2, s1.indices)
-        var numMatched = (0 until 26).count { currWindowCount[it] == targetCount[it] }
-        if (numMatched == 26) return true
-
-        for (index in s1.length until s2.length) {
-            val popCountIndex = s2[index - s1.length] - 'a'
-            currWindowCount[popCountIndex]--
-            when (currWindowCount[popCountIndex]) {
-                targetCount[popCountIndex] -> numMatched++
-                targetCount[popCountIndex] - 1 -> numMatched--
+            if (charCount.all { it == 0 }) {
+                return true
             }
-
-            val pushCountIndex = s2[index] - 'a'
-            currWindowCount[pushCountIndex]++
-            when (currWindowCount[pushCountIndex]) {
-                targetCount[pushCountIndex] -> numMatched++
-                targetCount[pushCountIndex] + 1 -> numMatched--
-            }
-
-            if (numMatched == 26) return true
         }
         return false
-    }
-
-    private fun countCharFrequency(
-        lowercase: String,
-        indexRange: IntRange = lowercase.indices
-    ): IntArray {
-        val count = IntArray(26)
-        for (index in indexRange) {
-            count[lowercase[index] - 'a']++
-        }
-        return count
     }
 }
