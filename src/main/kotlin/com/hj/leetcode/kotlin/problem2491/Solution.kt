06b4045da21bf4a75df8891565171f59fab7963a
hj-core <88603338+hj-core@users.noreply.github.com>
2024-10-05 10:07:24 +0800

    Add a solution to problem 2491 (#482)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2491/Solution.kt
@@ -0,0 +1,36 @@
+package com.hj.leetcode.kotlin.problem2491
+
+/**
+ * LeetCode page: [2491. Divide Players Into Teams of Equal Skill](https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the size of skill.
+     */
+    fun dividePlayers(skill: IntArray): Long {
+        val totalSkills = skill.sum()
+        if (totalSkills % (skill.size / 2) != 0) {
+            return -1
+        }
+
+        val teamSkills = totalSkills / (skill.size / 2)
+        val pendingPlayers = mutableMapOf<Int, Int>() // skill to count
+        var pairedPlayers = 0
+        var result = 0L
+        for (skl in skill) {
+            val complement = teamSkills - skl
+            if (complement < 1) {
+                return -1
+            }
+            val numComplement = pendingPlayers[complement] ?: 0
+            if (numComplement > 0) {
+                pairedPlayers += 2
+                pendingPlayers[complement] = numComplement - 1
+                result += skl * complement
+            } else {
+                pendingPlayers.compute(skl) { _, v -> (v ?: 0) + 1 }
+            }
+        }
+        return if (pairedPlayers == skill.size) result else -1
+    }
+}
