f0588540d1e95cbf11a1d75a8ac26a1e8392bbc0
hj-core <88603338+hj-core@users.noreply.github.com>
2024-10-10 18:53:40 +0800

    Add a solution to problem 962

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem962/Solution.kt
@@ -0,0 +1,36 @@
+package com.hj.leetcode.kotlin.problem962
+
+import kotlin.math.max
+
+/**
+ * LeetCode page: [962. Maximum Width Ramp](https://leetcode.com/problems/maximum-width-ramp/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the length of nums.
+     */
+    fun maxWidthRamp(nums: IntArray): Int {
+        // Indices of strictly increasing values start at last and backward
+        val rights = mutableListOf(nums.lastIndex)
+        for (right in nums.indices.reversed()) {
+            if (nums[right] > nums[rights.last()]) {
+                rights.add(right)
+            }
+        }
+
+        // Working with indices of strictly decreasing values start at first and forward
+        var result = 0
+        var lastLeftValue = nums[0] + 1
+        var iRights = rights.lastIndex
+        for ((left, value) in nums.withIndex()) {
+            if (lastLeftValue > value) {
+                lastLeftValue = value
+                while (iRights > 0 && nums[rights[iRights - 1]] >= value) {
+                    iRights -= 1
+                }
+                result = max(result, rights[iRights] - left)
+            }
+        }
+        return result
+    }
+}
