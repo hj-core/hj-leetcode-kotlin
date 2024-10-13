b10f1d03a121b9a15119529262e02ea4f89e6460
hj-core <88603338+hj-core@users.noreply.github.com>
2024-10-13 21:07:52 +0800

    Improve comment

--- src/main/kotlin/com/hj/leetcode/kotlin/problem632/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem632/Solution2.kt
@@ -25,7 +25,7 @@ class Solution2 {
 
         var result = rangeOfValues(sortedCells, nums)
         while (sortedCells.size == nums.size) {
-            // This is the best range given the left boundary
+            // This is the best range corresponding to the left boundary
             val range = rangeOfValues(sortedCells, nums)
             if (isSmaller(range, result)) {
                 result = range
