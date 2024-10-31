026f979885c317446627e6c7f20422b1c6790388
hj-core <88603338+hj-core@users.noreply.github.com>
2024-11-01 00:23:06 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2463/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2463/Solution2.kt
@@ -19,40 +19,39 @@ class Solution2 {
         val impossible = -1L
         // dp[startBot]_startFac::=
         // minimumTotalDistance(sortedRobot[startBot:], sortedFactories[startFac:])
-        var dp = LongArray(sortedRobots.size + 1) { impossible }
-        dp[sortedRobots.size] = 0L
+        val dp = LongArray(sortedRobots.size + 1) { impossible }
+        dp[sortedRobots.size] = 0L // base case startFac= sortedFactories.size
         var totalCapacity = 0
 
         for (startFac in sortedFactories.indices.reversed()) {
             val (facPosition, facLimits) = sortedFactories[startFac]
             totalCapacity += facLimits
-            val nextDp = LongArray(dp.size) // dp_(starFac-1)
+            val prevDp = dp.clone() // dp_(starFac+1)
 
             for (startBot in sortedRobots.indices.reversed()) {
                 val totalRobots = sortedRobots.size - startBot
                 if (totalCapacity < totalRobots) {
-                    nextDp[startBot] = impossible
+                    dp[startBot] = impossible
                     continue
                 }
                 // Consider all possible numbers of robots assigned to startFac
                 val maxAssigned = min(totalRobots, facLimits)
-                nextDp[startBot] = dp[startBot] // No robot is assigned to startFac
+                dp[startBot] = prevDp[startBot] // No robot is assigned to startFac
                 var accDistance = 0L // Distance of the robots assigned to startFac
 
                 for (assigned in 1..maxAssigned) {
                     val currentBot = startBot + assigned - 1
                     accDistance += abs(facPosition - sortedRobots[currentBot])
-                    if (dp[currentBot + 1] == impossible) {
+                    if (prevDp[currentBot + 1] == impossible) {
                         continue
                     }
-                    val distance = accDistance + dp[currentBot + 1]
-                    nextDp[startBot] =
-                        nextDp[startBot].let {
+                    val distance = accDistance + prevDp[currentBot + 1]
+                    dp[startBot] =
+                        dp[startBot].let {
                             if (it == impossible) distance else min(it, distance)
                         }
                 }
             }
-            dp = nextDp
         }
         return dp[0]
     }
