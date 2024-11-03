f9d7570854c80c1613e8e733704cd1060d76c65a
hj-core <88603338+hj-core@users.noreply.github.com>
2024-11-03 17:32:20 +0800

    Add a solution to problem 796 (#515)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem796/Solution.kt
@@ -0,0 +1,63 @@
+package com.hj.leetcode.kotlin.problem796
+
+/**
+ * LeetCode page: [796. Rotate String](https://leetcode.com/problems/rotate-string/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the smaller length between s and goal.
+     */
+    fun rotateString(
+        s: String,
+        goal: String,
+    ): Boolean = s.length == goal.length && KMP(goal).searchIn(s + s)
+}
+
+private class KMP(
+    val target: String,
+) {
+    // kmpTable[i]::=
+    // length of target's longest prefix that is a suffix of target[1..i]
+    private val kmpTable = IntArray(target.length)
+
+    init {
+        buildTable()
+    }
+
+    private tailrec fun buildTable(
+        index: Int = 1,
+        compareIndex: Int = 0,
+    ) {
+        when {
+            index == target.length -> { // Finish the whole table
+            }
+
+            target[index] == target[compareIndex] -> {
+                kmpTable[index] = compareIndex + 1
+                buildTable(index + 1, compareIndex + 1)
+            }
+
+            compareIndex == 0 -> {
+                kmpTable[index] = 0
+                buildTable(index + 1, 0)
+            }
+
+            else -> buildTable(index, kmpTable[compareIndex - 1])
+        }
+    }
+
+    fun searchIn(s: String): Boolean = searchIn(s, 0, 0)
+
+    private tailrec fun searchIn(
+        s: String,
+        index: Int,
+        compareIndex: Int,
+    ): Boolean =
+        when {
+            compareIndex == target.length -> true
+            s.length - index < target.length - compareIndex -> false
+            s[index] == target[compareIndex] -> searchIn(s, index + 1, compareIndex + 1)
+            compareIndex == 0 -> searchIn(s, index + 1, 0)
+            else -> searchIn(s, index, kmpTable[compareIndex - 1])
+        }
+}
