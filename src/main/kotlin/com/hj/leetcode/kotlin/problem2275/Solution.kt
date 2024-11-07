c1ef4600acef1b27e8c923cadb963401797fb5b1
hj-core <88603338+hj-core@users.noreply.github.com>
2024-11-07 18:31:56 +0800

    Add a solution to problem 2275

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2275/Solution.kt
@@ -0,0 +1,16 @@
+package com.hj.leetcode.kotlin.problem2275
+
+/**
+ * LeetCode page: [2275. Largest Combination With Bitwise AND Greater Than Zero](https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the length of candidates.
+     */
+    fun largestCombination(candidates: IntArray): Int =
+        (0..24) // candidates[i] <= 10^7 < 2^24
+            .map { 1 shl it }
+            .maxOf { bit ->
+                candidates.count { it and bit != 0 }
+            }
+}
