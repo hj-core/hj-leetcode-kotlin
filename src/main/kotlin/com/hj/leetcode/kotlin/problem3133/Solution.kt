e43a2b2c81c68241214494cf94c30110e854451c
hj-core <88603338+hj-core@users.noreply.github.com>
2024-11-09 11:00:10 +0800

    Add a solution to problem 3133

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3133/Solution.kt
@@ -0,0 +1,36 @@
+package com.hj.leetcode.kotlin.problem3133
+
+/**
+ * LeetCode page: [3133. Minimum Array End](https://leetcode.com/problems/minimum-array-end/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(1) and Space O(1).
+     */
+    fun minEnd(
+        n: Int,
+        x: Int,
+    ): Long {
+        // require(1 <= n, x <= 10^8 < 2^27)
+        // Imagine a row of 64 lightbulbs.
+        // Initially, lightbulbs at positions corresponding to set bits in x are broken.
+        // We disregard the broken lightbulbs and use the remaining lightbulbs to represent n-1.
+        var result = x.toLong()
+        var nextZeroBit = 1L
+        while (result and nextZeroBit != 0L) {
+            nextZeroBit = nextZeroBit shl 1
+        }
+        val last = n - 1
+        for (shift in 0..<27) {
+            val isSet = last and (1 shl shift) != 0
+            if (isSet) {
+                result = result or nextZeroBit
+            }
+            nextZeroBit = nextZeroBit shl 1
+            while (nextZeroBit and result != 0L) {
+                nextZeroBit = nextZeroBit shl 1
+            }
+        }
+        return result
+    }
+}
