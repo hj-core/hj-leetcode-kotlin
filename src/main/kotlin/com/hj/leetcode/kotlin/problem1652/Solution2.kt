5e74c5a43bbe7c7a6f9509f56452e95e679fa7b0
hj-core <88603338+hj-core@users.noreply.github.com>
2024-11-18 22:08:55 +0800

    Add a solution with another style

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1652/Solution2.kt
@@ -0,0 +1,29 @@
+package com.hj.leetcode.kotlin.problem1652
+
+/**
+ * LeetCode page: [1652. Defuse the Bomb](https://leetcode.com/problems/defuse-the-bomb/);
+ */
+class Solution2 {
+    /* Complexity:
+     * Time O(N) and Auxiliary Space O(1) where N is the length of code.
+     */
+    fun decrypt(
+        code: IntArray,
+        k: Int,
+    ): IntArray {
+        if (k == 0) {
+            return IntArray(code.size)
+        }
+        val n = code.size
+        val result = IntArray(n)
+        var (start, end) = if (k < 0) k + n to n else 1 to k + 1
+
+        result[0] = (start..<end).sumOf { code[it % n] }
+        for (i in 1..<n) {
+            result[i] = result[i - 1] + code[end % n] - code[start % n]
+            start += 1
+            end += 1
+        }
+        return result
+    }
+}
