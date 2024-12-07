8456902853c3f15e087b6726f17eb62dbd553512
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-07 22:40:48 +0800

    Add a solution to problem 1760 (#552)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1760/Solution.kt
@@ -0,0 +1,48 @@
+package com.hj.leetcode.kotlin.problem1760
+
+/**
+ * LeetCode page: [1760. Minimum Limit of Balls in a Bag](https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(NLogM) and Space O(1)
+     * where N is the size of nums and M is the maximum value in nums.
+     */
+    fun minimumSize(
+        nums: IntArray,
+        maxOperations: Int,
+    ): Int {
+        var low = 1
+        var high = nums.max()
+
+        while (low <= high) {
+            val middle = low + (high - low) / 2
+            if (canReduce(nums, middle, maxOperations)) {
+                high = middle - 1
+            } else {
+                low = middle + 1
+            }
+        }
+        return low
+    }
+
+    private fun canReduce(
+        bagSizes: IntArray,
+        reducedSize: Int,
+        maxOperations: Int,
+    ): Boolean {
+        var totalOps = 0
+        for (size in bagSizes) {
+            totalOps += requiredOps(size, reducedSize)
+            if (totalOps > maxOperations) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun requiredOps(
+        bagSize: Int,
+        reducedSize: Int,
+    ): Int = (bagSize - 1) / reducedSize
+}
