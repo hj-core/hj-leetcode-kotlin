33e9ac048078e9cc291e0dd0ffdfccabeb5a8cfc
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-14 21:28:26 +0800

    Improve comment

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2762/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2762/Solution.kt
@@ -9,7 +9,7 @@ class Solution {
      * where N is the length of nums and K is the allowed value difference.
      */
     fun continuousSubarrays(nums: IntArray): Long {
-        // Using two monotonic dequeues to control the lower bound and upper bound of values.
+        // Using two monotonic deques to control the lower bound and upper bound of values.
         // They store the indices of nums with strictly increasing/decreasing values.
         // nums[preStart+1..=end] >= nums[incDeque.first()]
         // nums[preStart+1..=end] <= nums[decDeque.first()]
