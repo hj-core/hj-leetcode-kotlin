10357d6a4ff5e10f77a245673e2a239898f7e011
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-16 16:27:04 +0800

    Add a solution to problem 3264

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3264/Solution.kt
@@ -0,0 +1,27 @@
+package com.hj.leetcode.kotlin.problem3264
+
+/**
+ * LeetCode page: [3264. Final Array State After K Multiplication Operations I](https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(kN) and Auxiliary Space O(1) where N is the length of nums.
+     */
+    fun getFinalState(
+        nums: IntArray,
+        k: Int,
+        multiplier: Int,
+    ): IntArray {
+        val result = nums.clone()
+        repeat(k) {
+            val i = result.firstIndexOfMin()
+            result[i] *= multiplier
+        }
+        return result
+    }
+
+    private fun IntArray.firstIndexOfMin(): Int =
+        this.foldIndexed(0) { i, acc, elem ->
+            if (elem < this[acc]) i else acc
+        }
+}
