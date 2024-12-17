9bff338548a9733b50caea4f33d006c5b09966be
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-17 11:16:04 +0800

    Add a solution to problem 2182

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2182/Solution.kt
@@ -0,0 +1,57 @@
+package com.hj.leetcode.kotlin.problem2182
+
+import kotlin.math.min
+
+/**
+ * LeetCode page: [2182. Construct String With Repeat Limit](https://leetcode.com/problems/construct-string-with-repeat-limit/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(N) where N is the length of s.
+     */
+    fun repeatLimitedString(
+        s: String,
+        repeatLimit: Int,
+    ): String =
+        buildString(s.length) {
+            val counts = charCounts(s)
+            while (counts.isNotEmpty()) {
+                val largest = counts.last()
+                if (largest.count <= repeatLimit || counts.size == 1) {
+                    val times = min(largest.count, repeatLimit)
+                    repeat(times) { append(largest.char) }
+                    counts.removeLast()
+                } else {
+                    repeat(repeatLimit) { append(largest.char) }
+                    largest.count -= repeatLimit
+
+                    val secondLargest = counts[counts.size - 2]
+                    append(secondLargest.char)
+                    if (secondLargest.count == 1) {
+                        counts.removeAt(counts.size - 2)
+                    } else {
+                        secondLargest.count -= 1
+                    }
+                }
+            }
+        }
+
+    // Return count of each lowercase in s in lexicographical order.
+    private fun charCounts(s: String): MutableList<CharCount> {
+        val counts = IntArray(26)
+        for (c in s) {
+            counts[c - 'a']++
+        }
+
+        return counts.foldIndexed(mutableListOf()) { index, acc, count ->
+            acc.apply {
+                if (count > 0) add(CharCount('a' + index, count))
+            }
+        }
+    }
+
+    private class CharCount(
+        var char: Char,
+        var count: Int,
+    )
+}
