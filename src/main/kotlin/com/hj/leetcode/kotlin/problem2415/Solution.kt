d0cdc08e314fa3801ca6cb9726c3c426314b7919
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-20 13:55:14 +0800

    Swap solution and solution2

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2415/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2415/Solution.kt
@@ -10,13 +10,10 @@ class Solution {
      * Time O(N) and Space O(Log(N)) where N is the number of nodes in root.
      */
     fun reverseOddLevels(root: TreeNode?): TreeNode? {
-        // Caution:
-        // The problem asks to reverse the node values, not the nodes themselves.
-        // This solution demonstrates the capability but doesn't fit the requirement.
         if (root == null) {
             return null
         }
-        root.left = root.right.also { root.right = root.left }
+        // Caution: The problem asks to reverse the node values, not the nodes themselves
         dfs(root.left, root.right, 1)
         return root
     }
@@ -29,12 +26,8 @@ class Solution {
         if (node == null || symmetricNode == null) {
             return
         }
-        if (level % 2 == 0) {
-            node.left = symmetricNode.right.also { symmetricNode.right = node.left }
-            node.right = symmetricNode.left.also { symmetricNode.left = node.right }
-        } else {
-            node.left = symmetricNode.left.also { symmetricNode.left = node.left }
-            node.right = symmetricNode.right.also { symmetricNode.right = node.right }
+        if (level % 2 == 1) {
+            node.`val` = symmetricNode.`val`.also { symmetricNode.`val` = node.`val` }
         }
         dfs(node.left, symmetricNode.right, level + 1)
         dfs(node.right, symmetricNode.left, level + 1)
--- src/main/kotlin/com/hj/leetcode/kotlin/problem2415/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2415/Solution2.kt
@@ -10,10 +10,13 @@ class Solution2 {
      * Time O(N) and Space O(Log(N)) where N is the number of nodes in root.
      */
     fun reverseOddLevels(root: TreeNode?): TreeNode? {
+        // Caution:
+        // The problem asks to reverse the node values, not the nodes themselves.
+        // This solution demonstrates the capability but doesn't fit the requirement.
         if (root == null) {
             return null
         }
-        // Caution: The problem asks to reverse the node values, not the nodes themselves
+        root.left = root.right.also { root.right = root.left }
         dfs(root.left, root.right, 1)
         return root
     }
@@ -26,8 +29,12 @@ class Solution2 {
         if (node == null || symmetricNode == null) {
             return
         }
-        if (level % 2 == 1) {
-            node.`val` = symmetricNode.`val`.also { symmetricNode.`val` = node.`val` }
+        if (level % 2 == 0) {
+            node.left = symmetricNode.right.also { symmetricNode.right = node.left }
+            node.right = symmetricNode.left.also { symmetricNode.left = node.right }
+        } else {
+            node.left = symmetricNode.left.also { symmetricNode.left = node.left }
+            node.right = symmetricNode.right.also { symmetricNode.right = node.right }
         }
         dfs(node.left, symmetricNode.right, level + 1)
         dfs(node.right, symmetricNode.left, level + 1)
