665915c59cd4c0a7c622c41da6749cd5bd064950
hj-core <88603338+hj-core@users.noreply.github.com>
2024-12-30 22:24:11 +0800

    Update problem 2466: Reimplement the solution (#576)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2466/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2466/Solution.kt
@@ -4,62 +4,38 @@ package com.hj.leetcode.kotlin.problem2466
  * LeetCode page: [2466. Count Ways To Build Good Strings](https://leetcode.com/problems/count-ways-to-build-good-strings/);
  */
 class Solution {
-
-    private val mod = 1_000_000_007 // modulo required by the problem
-
     /* Complexity:
-     * Time O(high) and Space O(high-low);
+     * Time O(high) and Space O(high).
      */
-    fun countGoodStrings(low: Int, high: Int, zero: Int, one: Int): Int {
-        /* The idea is to apply dynamic programming. Let subResults[i] be the number of ways
-         * to build a good string of length i.
-         */
-        val subResults = hashMapOf<Int, Int>().apply {
-            updateBaseCases(this)
-            updateRemainingCases(low, high, zero, one, this)
-        }
+    fun countGoodStrings(
+        low: Int,
+        high: Int,
+        zero: Int,
+        one: Int,
+    ): Int {
+        val modulus = 1_000_000_007
+        val (short, long) = if (zero <= one) zero to one else one to zero
 
-        return originalProblem(low, high, subResults)
-    }
+        // Shift everything by short.
+        // dp[L]::= the number of ways to construct length (L+short) string
+        val dp = IntArray(high - short + 1)
+        dp[0] = 1
+        dp[long - short] += 1
 
-    private fun updateBaseCases(subResults: HashMap<Int, Int>) {
-        // There is only one way to build an empty string
-        subResults[0] = 1
-    }
+        for (len in 0..<dp.size - short) {
+            val appendShort = len + short
+            dp[appendShort] = (dp[appendShort] + dp[len]) % modulus
 
-    private fun updateRemainingCases(
-        low: Int, high: Int,
-        zero: Int, one: Int,
-        subResults: HashMap<Int, Int>
-    ) {
-        /* Solve the sub results in increasing order of length using the relation that
-         * subResults[i-zero] + subResults[i-one] = subResults[i].
-         */
-        for (length in 0..high) {
-            val subResult = subResults[length] ?: continue
-
-            // Contribute the sub result of length to the cases that are related to it
-            subResults[length + zero] = subResults[length + zero]
-                ?.let { (it + subResult) % mod }
-                ?: subResult
-            subResults[length + one] = subResults[length + one]
-                ?.let { (it + subResult) % mod }
-                ?: subResult
-
-            // The sub result of length is no longer needed if it is less than low
-            if (length < low) {
-                subResults.remove(length)
+            val appendLong = len + long
+            if (appendLong < dp.size) {
+                dp[appendLong] = (dp[appendLong] + dp[len]) % modulus
             }
         }
-    }
 
-    private fun originalProblem(low: Int, high: Int, subResults: HashMap<Int, Int>): Int {
-        // The original problem is the sum of sub results from low to high
-        var originalProblem = 0
-        for (length in low..high) {
-            val subResult = subResults[length] ?: continue
-            originalProblem = (originalProblem + subResult) % mod
+        var result = 0
+        for (len in low - short..high - short) {
+            result = (result + dp[len]) % modulus
         }
-        return originalProblem
+        return result
     }
 }
