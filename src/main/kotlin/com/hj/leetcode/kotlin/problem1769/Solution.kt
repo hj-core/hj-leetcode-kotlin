07203a34d42a38378c89ac7f8f50103624b0adff
hj-core <88603338+hj-core@users.noreply.github.com>
2025-01-06 16:44:41 +0800

    Improve variable names

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1769/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1769/Solution.kt
@@ -10,19 +10,19 @@ class Solution {
     fun minOperations(boxes: String): IntArray {
         val result = IntArray(boxes.length)
         // Forward pass to account for moving ones in the left
+        var leftCost = 0
         var leftOnes = 0
-        var leftCosts = 0
         for (index in boxes.indices) {
-            leftCosts += leftOnes
-            result[index] += leftCosts
+            leftCost += leftOnes
+            result[index] += leftCost
             leftOnes += if (boxes[index] == '0') 0 else 1
         }
         // Backward pass to account for moving ones in the right
+        var rightCost = 0
         var rightOnes = 0
-        var rightCosts = 0
         for (index in boxes.indices.reversed()) {
-            rightCosts += rightOnes
-            result[index] += rightCosts
+            rightCost += rightOnes
+            result[index] += rightCost
             rightOnes += if (boxes[index] == '0') 0 else 1
         }
         return result
