ffe2173511fa65e0a0dbe429aef564b6b5d7349a
hj-core <88603338+hj-core@users.noreply.github.com>
2025-01-15 18:56:08 +0800

    Add a solution to problem 2429

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2429/Solution.kt
@@ -0,0 +1,61 @@
+package com.hj.leetcode.kotlin.problem2429
+
+/**
+ * LeetCode page: [2429. Minimize XOR](https://leetcode.com/problems/minimize-xor/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(1) and Space O(1);
+     */
+    fun minimizeXor(
+        num1: Int,
+        num2: Int,
+    ): Int {
+        val count1 = num1.countOneBits()
+        val count2 = num2.countOneBits()
+
+        return when {
+            count1 < count2 -> caseNum1LessSetBits(num1, count2 - count1)
+            count1 > count2 -> caseNum1MoreSetBits(num1, count1 - count2)
+            else -> num1
+        }
+    }
+
+    private fun caseNum1MoreSetBits(
+        num1: Int,
+        diff: Int,
+    ): Int {
+        // Compensate for the diff by clearing the first diff set bits,
+        // starting from the LSB.
+        var result = num1
+        var pending = diff
+        var bit = num1.takeLowestOneBit()
+        while (0 < pending) {
+            if (bit and num1 != 0) {
+                result = result xor bit
+                pending--
+            }
+            bit = bit shl 1
+        }
+        return result
+    }
+
+    private fun caseNum1LessSetBits(
+        num1: Int,
+        diff: Int,
+    ): Int {
+        // Compensate for the diff by setting the first diff unset bits,
+        // starting from the smallest position.
+        var result = num1
+        var pending = diff
+        var bit = 1
+        while (0 < pending) {
+            if (bit and num1 == 0) {
+                result = result xor bit
+                pending--
+            }
+            bit = bit shl 1
+        }
+        return result
+    }
+}
