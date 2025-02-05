03790077f19f4d895aed9f47a8910ca75daa5778
hj-core <88603338+hj-core@users.noreply.github.com>
2025-02-05 22:52:20 +0800

    Add a solution to problem 1790 (#615)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1790/Solution.kt
@@ -0,0 +1,27 @@
+package com.hj.leetcode.kotlin.problem1790
+
+/**
+ * LeetCode page: [1790. Check if One String Swap Can Make Strings Equal](https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(N) and Space O(1) where N is the length of `s1 `and `s2`.
+     */
+    fun areAlmostEqual(
+        s1: String,
+        s2: String,
+    ): Boolean =
+        s1
+            .asSequence()
+            .zip(s2.asSequence())
+            .filter { (a, b) -> a != b }
+            .take(3)
+            .toList()
+            .let {
+                when (it.size) {
+                    0 -> true
+                    2 -> it[0].first == it[1].second && it[0].second == it[1].first
+                    else -> false
+                }
+            }
+}
