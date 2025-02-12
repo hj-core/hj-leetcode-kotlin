19be1a7fce7e4ace477262576daec1183bf31b4e
hj-core <88603338+hj-core@users.noreply.github.com>
2025-02-12 22:48:13 +0800

    Add a solution to problem 2342 (#622)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2342/Solution.kt
@@ -0,0 +1,38 @@
+package com.hj.leetcode.kotlin.problem2342
+
+/**
+ * LeetCode page: [2342. Max Sum of a Pair With Equal Sum of Digits](https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/);
+ */
+class Solution {
+    /* Complexity:
+     * Time O(LogP) and Space O(LogM)
+     * where P is the product of numbers and M is the maximum number in `nums`.
+     */
+    fun maximumSum(nums: IntArray): Int {
+        var result = -1
+        val bestPeers = mutableMapOf<Int, Int>() // entry=(digitSum, bestPeerValue)
+
+        // Pair each nums[j] with the best peer before it and track the maximum sum
+        for (j in nums.indices) {
+            val digitSum = digitSum(nums[j])
+            if (digitSum in bestPeers) {
+                val bestPeer = bestPeers[digitSum]!!
+                result = maxOf(result, nums[j] + bestPeer)
+                bestPeers[digitSum] = maxOf(bestPeer, nums[j])
+            } else {
+                bestPeers[digitSum] = nums[j]
+            }
+        }
+        return result
+    }
+
+    private fun digitSum(x: Int): Int {
+        var result = 0
+        var left = x
+        while (left > 0) {
+            result += left % 10
+            left /= 10
+        }
+        return result
+    }
+}
