d9e591231ebf97baaa57a859f094cd9f02452faf
hj-core <88603338+hj-core@users.noreply.github.com>
2025-02-23 18:40:07 +0800

    Add solutions to problem 889 (#633)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem889/Solution2.kt
@@ -0,0 +1,42 @@
+package com.hj.leetcode.kotlin.problem889
+
+import com.hj.leetcode.kotlin.common.model.TreeNode
+
+/**
+ * LeetCode page: [889. Construct Binary Tree from Preorder and Postorder Traversal](https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/);
+ */
+class Solution2 {
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of `preorder` and `postorder`.
+    fun constructFromPrePost(
+        preorder: IntArray,
+        postorder: IntArray,
+    ): TreeNode? {
+        if (preorder.isEmpty()) {
+            return null
+        }
+        var postIndex = 0
+        val stack = mutableListOf<TreeNode>()
+
+        val result = TreeNode(preorder[0])
+        stack.add(result)
+
+        for (preIndex in 1..<preorder.size) {
+            var isLeft = true
+            while (stack.last().`val` == postorder[postIndex]) {
+                isLeft = false
+                stack.removeLast()
+                postIndex++
+            }
+
+            val node = TreeNode(preorder[preIndex])
+            if (isLeft) {
+                stack.last().left = node
+            } else {
+                stack.last().right = node
+            }
+            stack.add(node)
+        }
+        return result
+    }
+}
