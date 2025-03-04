b428e5e1901e687a0f26b326d424aa20c6c106f0
hj-core <88603338+hj-core@users.noreply.github.com>
2025-03-04 20:31:22 +0800

    Add a solution to problem 1780 (#642)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1780/Solution.kt
@@ -0,0 +1,10 @@
+package com.hj.leetcode.kotlin.problem1780
+
+/**
+ * LeetCode page: [1780. Check if Number is a Sum of Powers of Three](https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(Log(n)) and Space O(Log(n)).
+    fun checkPowersOfThree(n: Int): Boolean = n.toString(radix = 3).all { it != '2' }
+}
