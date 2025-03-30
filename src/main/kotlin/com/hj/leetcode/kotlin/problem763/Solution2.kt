012ac4fa4f7f756df455300429996da6c2afa152
hj-core <88603338+hj-core@users.noreply.github.com>
2025-03-30 11:45:13 +0800

    Add a two pointers solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem763/Solution2.kt
@@ -0,0 +1,31 @@
+package com.hj.leetcode.kotlin.problem763
+
+/**
+ * LeetCode page: [763. Partition Labels](https://leetcode.com/problems/partition-labels/);
+ */
+class Solution2 {
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length s.
+    // Here, we treat the size of the charset, which is 26 in this problem, as constant.
+    fun partitionLabels(s: String): List<Int> {
+        val lastIndices = IntArray(26) { s.length }
+        for ((index, c) in s.withIndex()) {
+            lastIndices[c - 'a'] = index
+        }
+
+        var partitionStart = 0
+        var partitionEnd = -1
+        val result = mutableListOf<Int>()
+
+        for ((index, c) in s.withIndex()) {
+            val lastIndex = lastIndices[c - 'a']
+            partitionEnd = maxOf(partitionEnd, lastIndex)
+
+            if (index == partitionEnd) {
+                result.add(index - partitionStart + 1)
+                partitionStart = index + 1
+            }
+        }
+        return result
+    }
+}
