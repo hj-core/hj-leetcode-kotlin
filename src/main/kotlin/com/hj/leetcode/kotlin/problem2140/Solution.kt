0a808b60c47d81d3238a2535e33b219fa32e59b6
hj-core <88603338+hj-core@users.noreply.github.com>
2025-04-01 18:23:48 +0800

    Add a solution to problem 2140

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2140/Solution.kt
@@ -0,0 +1,19 @@
+package com.hj.leetcode.kotlin.problem2140
+
+/**
+ * LeetCode page: [2140. Solving Questions With Brainpower](https://leetcode.com/problems/solving-questions-with-brainpower/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of question.
+    fun mostPoints(questions: Array<IntArray>): Long {
+        val n = questions.size
+        val dp = LongArray(n + 1) // dp[i]::= mostPoints(question[i..<n])
+
+        for (i in questions.indices.reversed()) {
+            val (points, brainpower) = questions[i]
+            dp[i] = maxOf(dp[i + 1], points + dp[minOf(i + brainpower + 1, n)])
+        }
+        return dp[0]
+    }
+}
