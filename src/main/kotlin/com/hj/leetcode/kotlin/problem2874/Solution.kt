367770751693b1be99c0dbc00878691a50e666fd
hj-core <88603338+hj-core@users.noreply.github.com>
2025-04-03 21:49:13 +0800

    Add solutions to problem 2874 (#673)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2874/Solution.kt
@@ -0,0 +1,13 @@
+package com.hj.leetcode.kotlin.problem2874
+
+/**
+ * LeetCode page: [2874. Maximum Value of an Ordered Triplet II](https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of nums.
+    fun maximumTripletValue(nums: IntArray): Long =
+        com.hj.leetcode.kotlin.problem2873
+            .Solution()
+            .maximumTripletValue(nums)
+}
---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2874/Solution2.kt
@@ -0,0 +1,13 @@
+package com.hj.leetcode.kotlin.problem2874
+
+/**
+ * LeetCode page: [2874. Maximum Value of an Ordered Triplet II](https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/);
+ */
+class Solution2 {
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of nums.
+    fun maximumTripletValue(nums: IntArray): Long =
+        com.hj.leetcode.kotlin.problem2873
+            .Solution2()
+            .maximumTripletValue(nums)
+}
