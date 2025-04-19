c8bd71710eb94a0c0a17c6e211e64cbb952d6aa5
hj-core <88603338+hj-core@users.noreply.github.com>
2025-04-19 10:18:24 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2563/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2563/Solution.kt
@@ -4,36 +4,34 @@ package com.hj.leetcode.kotlin.problem2563
  * LeetCode page: [2563. Count the Number of Fair Pairs](https://leetcode.com/problems/count-the-number-of-fair-pairs/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(NLogN) and Space O(N) where N is the size of nums.
-     */
+    // Complexity:
+    // Time O(NLogN) and Space O(N) where N is the length of nums.
     fun countFairPairs(
         nums: IntArray,
         lower: Int,
         upper: Int,
     ): Long {
-        // We can swap i and j while still satisfying the sum constraint
-        // One-to-one mapping between fair pairs in nums and in sortedNums
         val sortedNums = nums.sorted()
-        var result = 0L
-        var left = sortedNums.lastIndex // last index that violates lower
-        var right = sortedNums.lastIndex // last index that satisfies upper
 
-        for ((i, num) in sortedNums.withIndex()) {
-            while (0 <= left && lower <= num + sortedNums[left]) {
-                left -= 1
+        // The valid pairs for the left are in the range of (minRight, maxRight]
+        var minRight = sortedNums.lastIndex
+        var maxRight = sortedNums.lastIndex
+        var result = 0L
+
+        for (left in sortedNums.indices) {
+            while (left < maxRight && upper < sortedNums[left] + sortedNums[maxRight]) {
+                maxRight--
             }
-            while (0 <= right && upper < num + sortedNums[right]) {
-                right -= 1
+
+            minRight = maxOf(left, minOf(minRight, maxRight))
+            while (left < minRight && lower <= sortedNums[left] + sortedNums[minRight]) {
+                minRight--
             }
-            // Limit the pairing indices to the left of i to avoid duplicates
-            val smallerPairs =
-                when {
-                    i <= left -> 0
-                    i <= right -> (i - left) - 1 // one for i itself
-                    else -> right - left
-                }
-            result += smallerPairs
+
+            if (maxRight == left) {
+                break
+            }
+            result += maxRight - minRight
         }
         return result
     }
