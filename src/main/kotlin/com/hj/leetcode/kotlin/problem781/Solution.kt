7b3cecc027401ad4adf09a06fbe81fddb0a5dcd2
hj-core <88603338+hj-core@users.noreply.github.com>
2025-04-20 18:26:13 +0800

    Add a solution to problem 781 (#689)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem781/Solution.kt
@@ -0,0 +1,23 @@
+package com.hj.leetcode.kotlin.problem781
+
+/**
+ * LeetCode page: [781. Rabbits in Forest](https://leetcode.com/problems/rabbits-in-forest/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of answers.
+    fun numRabbits(answers: IntArray): Int {
+        val groups = mutableMapOf<Int, Int>() // entry=(answer, known members)
+        var result = 0
+
+        for (answer in answers) {
+            val knownMembers = 1 + (groups[answer] ?: 0)
+
+            if (knownMembers == 1) {
+                result += answer + 1
+            }
+            groups[answer] = if (knownMembers == answer + 1) 0 else knownMembers
+        }
+        return result
+    }
+}
