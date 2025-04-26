97f51f6dbc9c67956943f29f17e8d5bb08d1c8a1
hj-core <88603338+hj-core@users.noreply.github.com>
2025-04-26 12:47:41 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2444/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2444/Solution.kt
@@ -9,27 +9,34 @@ class Solution {
     /* Complexity:
      * Time O(N) and Space O(1) where N is the size of nums;
      */
-    fun countSubarrays(nums: IntArray, minK: Int, maxK: Int): Long {
+    fun countSubarrays(
+        nums: IntArray,
+        minK: Int,
+        maxK: Int,
+    ): Long {
+        // We consider the number of fixed-bound subarrays for each subarray end index,
+        // where the range of valid start indices is determined by the index of the last
+        // out-of-bounds element, as well as the last occurrences of minK and maxK.
         var result = 0L
-        var boundary = -1
+        var leftBound = -1
         var latestMin = -1
         var latestMax = -1
 
-        for ((end, num) in nums.withIndex()) {
+        for ((right, num) in nums.withIndex()) {
             if (num !in minK..maxK) {
-                boundary = end
+                leftBound = right
                 continue
             }
 
             if (num == minK) {
-                latestMin = end
+                latestMin = right
             }
             if (num == maxK) {
-                latestMax = end
+                latestMax = right
             }
 
-            if (boundary < latestMin && boundary < latestMax) {
-                result += min(latestMin, latestMax) - boundary
+            if (leftBound < latestMin && leftBound < latestMax) {
+                result += min(latestMin, latestMax) - leftBound
             }
         }
         return result
