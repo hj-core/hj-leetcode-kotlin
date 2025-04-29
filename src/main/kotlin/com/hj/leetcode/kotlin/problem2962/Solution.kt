d1b04c5a619fa8765ff193ac81150d43d5ccdffd
hj-core <88603338+hj-core@users.noreply.github.com>
2025-04-29 16:50:16 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2962/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2962/Solution.kt
@@ -7,28 +7,28 @@ class Solution {
     /* Complexity:
      * Time O(N) and Space O(1) where N is the size of nums;
      */
-    fun countSubarrays(nums: IntArray, k: Int): Long {
+    fun countSubarrays(
+        nums: IntArray,
+        k: Int,
+    ): Long {
+        // For each possible end index of a subarray, we consider the earliest start index
+        // such that nums[start..=end] becomes invalid.
         val max = nums.max()
         var result = 0L
         var start = 0
-        var countMax = 0
+        var maxCount = 0
 
-        // For each end, try to find the latest start that contains k maxes
         for (end in nums.indices) {
             if (nums[end] == max) {
-                countMax += 1
+                maxCount++
             }
-
-            while (start <= end && (nums[start] != max || countMax > k)) {
+            while (maxCount >= k) {
                 if (nums[start] == max) {
-                    countMax -= 1
+                    maxCount--
                 }
-                start += 1
+                start++
             }
-
-            if (countMax == k) {
-                result += start + 1
-            }
+            result += start
         }
         return result
     }
