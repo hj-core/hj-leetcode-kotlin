24a954361bde2233c94fd33909d89b62d608f63e
hj-core <88603338+hj-core@users.noreply.github.com>
2025-05-07 15:44:35 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3341/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3341/Solution.kt
@@ -6,68 +6,47 @@ import java.util.*
  * LeetCode page: [3341. Find Minimum Time to Reach Last Room I](https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/);
  */
 class Solution {
-    val maxN = 50
-    val maxM = 50
-    val maxMoveTime = 1_000_000_000
-
     // Complexity:
     // Time O(NM*Log(NM)) and Space O(NM) where N and M are the number of rows
     // and columns of moveTime, respectively.
     fun minTimeToReach(moveTime: Array<IntArray>): Int {
-        val infMoveTime = maxMoveTime + maxN + maxM
-        val n = moveTime.size
-        val m = moveTime[0].size
-
-        val minTime = Array(n) { IntArray(m) { infMoveTime } }
         val pq = PriorityQueue<Cell>(compareBy(Cell::minTime))
-        minTime[0][0] = 0
-        pq.offer(Cell(0, 0, 0))
+        val visited = Array(moveTime.size) { BooleanArray(moveTime[0].size) }
+        val moves = arrayOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
 
+        pq.offer(Cell(0, 0, 0))
         while (pq.isNotEmpty()) {
             val curr = pq.poll()
-            if (curr.row == n - 1 && curr.col == m - 1) {
-                break
+            if (curr.row == moveTime.size - 1 && curr.col == moveTime[0].size - 1) {
+                return curr.minTime
             }
-            if (curr.minTime > minTime[curr.row][curr.col]) {
+
+            if (visited[curr.row][curr.col]) {
                 continue
             }
+            visited[curr.row][curr.col] = true
 
-            for (neighbour in getNeighbours(curr, n, m, minTime)) {
-                val time = 1 + maxOf(curr.minTime, moveTime[neighbour.row][neighbour.col])
-                if (time < neighbour.minTime) {
-                    minTime[neighbour.row][neighbour.col] = time
-                    neighbour.minTime = time
-                    pq.add(neighbour)
+            for ((dr, dc) in moves) {
+                val neighRow = curr.row + dr
+                val neighCol = curr.col + dc
+                if (neighRow !in moveTime.indices || neighCol !in moveTime[neighRow].indices || visited[neighRow][neighCol]) {
+                    continue
                 }
+                val neighTime = 1 + maxOf(curr.minTime, moveTime[neighRow][neighCol])
+                pq.offer(Cell(neighRow, neighCol, neighTime))
             }
         }
-        return minTime[n - 1][m - 1]
+        throw IllegalStateException("Code should not reach here")
     }
 
-    private class Cell(
-        val row: Int,
-        val col: Int,
-        var minTime: Int,
-    )
+    @JvmInline
+    value class Cell private constructor(
+        val data: IntArray,
+    ) {
+        val row: Int get() = data[0]
+        val col: Int get() = data[1]
+        val minTime: Int get() = data[2]
 
-    private fun getNeighbours(
-        curr: Cell,
-        n: Int,
-        m: Int,
-        minTime: Array<IntArray>,
-    ): List<Cell> =
-        buildList {
-            if (curr.row > 0) {
-                add(Cell(curr.row - 1, curr.col, minTime[curr.row - 1][curr.col]))
-            }
-            if (curr.row < n - 1) {
-                add(Cell(curr.row + 1, curr.col, minTime[curr.row + 1][curr.col]))
-            }
-            if (curr.col > 0) {
-                add(Cell(curr.row, curr.col - 1, minTime[curr.row][curr.col - 1]))
-            }
-            if (curr.col < m - 1) {
-                add(Cell(curr.row, curr.col + 1, minTime[curr.row][curr.col + 1]))
-            }
-        }
+        constructor(row: Int, col: Int, minTime: Int) : this(data = intArrayOf(row, col, minTime))
+    }
 }
