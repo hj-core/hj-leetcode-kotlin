47dfd9e83ac607fdc0a3f83d69a64692c1a07614
hj-core <88603338+hj-core@users.noreply.github.com>
2025-05-07 16:49:33 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3341/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3341/Solution.kt
@@ -10,21 +10,17 @@ class Solution {
     // Time O(NM*Log(NM)) and Space O(NM) where N and M are the number of rows
     // and columns of moveTime, respectively.
     fun minTimeToReach(moveTime: Array<IntArray>): Int {
+        val moves = arrayOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
         val pq = PriorityQueue<Cell>(compareBy(Cell::minTime))
         val visited = Array(moveTime.size) { BooleanArray(moveTime[0].size) }
-        val moves = arrayOf(0 to 1, 0 to -1, 1 to 0, -1 to 0)
 
         pq.offer(Cell(0, 0, 0))
+        visited[0][0] = true
         while (pq.isNotEmpty()) {
             val curr = pq.poll()
             if (curr.row == moveTime.size - 1 && curr.col == moveTime[0].size - 1) {
                 return curr.minTime
             }
-
-            if (visited[curr.row][curr.col]) {
-                continue
-            }
-            visited[curr.row][curr.col] = true
 
             for ((dr, dc) in moves) {
                 val neighRow = curr.row + dr
@@ -38,6 +34,7 @@ class Solution {
 
                 val neighTime = 1 + maxOf(curr.minTime, moveTime[neighRow][neighCol])
                 pq.offer(Cell(neighRow, neighCol, neighTime))
+                visited[neighRow][neighCol] = true
             }
         }
         throw IllegalStateException("Code should not reach here")
