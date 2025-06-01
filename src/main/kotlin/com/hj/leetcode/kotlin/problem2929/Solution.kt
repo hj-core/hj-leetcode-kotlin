3de8acca84aeceacd0af20a9c04883c01fc8fda8
hj-core <88603338+hj-core@users.noreply.github.com>
2025-06-01 22:14:07 +0800

    Add a solution to problem 2929 (#730)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2929/Solution.kt
@@ -0,0 +1,34 @@
+package com.hj.leetcode.kotlin.problem2929
+
+/**
+ * LeetCode page: [2929. Distribute Candies Among Children II](https://leetcode.com/problems/distribute-candies-among-children-ii/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(1) and Space O(1).
+    fun distributeCandies(
+        n: Int,
+        limit: Int,
+    ): Long {
+        val minSumAB = maxOf(0, n - limit)
+        val maxSumAB = minOf(n, limit * 2)
+        if (minSumAB > maxSumAB) {
+            return 0L
+        }
+        return distributeAB(maxSumAB, limit) - distributeAB(minSumAB - 1, limit)
+    }
+
+    // `distributeAB` returns the number of (a, b) pairs so that both a and b
+    // are in the range [0, limit], and their sum is not greater than maxSum.
+    private fun distributeAB(
+        maxSum: Int,
+        limit: Int,
+    ): Long =
+        if (maxSum < 0) {
+            0L
+        } else if (maxSum <= limit) {
+            (1L + maxSum) * (2L + maxSum) / 2
+        } else {
+            (1L + limit) * (1L + limit) - distributeAB(limit * 2 - maxSum - 1, limit)
+        }
+}
