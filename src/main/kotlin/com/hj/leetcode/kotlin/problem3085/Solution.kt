b3def6709ab8652730e7dde3d2291f92d8b313f6
hj-core <88603338+hj-core@users.noreply.github.com>
2025-06-21 23:01:51 +0800

    Add a solution to problem 3085 (#747)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3085/Solution.kt
@@ -0,0 +1,49 @@
+package com.hj.leetcode.kotlin.problem3085
+
+/**
+ * LeetCode page: [3085. Minimum Deletions to Make String K-Special](https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/);
+ */
+class Solution {
+    // Complexity;
+    // Time O(N+SLogS) and Space O(S) where N is the length of word and
+    // S is the number of allowed characters.
+    fun minimumDeletions(
+        word: String,
+        k: Int,
+    ): Int {
+        val freq = IntArray(26)
+        for (c in word) {
+            freq[c - 'a']++
+        }
+
+        freq.sort()
+        var result = word.length
+        // freq[0..<prefix] < freq[i] and freq[suffix..] > freq[i]+k
+        var prefix = 0
+        var suffix = 0
+        var prefixSum = 0 // The sum of freq[0..<prefix]
+        var suffixSum = word.length // The sum of freq[suffix..]
+
+        // Try each freq[i] as the minimum frequency after deletions
+        for (i in freq.indices) {
+            if (freq[i] == 0) {
+                prefix = i
+                suffix = i
+                continue
+            }
+
+            while (freq[prefix] < freq[i]) {
+                prefixSum += freq[prefix]
+                prefix++
+            }
+            while (suffix < freq.size && freq[suffix] <= freq[i] + k) {
+                suffixSum -= freq[suffix]
+                suffix++
+            }
+
+            val subResult = prefixSum + suffixSum - (freq[i] + k) * (freq.size - suffix)
+            result = minOf(result, subResult)
+        }
+        return result
+    }
+}
