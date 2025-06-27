e4570b6f7e33a9e34163f4a1e2b1e626aa7f7aa9
hj-core <88603338+hj-core@users.noreply.github.com>
2025-06-27 18:27:51 +0800

    Add a solution to problem 2014

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2014/Solution.kt
@@ -0,0 +1,111 @@
+package com.hj.leetcode.kotlin.problem2014
+
+/**
+ * LeetCode page: [2014. Longest Subsequence Repeated k Times](https://leetcode.com/problems/longest-subsequence-repeated-k-times/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(???) and Space O(???).
+    fun longestSubsequenceRepeatedK(
+        s: String,
+        k: Int,
+    ): String {
+        val freq = countLowercases(s)
+        toMaxOccurrences(freq, k)
+        return dfs(pruneChars(s, freq), k, freq).joinToString(separator = "")
+    }
+
+    // Returns the frequency of each lowercase.
+    private fun countLowercases(s: String): IntArray {
+        val result = IntArray(26)
+        for (c in s) {
+            result[c - 'a']++
+        }
+        return result
+    }
+
+    // Modifies the freq to the maximum number of occurrences in a subsequence
+    // that is k-repeated.
+    private fun toMaxOccurrences(
+        freq: IntArray,
+        k: Int,
+    ) {
+        for (i in freq.indices) {
+            freq[i] /= k
+        }
+    }
+
+    // Returns a new string with the characters removed from s that cannot
+    // be part of a k-repeated subsequence.
+    private fun pruneChars(
+        s: String,
+        maxOccur: IntArray,
+    ): String {
+        val builder = StringBuilder()
+        for (c in s) {
+            if (maxOccur[c - 'a'] > 0) {
+                builder.append(c)
+            }
+        }
+        return builder.toString()
+    }
+
+    // Returns the longest subsequence that is k-repeated, opting for the
+    // lexicographically largest one if there are multiple such subsequences.
+    private fun dfs(
+        s: String,
+        k: Int,
+        // Extra occurrence of each lowercase that can be added to the
+        // subsequence.
+        extraOccur: IntArray,
+        subsequence: MutableList<Char> = mutableListOf(),
+    ): List<Char> {
+        if (!isKRepeated(s, k, subsequence)) {
+            return emptyList()
+        }
+
+        var result = subsequence.toList()
+        for (i in 25 downTo 0) {
+            if (extraOccur[i] == 0) {
+                continue
+            }
+            extraOccur[i]--
+            subsequence.add('a' + i)
+            val next = dfs(s, k, extraOccur, subsequence)
+
+            if (next.size > result.size) {
+                result = next
+            }
+            subsequence.removeLast()
+            extraOccur[i]++
+        }
+        return result
+    }
+
+    // Returns whether the subsequence is k-repeated.
+    private fun isKRepeated(
+        s: String,
+        k: Int,
+        subsequence: List<Char>,
+    ): Boolean {
+        if (subsequence.isEmpty()) {
+            return true
+        }
+
+        var cnt = 0
+        var i = 0
+        for (c in s) {
+            if (c == subsequence[i]) {
+                i++
+                if (i == subsequence.size) {
+                    i = 0
+                    cnt++
+                    if (cnt == k) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+}
