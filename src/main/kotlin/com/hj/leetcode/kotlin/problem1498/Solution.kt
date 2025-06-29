7497b3eefe79eae3e0134add80aee98567604b44
hj-core <88603338+hj-core@users.noreply.github.com>
2025-06-29 23:14:15 +0800

    Update problem 1498: Refactor implementation (#755)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1498/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1498/Solution.kt
@@ -4,68 +4,65 @@ package com.hj.leetcode.kotlin.problem1498
  * LeetCode page: [1498. Number of Subsequences That Satisfy the Given Sum Condition](https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(NLogN) and Space O(N) where N is the size of nums;
-     */
-    fun numSubseq(nums: IntArray, target: Int): Int {
-        /* Instead of finding the result of nums, we can find the result of the sorted nums
-         * since their subsequences are one to one mapping.
-         */
-        val sortedNums = nums.sorted()
-        var result = 0
-        val mod = 1_000_000_007
-
-        /* For each suffix array of sorted nums, find the result of it with a constraint that
-         * its first element must be included, and add to the final result.
-         *
-         * Two pointers technique is used to determine the maximum index that the subsequence
-         * can extend to.
-         */
-        var suffixStart = 0
-        var maxEndIndex = nums.lastIndex
-
-        while (suffixStart <= maxEndIndex) {
-            // Find the max index that the subsequence can extend to
-            while (
-                suffixStart <= maxEndIndex &&
-                sortedNums[suffixStart] + sortedNums[maxEndIndex] > target
-            ) {
-                maxEndIndex--
-            }
-
-            // Compute the sub result and add it to the final result
-            val maxTailLength = maxEndIndex - suffixStart
-            val subResult = when {
-                maxTailLength < 0 -> 0
-                maxTailLength == 0 -> 1
-                else -> normalizedNumSubsequences(maxTailLength, mod)
-            }
-            result = (result + subResult) % mod
-
-            suffixStart++
-        }
-
-        // Return the final result.
-        return result
-    }
-
-    private fun normalizedNumSubsequences(length: Int, mod: Int): Int {
-        if (length <= 0) {
+    // Complexity:
+    // Time O(NLogN) and Space O(N) where N is the length of nums.
+    fun numSubseq(
+        nums: IntArray,
+        target: Int,
+    ): Int {
+        val sortedNums = nums.sortedArray()
+        if (sortedNums[0] * 2 > target) {
             return 0
         }
 
-        if (length == 1) {
-            return 2 % mod
+        var result = 0
+        var left = 0
+        var right = partition(sortedNums, target - sortedNums[left]) - 1
+        val modulo = 1_000_000_007
+        val shlMod = computeShlModTable(right, modulo)
+
+        while (left <= right) {
+            if (sortedNums[left] + sortedNums[right] > target) {
+                right--
+            } else {
+                result = (result + shlMod[right - left]) % modulo
+                left++
+            }
         }
-
-        val halfLength = length / 2
-        val halfLengthResult = normalizedNumSubsequences(halfLength, mod).toLong()
-        val oddFactor = if (length.isOdd()) 2 else 1
-
-        return halfLengthResult.let {
-            (((it * it) % mod) * oddFactor) % mod
-        }.toInt()
+        return result
     }
 
-    private fun Int.isOdd() = this and 1 == 1
+    // Returns the number of elements in sortedNums that are not greater
+    // than threshold.
+    private fun partition(
+        sortedNums: IntArray,
+        threshold: Int,
+    ): Int {
+        // Our result is in the range [left, right]
+        var left = 0
+        var right = sortedNums.size
+
+        while (left < right) {
+            val mid = left + (right - left) / 2
+            if (sortedNums[mid] <= threshold) {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+        return left
+    }
+
+    // Returns a lookup table where the value at index i equals 2^i % modulo.
+    private fun computeShlModTable(
+        maxExp: Int,
+        modulo: Int,
+    ): IntArray {
+        val result = IntArray(maxExp + 1)
+        result[0] = 1
+        for (i in 1..<result.size) {
+            result[i] = (result[i - 1] * 2) % modulo
+        }
+        return result
+    }
 }
