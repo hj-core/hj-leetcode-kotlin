86a6accac7f016014160c7b2730c98465dfc4db2
hj-core <88603338+hj-core@users.noreply.github.com>
2025-07-11 21:40:50 +0800

    Add a solution to problem 2402 (#769)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2402/Solution.kt
@@ -0,0 +1,44 @@
+package com.hj.leetcode.kotlin.problem2402
+
+import java.util.*
+
+/**
+ * LeetCode page: [2402. Meeting Rooms III](https://leetcode.com/problems/meeting-rooms-iii/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(nLog(n)+MLogM+MLog(n)) and Space O(n+M) where M is the length
+    // of meetings.
+    fun mostBooked(
+        n: Int,
+        meetings: Array<IntArray>,
+    ): Int {
+        val freeRooms = PriorityQueue<Int>()
+        for (i in 0..<n) {
+            freeRooms.add(i)
+        }
+
+        val usedRooms = PriorityQueue<Pair<Int, Long>>(compareBy { (room, end) -> end })
+        val bookCount = IntArray(n)
+        var newStart = 0L
+        val sortedMeetings = meetings.sortedBy { (start, _) -> start }
+
+        for (meeting in sortedMeetings) {
+            newStart = maxOf(newStart, meeting[0].toLong())
+            if (freeRooms.isEmpty()) {
+                newStart = maxOf(newStart, usedRooms.peek().second)
+            }
+
+            while (usedRooms.isNotEmpty() && usedRooms.peek().second <= newStart) {
+                val room = usedRooms.poll().first
+                freeRooms.add(room)
+            }
+
+            val room = freeRooms.poll()
+            val end = newStart + meeting[1] - meeting[0]
+            usedRooms.add(Pair(room, end))
+            bookCount[room]++
+        }
+        return bookCount.withIndex().maxBy { it.value }.index
+    }
+}
