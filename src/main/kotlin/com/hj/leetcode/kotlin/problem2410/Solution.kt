971bdef65bc9b55ab62dd327253e127a9d49fe09
hj-core <88603338+hj-core@users.noreply.github.com>
2025-07-13 10:09:20 +0800

    Add a solution to problem 2410

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2410/Solution.kt
@@ -0,0 +1,30 @@
+package com.hj.leetcode.kotlin.problem2410
+
+/**
+ * LeetCode page: [2410. Maximum Matching of Players With Trainers](https://leetcode.com/problems/maximum-matching-of-players-with-trainers/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(NLogN+MLogM) and Space O(N+M) where N is the length of
+    // players and M is the length of trainers.
+    fun matchPlayersAndTrainers(
+        players: IntArray,
+        trainers: IntArray,
+    ): Int {
+        val sortedPlayers = players.sortedArray()
+        val sortedTrainers = trainers.sortedArray()
+
+        var result = 0
+        var i = 0
+        for (trainer in sortedTrainers) {
+            if (sortedPlayers[i] <= trainer) {
+                result++
+                i++
+                if (i == sortedPlayers.size) {
+                    break
+                }
+            }
+        }
+        return result
+    }
+}
