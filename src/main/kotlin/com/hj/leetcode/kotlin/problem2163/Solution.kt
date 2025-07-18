8d59bc9af644806164ab706fb3bc1810be014ab0
hj-core <88603338+hj-core@users.noreply.github.com>
2025-07-18 12:39:32 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2163/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2163/Solution.kt
@@ -13,23 +13,12 @@ class Solution {
 
         // nSmallestSumL[i]:= the sum of the n smallest elements from
         // nums[0..<n+i].
-        val nSmallestSumL = LongArray(n + 1)
-        val nSmallestL = PriorityQueue<Int>(reverseOrder())
+        val nSmallestSumL = computeNSmallestSumL(nums)
 
-        for (j in 0..<n) {
-            nSmallestL.add(nums[j])
-            nSmallestSumL[0] += nums[j]
-        }
-        for (i in 1..n) {
-            nSmallestL.add(nums[n + i - 1])
-            nSmallestSumL[i] = nSmallestSumL[i - 1] + nums[n + i - 1] - nSmallestL.poll()
-        }
-
-        // nLargestSumR[i]:= the sum of the n largest elements from
-        // nums[n+i..<3n].
-        var nLargestSumR = 0L // Initialized with i = n
+        // nLargestSumR@i:= the sum of the n largest elements from
+        // nums[n+i..<3n], initialized with i = n.
+        var nLargestSumR = 0L
         val nLargestR = PriorityQueue<Int>()
-
         for (j in 2 * n..<3 * n) {
             nLargestR.add(nums[j])
             nLargestSumR += nums[j]
@@ -41,6 +30,25 @@ class Solution {
             nLargestR.add(nums[n + i])
             nLargestSumR += nums[n + i] - nLargestR.poll()
             result = minOf(result, nSmallestSumL[i] - nLargestSumR)
+        }
+        return result
+    }
+
+    // Returns a length n+1 array where the value at index i is the sum
+    // of the n smallest elements from nums[0..<n+i]. The length of nums
+    // is 3*n.
+    private fun computeNSmallestSumL(nums: IntArray): LongArray {
+        val n = nums.size / 3
+        val result = LongArray(n + 1)
+        val nSmallestL = PriorityQueue<Int>(reverseOrder())
+
+        for (j in 0..<n) {
+            nSmallestL.add(nums[j])
+            result[0] += nums[j]
+        }
+        for (i in 1..n) {
+            nSmallestL.add(nums[n + i - 1])
+            result[i] = result[i - 1] + nums[n + i - 1] - nSmallestL.poll()
         }
         return result
     }
