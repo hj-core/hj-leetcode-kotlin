1737dc5dee630fdf685723ea7abd9fdb275debac
hj-core <88603338+hj-core@users.noreply.github.com>
2025-07-19 22:58:07 +0800

    Update Problem 1233: Add new solution (#778)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1233/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1233/Solution.kt
@@ -5,57 +5,71 @@ package com.hj.leetcode.kotlin.problem1233
  */
 class Solution {
     /* Complexity:
-     * Time O(N) and Space O(N) where N is the flattened length of folder.
+     * Time O(M) and Space O(M) where M is the flattened length of folder.
      */
-    fun removeSubfolders(folder: Array<String>): List<String> {
-        val root = Trie()
-        root.retainNonSubfolders(folder)
+    fun removeSubfolders(folder: Array<String>): List<String> = NonSubFolders(folder).toList()
+}
 
-        return buildList {
-            dfs(root) { node ->
-                add(folder[node.terminationId])
-            }
+private class NonSubFolders(
+    val folders: Array<String>,
+) {
+    private val root = TrieNode()
+
+    init {
+        for ((i, f) in folders.withIndex()) {
+            root.mergeNew(i, f)
         }
     }
 
-    private class Trie {
-        val children = mutableMapOf<Char, Trie>()
-        var terminationId = -1
-
-        fun retainNonSubfolders(allFolders: Array<String>) {
-            for ((index, path) in allFolders.withIndex()) {
-                merge(index, path)
-            }
+    fun toList(): List<String> =
+        buildList {
+            dfs(root) { add(folders[it]) }
         }
-
-        fun merge(
-            id: Int,
-            path: String,
-            start: Int = 0,
-        ) {
-            if (start == path.length) {
-                terminationId = id
-                children.remove('/')
-                return
-            }
-            if (path[start] == '/' && terminationId != -1) {
-                return
-            }
-            children
-                .computeIfAbsent(path[start]) { Trie() }
-                .merge(id, path, start + 1)
-        }
-    }
 
     private fun dfs(
-        root: Trie,
-        onEachTermination: (node: Trie) -> Unit,
+        node: TrieNode,
+        onEachFolder: (index: Int) -> Unit,
     ) {
-        if (root.terminationId != -1) {
-            onEachTermination(root)
+        if (node.index >= 0) {
+            onEachFolder(node.index)
         }
-        for (child in root.children.values) {
-            dfs(child, onEachTermination)
+        for (child in node.children) {
+            dfs(child.value, onEachFolder)
         }
+    }
+}
+
+private class TrieNode {
+    val children: Map<String, TrieNode>
+        get() = _children
+    private var _children = mutableMapOf<String, TrieNode>()
+
+    var index = -1
+        private set
+
+    // Does nothing if the folder is a subfolder of an existing folder.
+    // Otherwise, inserts the folder and removes all its subfolders
+    // from the trie.
+    fun mergeNew(
+        index: Int,
+        folder: String,
+    ) {
+        var node = this
+        var start = 1
+        while (start < folder.length) {
+            val end =
+                folder
+                    .indexOf('/', start)
+                    .let { if (it == -1) folder.length else it }
+            val segment = folder.substring(start, end)
+
+            node = node._children.computeIfAbsent(segment) { TrieNode() }
+            if (node.index >= 0) {
+                return
+            }
+            start = end + 1
+        }
+        node.index = index
+        node._children = mutableMapOf()
     }
 }
