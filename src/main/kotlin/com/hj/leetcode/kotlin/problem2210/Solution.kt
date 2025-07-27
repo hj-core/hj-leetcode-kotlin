8edcb3b7d29ff791a155f313aafa6891b79935a9
hj-core <88603338+hj-core@users.noreply.github.com>
2025-07-27 22:49:40 +0800

    Update problem 2210: Reimplement the solution (#787)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2210/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2210/Solution.kt
@@ -4,37 +4,25 @@ package com.hj.leetcode.kotlin.problem2210
  * LeetCode page: [2210. Count Hills and Valleys in an Array](https://leetcode.com/problems/count-hills-and-valleys-in-an-array/);
  */
 class Solution {
-    private enum class LevelTrend { INCREASE, DECREASE, UNKNOWN }
-
-    /* Complexity:
-     * Time O(N) and Space O(1) where N is the size of nums;
-     */
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of nums.
     fun countHillValley(nums: IntArray): Int {
-        var hillValleyCount = 0
-        var prevLevelTrend = LevelTrend.UNKNOWN
-
-        for (index in 0 until nums.lastIndex) {
-            val currLevelTrend = getLevelTrend(index, nums)
-            if (isHillOrValley(currLevelTrend, prevLevelTrend)) hillValleyCount++
-            if (currLevelTrend != LevelTrend.UNKNOWN) prevLevelTrend = currLevelTrend
+        var result = 0
+        var left = nums[0]
+        for (i in 1..<(nums.size - 1)) {
+            if (nums[i] != nums[i + 1]) {
+                if (isHillOrValley(left, nums[i], nums[i + 1])) {
+                    result++
+                }
+                left = nums[i]
+            }
         }
-
-        return hillValleyCount
+        return result
     }
 
-    private fun getLevelTrend(indexOfLevel: Int, levels: IntArray): LevelTrend {
-        return when {
-            levels[indexOfLevel] > levels[indexOfLevel + 1] -> LevelTrend.DECREASE
-            levels[indexOfLevel] < levels[indexOfLevel + 1] -> LevelTrend.INCREASE
-            else -> LevelTrend.UNKNOWN
-        }
-    }
-
-    private fun isHillOrValley(currLevelTrend: LevelTrend, prevLevelTrend: LevelTrend): Boolean {
-        return when (currLevelTrend) {
-            LevelTrend.INCREASE -> prevLevelTrend == LevelTrend.DECREASE
-            LevelTrend.DECREASE -> prevLevelTrend == LevelTrend.INCREASE
-            LevelTrend.UNKNOWN -> false
-        }
-    }
+    private fun isHillOrValley(
+        left: Int,
+        mid: Int,
+        right: Int,
+    ): Boolean = (left < mid && mid > right) || (left > mid && mid < right)
 }
