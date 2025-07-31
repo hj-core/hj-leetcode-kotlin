cea90d78108e1481f9cb04762b03ec56b59a34aa
hj-core <88603338+hj-core@users.noreply.github.com>
2025-07-31 22:49:16 +0800

    Add a solution to problem 898 (#790)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem898/Solution.kt
@@ -0,0 +1,38 @@
+package com.hj.leetcode.kotlin.problem898
+
+/**
+ * LeetCode page: [898. Bitwise ORs of Subarrays](https://leetcode.com/problems/bitwise-ors-of-subarrays/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(NW) and Space O(NW), where N is the length
+    // of arr and W is the bits of a word.
+    fun subarrayBitwiseORs(arr: IntArray): Int {
+        val allOrs = hashSetOf<Int>()
+
+        // Distinct OR values of the subarrays ending at the
+        // previous index, strictly increasing.
+        val prevOrs = IntArray(32)
+        var size = 0
+
+        for (num in arr) {
+            var newOr = prevOrs[0] or num
+
+            prevOrs[0] = num
+            var newSize = 1
+            allOrs.add(num)
+
+            for (i in 0..<size) {
+                if (newOr == prevOrs[newSize - 1]) {
+                    newOr = prevOrs[i + 1] or num
+                } else {
+                    allOrs.add(newOr)
+                    prevOrs[newSize] = newOr.also { newOr = prevOrs[i + 1] or num }
+                    newSize++
+                }
+            }
+            size = newSize
+        }
+        return allOrs.size
+    }
+}
