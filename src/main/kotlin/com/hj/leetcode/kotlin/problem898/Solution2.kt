6245827e9ebf47198a5e46227d03263627ff6ad8
hj-core <88603338+hj-core@users.noreply.github.com>
2025-07-31 12:20:45 +0800

    Add a two-pointer solution

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem898/Solution2.kt
@@ -0,0 +1,29 @@
+package com.hj.leetcode.kotlin.problem898
+
+/**
+ * LeetCode page: [898. Bitwise ORs of Subarrays](https://leetcode.com/problems/bitwise-ors-of-subarrays/);
+ */
+class Solution2 {
+    // Complexity:
+    // Time O(NLogM) and Space O(NLogM), where N is the
+    // length of arr and M is the maximum value in arr.
+    fun subarrayBitwiseORs(arr: IntArray): Int {
+        val allOrs = hashSetOf<Int>()
+        for ((i, num) in arr.withIndex()) {
+            var prevOr = 0 // OR value of arr[j..<i]
+            var currOr = num
+            allOrs.add(currOr)
+
+            for (j in (i - 1) downTo 0) {
+                prevOr = prevOr or arr[j]
+                currOr = currOr or arr[j]
+                if (prevOr == currOr) {
+                    break
+                } else {
+                    allOrs.add(currOr)
+                }
+            }
+        }
+        return allOrs.size
+    }
+}
