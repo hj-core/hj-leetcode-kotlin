5707e0db661a643b99dfdcef8dcce72b1ce5c5de
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-01 12:23:09 +0800

    Update problem 118: Simplify implementation (#791)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem118/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem118/Solution.kt
@@ -8,14 +8,15 @@ class Solution {
      * Time O(numRows^2) and Space O(numRows^2);
      */
     fun generate(numRows: Int): List<List<Int>> {
-        val generator = generateSequence(listOf(1)) { previousRow ->
-            val newRow = mutableListOf(1)
-            previousRow.asSequence()
-                .zipWithNext { a, b -> a + b }
-                .forEach { newRow.add(it) }
-            newRow.add(1)
-            newRow
+        val result = mutableListOf(mutableListOf(1))
+        for (i in 1..<numRows) {
+            val row = mutableListOf(1)
+            for (j in 1..<i) {
+                row.add(result[i - 1][j - 1] + result[i - 1][j])
+            }
+            row.add(1)
+            result.add(row)
         }
-        return generator.take(numRows).toList()
+        return result
     }
 }
