52ef107789d733e1c050dc438c17987605394745
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-07 23:14:28 +0800

    Merge solveChild2 and solveChild3

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3363/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3363/Solution.kt
@@ -7,7 +7,7 @@ class Solution {
     // Complexity:
     // Time O(N^2) and Space O(N) where N is the height and
     // width of fruits.
-    fun maxCollectedFruits(fruits: Array<IntArray>): Int = solveChild1(fruits) + solveChild2(fruits) + solveChild3(fruits)
+    fun maxCollectedFruits(fruits: Array<IntArray>): Int = solveChild1(fruits) + solveChild2n3(fruits)
 
     // Child 1 starts from (0, 0). He can only move along the
     // diagonal.
@@ -17,41 +17,30 @@ class Solution {
     // the diagonal. Since Child 1 has already collected all the
     // fruits in the diagonal, we only consider Child 2 moving
     // above the diagonal, excluding the goal cell as well.
-    private fun solveChild2(fruits: Array<IntArray>): Int {
+    //
+    // Child 3 follows the symmetric version of child 2. Therefore,
+    // we handle them together.
+    private fun solveChild2n3(fruits: Array<IntArray>): Int {
         val n = fruits.size
-        // dp[j]@r:= the maximum fruits child 2 can collect
-        // when he reaches cell(r, n-1-j).
-        val dp = IntArray(n / 2 + 1)
+        // dp2[j]@i:= the maximum fruits child 2 can collect
+        // when he reaches cell(i, n-1-j).
+        val dp2 = IntArray(n / 2 + 1)
 
-        dp[0] = fruits[0][n - 1]
-        for (r in 1..<(n - 1)) {
-            val maxJ = minOf(r, n - 2 - r)
-            var tmp = dp[0]
+        // dp3[j]@i:= the maximum fruits child 3 can collect
+        // when he reaches cell(n-1-j, i).
+        val dp3 = IntArray(n / 2 + 1)
+
+        dp2[0] = fruits[0][n - 1]
+        dp3[0] = fruits[n - 1][0]
+        for (i in 1..<(n - 1)) {
+            val maxJ = minOf(i, n - 2 - i)
+            var tmp2 = dp2[0] // Previous dp2[j-1]
+            var tmp3 = dp3[0] // Previous dp3[j-1]
             for (j in 0..maxJ) {
-                dp[j] = fruits[r][n - 1 - j] + maxOf(tmp, dp[j], dp[j + 1]).also { tmp = dp[j] }
+                dp2[j] = fruits[i][n - 1 - j] + maxOf(tmp2, dp2[j], dp2[j + 1]).also { tmp2 = dp2[j] }
+                dp3[j] = fruits[n - 1 - j][i] + maxOf(tmp3, dp3[j], dp3[j + 1]).also { tmp3 = dp3[j] }
             }
         }
-        return dp[0]
-    }
-
-    // Child 3 starts from (n-1, 0). He can move below or along
-    // the diagonal. Since Child 1 has already collected all the
-    // fruits in the diagonal, we only consider Child 3 moving
-    // below the diagonal, excluding the goal cell as well.
-    private fun solveChild3(fruits: Array<IntArray>): Int {
-        val n = fruits.size
-        // dp[j]@c:= the maximum fruits child 3 can collect
-        // when he reaches cell(n-1-j, c).
-        val dp = IntArray(n / 2 + 1)
-
-        dp[0] = fruits[n - 1][0]
-        for (c in 1..<(n - 1)) {
-            val maxJ = minOf(c, n - 2 - c)
-            var tmp = dp[0]
-            for (j in 0..maxJ) {
-                dp[j] = fruits[n - 1 - j][c] + maxOf(tmp, dp[j], dp[j + 1]).also { tmp = dp[j] }
-            }
-        }
-        return dp[0]
+        return dp2[0] + dp3[0]
     }
 }
