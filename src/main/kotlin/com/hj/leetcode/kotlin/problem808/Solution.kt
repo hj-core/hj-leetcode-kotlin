3e7bf3905dafdee96aa55c98196769230819c646
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-08 20:16:23 +0800

    Update problem 808: Refactor implementation (#802)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem808/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem808/Solution.kt
@@ -4,50 +4,64 @@ package com.hj.leetcode.kotlin.problem808
  * LeetCode page: [808. Soup Servings](https://leetcode.com/problems/soup-servings/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(1) and Space O(1) because the number of recursions is bounded by O(n0^2),
-     * where n0 is a constant much smaller than the upper bound of n, and for n is not
-     * less than n0, the probability is not less than 1-tolerance;
-     */
+    // Complexity:
+    // Time O(n^2) and Space O(n) for n < 4801. Constant
+    // time and space otherwise.
     fun soupServings(n: Int): Double {
-        val memoization = hashMapOf<RemainingSoupMl, Double>()
-        val resultTolerance = 1e-5
-        val mlPrecision = 25 // 1 to 25 is equivalent to 25, 26 to 50 is equivalent to 50, and so on
-        for (amount in 0..n step mlPrecision) {
-            if (probabilityAsked(RemainingSoupMl(amount, amount), memoization) > 1 - resultTolerance) {
-                return 1.0
+        if (n == 0) {
+            return 0.5
+        }
+        if (n > 4800) {
+            return 1.0 // P > 0.999995 for n >= 4801
+        }
+
+        // Transform the problem: There is an m x m board, and
+        // we start from cell (0, 0). We want to determine the
+        // probability of falling off from the edge of column m-1.
+        // The moves are scaled down by a factor of 25.
+        val m = (n + 24) / 25
+
+        // dp[b][a%4]:= the probability of moving from (0, 0)
+        // to (a, b). We use a sliding window of size 4 for a.
+        val dp = Array(m) { DoubleArray(4) }
+
+        var result = 0.0
+        dp[0][0] = 1.0
+        for (a in 0..<m) {
+            for (b in 0..<m) {
+                result += forwardUpdate(dp, m, a, b)
             }
         }
-        return probabilityAsked(RemainingSoupMl(n, n), memoization)
-    }
-
-    private fun probabilityAsked(
-        remainingSoupMl: RemainingSoupMl,
-        memoization: MutableMap<RemainingSoupMl, Double> = hashMapOf()
-    ): Double = with(remainingSoupMl) {
-        if (this in memoization) {
-            return checkNotNull(memoization[this])
-        }
-        if (typeA > 0 && typeB <= 0) {
-            return 0.0
-        }
-        if (typeA <= 0) {
-            return if (typeB <= 0) 0.5 else 1.0
-        }
-
-        val result = 0.25 * (probabilityAsked(remainingSoupMl.afterOperation1(), memoization)
-                + probabilityAsked(remainingSoupMl.afterOperation2(), memoization)
-                + probabilityAsked(remainingSoupMl.afterOperation3(), memoization)
-                + probabilityAsked(remainingSoupMl.afterOperation4(), memoization))
-        memoization[remainingSoupMl] = result
         return result
     }
 
-    private data class RemainingSoupMl(val typeA: Int, val typeB: Int) {
+    // Distributes the probability to the cells reachable
+    // from (a, b), and returns the probability of falling
+    // off from the edge of column m-1.
+    fun forwardUpdate(
+        dp: Array<DoubleArray>,
+        m: Int,
+        a: Int,
+        b: Int,
+    ): Double {
+        var result = 0.0
+        val p = dp[b][a and 3] * 0.25
+        dp[b][a and 3] = 0.0
 
-        fun afterOperation1(): RemainingSoupMl = RemainingSoupMl(typeA - 100, typeB)
-        fun afterOperation2(): RemainingSoupMl = RemainingSoupMl(typeA - 75, typeB - 25)
-        fun afterOperation3(): RemainingSoupMl = RemainingSoupMl(typeA - 50, typeB - 50)
-        fun afterOperation4(): RemainingSoupMl = RemainingSoupMl(typeA - 25, typeB - 75)
+        for (move in 1..4) {
+            val dstA = a + move
+            val dstB = b + (4 - move)
+            if (dstA < m && dstB < m) {
+                dp[dstB][dstA and 3] += p
+            } else if (m <= dstA) {
+                result +=
+                    if (dstB < m) {
+                        p
+                    } else {
+                        p * 0.5
+                    }
+            }
+        }
+        return result
     }
 }
