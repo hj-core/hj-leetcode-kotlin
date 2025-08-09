1755ea54812c67e2c4195066fef910fbdc9ed08c
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-09 16:27:08 +0800

    Update problem 231: Fix time and space complexity (#803)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem231/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem231/Solution.kt
@@ -4,10 +4,7 @@ package com.hj.leetcode.kotlin.problem231
  * LeetCode page: [231. Power of Two](https://leetcode.com/problems/power-of-two/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(N) where N is the size of n;
-     */
-    fun isPowerOfTwo(n: Int): Boolean {
-        return n > 0 && (n and (n - 1) == 0)
-    }
+    // Complexity:
+    // Time O(1) and Space O(1).
+    fun isPowerOfTwo(n: Int): Boolean = n > 0 && (n and (n - 1) == 0)
 }
