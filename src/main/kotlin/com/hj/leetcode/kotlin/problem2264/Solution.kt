d9cc8e79b4de81071ef31af438705aba7b04c974
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-14 09:44:21 +0800

    Add a solution to problem 2264

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2264/Solution.kt
@@ -0,0 +1,22 @@
+package com.hj.leetcode.kotlin.problem2264
+
+/**
+ * LeetCode page: [2264. Largest 3-Same-Digit Number in String](https://leetcode.com/problems/largest-3-same-digit-number-in-string/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of num.
+    fun largestGoodInteger(num: String): String {
+        var maxDigit = -1
+        var i = 0
+        while (i < num.length - 2) {
+            if (num[i] == num[i + 1] && num[i] == num[i + 2]) {
+                maxDigit = maxOf(maxDigit, num[i].digitToInt())
+                i += 3
+            } else {
+                i += 1
+            }
+        }
+        return if (maxDigit == -1) "" else "$maxDigit$maxDigit$maxDigit"
+    }
+}
