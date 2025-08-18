314e3c9f34df7f9a7f486c2288d42c931a6968bc
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-18 17:35:16 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem679/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem679/Solution.kt
@@ -19,7 +19,8 @@ class Solution {
                     for (op0 in 0..<4) {
                         for (op1 in 0..<4) {
                             for (op2 in 0..<4) {
-                                for (order in 0..<5) {
+                                val orders = if (op0 == 3 || op1 == 3 || op2 == 3) 0..<5 else 0..<2
+                                for (order in orders) {
                                     val (numerator, denominator) = evaluate(newCards, op0, op1, op2, order)
                                     if (denominator != 0 && numerator == denominator * 24) {
                                         return true
@@ -46,9 +47,9 @@ class Solution {
         order: Int,
     ): IntArray =
         when (order) {
-            0 -> evaluateOp(op0, cards[0], evaluateOp(op1, cards[1], evaluateOp(op2, cards[2], cards[3])))
-            1 -> evaluateOp(op2, evaluateOp(op1, evaluateOp(op0, cards[0], cards[1]), cards[2]), cards[3])
-            2 -> evaluateOp(op1, evaluateOp(op0, cards[0], cards[1]), evaluateOp(op2, cards[2], cards[3]))
+            0 -> evaluateOp(op1, evaluateOp(op0, cards[0], cards[1]), evaluateOp(op2, cards[2], cards[3]))
+            1 -> evaluateOp(op0, cards[0], evaluateOp(op1, cards[1], evaluateOp(op2, cards[2], cards[3])))
+            2 -> evaluateOp(op2, evaluateOp(op1, evaluateOp(op0, cards[0], cards[1]), cards[2]), cards[3])
             3 -> evaluateOp(op0, cards[0], evaluateOp(op2, evaluateOp(op1, cards[1], cards[2]), cards[3]))
             4 -> evaluateOp(op2, evaluateOp(op0, cards[0], evaluateOp(op1, cards[1], cards[2])), cards[3])
             else -> throw NoWhenBranchMatchedException("invalid order $order")
