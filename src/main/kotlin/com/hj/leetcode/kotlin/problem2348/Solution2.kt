a5e791c0e2b9daa27cb096c0645baee3718bcc16
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-19 09:21:01 +0800

    Simplify implementations

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2348/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2348/Solution2.kt
@@ -4,15 +4,19 @@ package com.hj.leetcode.kotlin.problem2348
  * LeetCode page: [2348. Number of Zero-Filled Subarrays](https://leetcode.com/problems/number-of-zero-filled-subarrays/);
  */
 class Solution2 {
-    /* Complexity:
-     * Time O(N) and Space O(1) where N is the size of nums;
-     */
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of nums.
     fun zeroFilledSubarray(nums: IntArray): Long {
         var result = 0L
-        var numTargetSubarraysEndedAtIndex = 0
-        for (num in nums) {
-            numTargetSubarraysEndedAtIndex = if (num == 0) numTargetSubarraysEndedAtIndex + 1 else 0
-            result += numTargetSubarraysEndedAtIndex
+        var start = -1
+        // We accumulate the number of zero-filled subarrays
+        // ending at each index.
+        for ((end, num) in nums.withIndex()) {
+            if (num == 0) {
+                result += end - start
+            } else {
+                start = end
+            }
         }
         return result
     }
