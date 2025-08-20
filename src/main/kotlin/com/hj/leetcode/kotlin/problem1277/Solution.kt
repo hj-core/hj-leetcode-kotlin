12cb90d3ad934817657639345de800a2c0b12a6f
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-20 22:46:04 +0800

    Update problem 1277: Improve readability (#815)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1277/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1277/Solution.kt
@@ -4,28 +4,26 @@ package com.hj.leetcode.kotlin.problem1277
  * LeetCode page: [1277. Count Square Submatrices with All Ones](https://leetcode.com/problems/count-square-submatrices-with-all-ones/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(MN) and Space O(N) where M is the number of rows in matrix
-     * and N is the number of columns in matrix.
-     */
+    // Complexity:
+    // Time O(MN) and Space O(N) where M and N are the number
+    // of rows and columns in matrix, respectively.
     fun countSquares(matrix: Array<IntArray>): Int {
-        // dp[row][col] ::=
-        // size of the largest one-square with (row, col) as its top-left corner,
-        // i.e. the number of one-squares with (row, col) as its top-left corner.
-        val dp = IntArray(matrix[0].size + 1) // base case: row=matrix.size
         var result = 0
+        // dp[c]@r:= the size of the largest one-square whose
+        // top-left corner is cell(r, c).
+        val dp = IntArray(matrix[0].size + 1) // base case: r=matrix.size
 
-        for (row in matrix.indices.reversed()) {
-            var temp = 0 // dp[row+1][col+1]
-            for (col in matrix[row].indices.reversed()) {
+        for (r in matrix.indices.reversed()) {
+            var tmp = 0 // dp[c+1]@r+1
+            for (c in matrix[r].indices.reversed()) {
                 val subResult =
-                    if (matrix[row][col] == 0) {
+                    if (matrix[r][c] == 0) {
                         0
-                    } else { // 1 + minOf(dp[row+1][col], dp[row][col+1], dp[row+1][col+1])
-                        1 + minOf(dp[col], dp[col + 1], temp)
+                    } else {
+                        1 + minOf(dp[c], dp[c + 1], tmp)
                     }
                 result += subResult
-                dp[col] = subResult.also { temp = dp[col] }
+                dp[c] = subResult.also { tmp = dp[c] }
             }
         }
         return result
