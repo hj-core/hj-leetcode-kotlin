5b78071cbfd63b3e078fb2d5e8e12eb8b5e7f823
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-26 18:50:56 +0800

    Add a solution to problem 30000

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3000/Solution.kt
@@ -0,0 +1,19 @@
+package com.hj.leetcode.kotlin.problem3000
+
+import java.util.Comparator.comparing
+
+/**
+ * LeetCode page: [3000. Maximum Area of Longest Diagonal Rectangle](https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of dimensions.
+    fun areaOfMaxDiagonal(dimensions: Array<IntArray>): Int =
+        dimensions
+            .maxWith(comparing(::calcDiagonalSquare).thenBy(::calcArea))
+            .let(::calcArea)
+
+    private fun calcDiagonalSquare(rectangle: IntArray): Int = rectangle[0] * rectangle[0] + rectangle[1] * rectangle[1]
+
+    private fun calcArea(rectangle: IntArray): Int = rectangle[0] * rectangle[1]
+}
