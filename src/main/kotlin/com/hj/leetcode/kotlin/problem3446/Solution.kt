e620bea258f22c97820f1525646d389e70f6fd49
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-28 18:28:44 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3446/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3446/Solution.kt
@@ -7,26 +7,13 @@ class Solution {
     // Complexity:
     // Time O(N^2 * LogN) and Space O(N) where N is the number
     // of rows and columns in grid, respectively.
-    fun sortMatrix(grid: Array<IntArray>): Array<IntArray> {
+    fun sortMatrix(grid: Array<IntArray>): Array<IntArray> = sortTopRight(sortBottomLeft(grid))
+
+    private fun sortTopRight(grid: Array<IntArray>): Array<IntArray> {
         val n = grid.size
+        val values = MutableList(n - 1) { 0 }
 
-        // Rearrange the bottom-left triangle
-        var values = MutableList<Int>(n) { 0 }
-        for (i in grid.indices) {
-            for (step in 0..<(n - i)) {
-                values[step] = grid[i + step][step]
-            }
-            values.sortDescending()
-
-            for (step in 0..<(n - i)) {
-                grid[i + step][step] = values[step]
-            }
-            values.removeLast()
-        }
-
-        // Rearrange the top-right triangle
-        values = MutableList(n - 1) { 0 }
-        for (i in 1..<n) {
+        for (i in 1..<(n - 1)) {
             for (step in 0..<(n - i)) {
                 values[step] = grid[step][i + step]
             }
@@ -37,7 +24,24 @@ class Solution {
             }
             values.removeLast()
         }
+        return grid
+    }
 
+    private fun sortBottomLeft(grid: Array<IntArray>): Array<IntArray> {
+        val n = grid.size
+        val values = MutableList(n) { 0 }
+
+        for (i in 0..<(n - 1)) {
+            for (step in 0..<(n - i)) {
+                values[step] = grid[i + step][step]
+            }
+            values.sortDescending()
+
+            for (step in 0..<(n - i)) {
+                grid[i + step][step] = values[step]
+            }
+            values.removeLast()
+        }
         return grid
     }
 }
