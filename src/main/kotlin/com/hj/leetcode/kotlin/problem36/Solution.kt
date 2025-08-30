91ba87e1787432a5072be78686303323b10a28a5
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-30 17:23:10 +0800

    Update problem 36: Fix complexities (#824)
    The existing time and space complexities are inconsistent
    where space complexity treats board width as constant while
    the time complexity doesn't. This commit fixes the issue
    and opts for treating the board size as constant.

--- src/main/kotlin/com/hj/leetcode/kotlin/problem36/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem36/Solution.kt
@@ -4,56 +4,76 @@ package com.hj.leetcode.kotlin.problem36
  * LeetCode page: [36. Valid Sudoku](https://leetcode.com/problems/valid-sudoku/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(|board|) and Space O(1);
-     */
-    fun isValidSudoku(board: Array<CharArray>): Boolean {
-        return checkRows(board) && checkColumns(board) && checkSubBoxes(board)
-    }
+    // Complexity:
+    // Time O(1) and Space O(1) if we treat the 9 by 9 size
+    // as a constant.
+    fun isValidSudoku(board: Array<CharArray>): Boolean = checkRows(board) && checkColumns(board) && checkSquares(board)
 
     private fun checkRows(board: Array<CharArray>): Boolean {
-        for (row in board) {
+        for (r in board.indices) {
             val visited = BooleanArray(9)
-            for (value in row) {
-                if (value == '.') continue
-                val index = value - '1'
-                if (visited[index]) return false else visited[index] = true
+            for (c in board[r].indices) {
+                if (markVisited(board[r][c], visited)) {
+                    return false
+                }
             }
         }
         return true
+    }
+
+    // Marks the value visited and returns whether it has been
+    // visited before.
+    private fun markVisited(
+        value: Char,
+        visited: BooleanArray,
+    ): Boolean {
+        if (value == '.') {
+            return false
+        }
+
+        val index = value - '1'
+        if (visited[index]) {
+            return true
+        }
+
+        visited[index] = true
+        return false
     }
 
     private fun checkColumns(board: Array<CharArray>): Boolean {
-        for (column in board[0].indices) {
+        for (c in board[0].indices) {
             val visited = BooleanArray(9)
-            for (row in board.indices) {
-                val value = board[row][column]
-                if (value == '.') continue
-                val index = value - '1'
-                if (visited[index]) return false else visited[index] = true
+            for (r in board.indices) {
+                if (markVisited(board[r][c], visited)) {
+                    return false
+                }
             }
         }
         return true
     }
 
-    private fun checkSubBoxes(board: Array<CharArray>): Boolean {
-        for (row in board.indices step 3) {
-            for (column in board[row].indices step 3) {
-                val isValidSubBox = checkSubBox(board, row, column)
-                if (!isValidSubBox) return false
+    private fun checkSquares(board: Array<CharArray>): Boolean {
+        for (r0 in board.indices step 3) {
+            for (c0 in board[r0].indices step 3) {
+                if (!checkSquare(board, r0, c0)) {
+                    return false
+                }
             }
         }
         return true
     }
 
-    private fun checkSubBox(board: Array<CharArray>, rowTopLeft: Int, columnTopLeft: Int): Boolean {
+    private fun checkSquare(
+        board: Array<CharArray>,
+        r0: Int,
+        c0: Int,
+    ): Boolean {
         val visited = BooleanArray(9)
-        for (row in rowTopLeft until rowTopLeft + 3) {
-            for (column in columnTopLeft until columnTopLeft + 3) {
-                val value = board[row][column]
-                if (value == '.') continue
-                val index = value - '1'
-                if (visited[index]) return false else visited[index] = true
+        for (r in r0 until r0 + 3) {
+            for (c in c0 until c0 + 3) {
+                if (markVisited(board[r][c], visited)) {
+                    return false
+                }
             }
         }
         return true
