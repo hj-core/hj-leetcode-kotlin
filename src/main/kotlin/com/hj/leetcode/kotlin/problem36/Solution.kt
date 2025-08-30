cf358c29d79364e5dd55a21e07b07d448236a9a3
hj-core <88603338+hj-core@users.noreply.github.com>
2025-08-30 10:43:18 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem36/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem36/Solution.kt
@@ -7,7 +7,7 @@ class Solution {
     // Complexity:
     // Time O(1) and Space O(1) if we treat the 9 by 9 size
     // as a constant.
-    fun isValidSudoku(board: Array<CharArray>): Boolean = checkRows(board) && checkColumns(board) && checkSubBoxes(board)
+    fun isValidSudoku(board: Array<CharArray>): Boolean = checkRows(board) && checkColumns(board) && checkSquares(board)
 
     private fun checkRows(board: Array<CharArray>): Boolean {
         for (row in board) {
@@ -34,24 +34,23 @@ class Solution {
         return true
     }
 
-    private fun checkSubBoxes(board: Array<CharArray>): Boolean {
-        for (row in board.indices step 3) {
-            for (column in board[row].indices step 3) {
-                val isValidSubBox = checkSubBox(board, row, column)
-                if (!isValidSubBox) return false
+    private fun checkSquares(board: Array<CharArray>): Boolean {
+        for (r0 in board.indices step 3) {
+            for (c0 in board[r0].indices step 3) {
+                if (!checkSquare(board, r0, c0)) return false
             }
         }
         return true
     }
 
-    private fun checkSubBox(
+    private fun checkSquare(
         board: Array<CharArray>,
-        rowTopLeft: Int,
-        columnTopLeft: Int,
+        r0: Int,
+        c0: Int,
     ): Boolean {
         val visited = BooleanArray(9)
-        for (row in rowTopLeft until rowTopLeft + 3) {
-            for (column in columnTopLeft until columnTopLeft + 3) {
+        for (row in r0 until r0 + 3) {
+            for (column in c0 until c0 + 3) {
                 val value = board[row][column]
                 if (value == '.') continue
                 val index = value - '1'
