8dc262ec02da8cfd5109df2d093739ae1b4bd53e
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-01 20:58:11 +0800

    Update problem 1792: Refactor implementation (#826)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1792/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1792/Solution.kt
@@ -5,45 +5,43 @@ import java.util.*
 /**
  * LeetCode page: [1792. Maximum Average Pass Ratio](https://leetcode.com/problems/maximum-average-pass-ratio/);
  */
+private typealias Class = DoubleArray // (pass, total, oneExtraGain * -1.0)
+
 class Solution {
-    /* Complexity:
-     * Time O(NLogN+KLogN) and Space O(N)
-     * where N is the length of classes and K is extraStudents.
-     */
+    // Complexity:
+    // Time O(NLogN+KLogN) and Space O(N) where N is the
+    // length of classes and K is extraStudents.
     fun maxAverageRatio(
         classes: Array<IntArray>,
         extraStudents: Int,
     ): Double {
-        val classPq = PriorityQueue<IntArray> { a, b -> -comparingMarginalGain(a, b) }
-        for (theClass in classes) {
-            classPq.offer(theClass)
+        val pq = PriorityQueue<Class>(classes.size, compareBy { it[2] })
+        for (c in classes) {
+            pq.offer(newClass(c[0], c[1]))
         }
 
         repeat(extraStudents) {
-            val (pass, total) = classPq.poll()
-            val withExtra = intArrayOf(pass + 1, total + 1)
-            classPq.offer(withExtra)
+            val c = pq.poll()
+            c[0]++
+            c[1]++
+            c[2] = -1.0 * calcOneExtraGain(c[0], c[1])
+            pq.offer(c)
         }
-        return averagePassRatio(classPq)
+
+        return pq.sumOf { it[0] / it[1] } / pq.size
     }
 
-    private fun comparingMarginalGain(
-        classA: IntArray,
-        classB: IntArray,
-    ): Int {
-        val (passA, totalA) = classA
-        val (passB, totalB) = classB
-        val diff =
-            (totalA - passA).toLong() * totalB * (totalB + 1) -
-                (totalB - passB).toLong() * totalA * (totalA + 1)
-
-        return when {
-            diff > 0 -> 1
-            diff < 0 -> -1
-            else -> 0
-        }
+    private fun newClass(
+        pass: Int,
+        total: Int,
+    ): Class {
+        val pass = pass.toDouble()
+        val total = total.toDouble()
+        return doubleArrayOf(pass, total, -1.0 * calcOneExtraGain(pass, total))
     }
 
-    private fun averagePassRatio(classes: Collection<IntArray>): Double =
-        classes.sumOf { (pass, total) -> pass.toDouble() / total } / classes.size
+    private fun calcOneExtraGain(
+        pass: Double,
+        total: Double,
+    ): Double = (pass + 1.0) / (total + 1.0) - pass / total
 }
