b441e90a4b499902be12023499120b0ed94662d0
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-03 22:47:16 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3027/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3027/Solution.kt
@@ -7,20 +7,13 @@ class Solution {
     // Complexity:
     // Time O(N^2) and Space O(N) where N is the length of points.
     fun numberOfPairs(points: Array<IntArray>): Int {
-        val sortedPoints =
-            points.sortedArrayWith(
-                compareBy({ it[0] }, { -it[1] }),
-            )
-
+        val sorted = points.sortedArrayWith(compareBy({ it[0] }, { -it[1] }))
         var result = 0
-        for (i in 1..<sortedPoints.size) {
-            val y0 = sortedPoints[i][1]
+        for (i in 1..<sorted.size) {
             var minY = Int.MAX_VALUE
-
             for (j in i - 1 downTo 0) {
-                val y1 = sortedPoints[j][1]
-                if (y1 in y0..<minY) {
-                    minY = y1
+                if (sorted[j][1] in sorted[i][1]..<minY) {
+                    minY = sorted[j][1]
                     result++
                 }
             }
