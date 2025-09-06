444dcf4175f995eb9cfdd7aa694584cd65df454d
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-06 11:41:41 +0800

    Add a solution to problem 3495

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3495/Solution.kt
@@ -0,0 +1,37 @@
+package com.hj.leetcode.kotlin.problem3495
+
+/**
+ * LeetCode page: [3495. Minimum Operations to Make Array Elements Zero](https://leetcode.com/problems/minimum-operations-to-make-array-elements-zero/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N+LogM) and Space O(1) where N is the length
+    // of queries and M is the product of all query rights.
+    fun minOperations(queries: Array<IntArray>): Long {
+        var result = 0L
+        for (query in queries) {
+            result += solveQuery(query)
+        }
+        return result
+    }
+
+    private fun solveQuery(query: IntArray): Long {
+        val right = query[1]
+        var newLeft = query[0]
+        var totalDivs = 0L
+        var power4 = 1
+        var unitDivs = 0L
+
+        while (power4 <= right) {
+            power4 = power4 shl 2
+            unitDivs++
+
+            if (newLeft < power4) {
+                val count = minOf(right, power4 - 1) - newLeft + 1
+                totalDivs += count * unitDivs
+                newLeft = power4
+            }
+        }
+        return (totalDivs + 1) / 2
+    }
+}
