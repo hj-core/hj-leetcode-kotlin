907f503bd25c8e62a9043d1af3f53cd1351133e8
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-07 09:39:43 +0800

    Add a solution to problem 1304

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1304/Solution.kt
@@ -0,0 +1,17 @@
+package com.hj.leetcode.kotlin.problem1304
+
+/**
+ * LeetCode page: [1304. Find N Unique Integers Sum up to Zero](https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(n) and Auxiliary Space O(1).
+    fun sumZero(n: Int): IntArray {
+        val result = IntArray(n)
+        for (i in 0..<(n - 1) step 2) {
+            result[i] = i + 1
+            result[i + 1] = -result[i]
+        }
+        return result
+    }
+}
