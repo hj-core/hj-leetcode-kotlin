fc98f52f1baff72f437a0b829be3713fbe780e91
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-08 09:44:03 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1317/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1317/Solution.kt
@@ -4,30 +4,26 @@ package com.hj.leetcode.kotlin.problem1317
  * LeetCode page: [1317. Convert Integer to the Sum of Two No-Zero Integers](https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(1) and Space O(1) since n is in 2..10000 thus at most 5 digits;
-     */
+    // Complexity:
+    // Time O(Log n) and Space O(1).
     fun getNoZeroIntegers(n: Int): IntArray {
-        val firstNoZero = constructFirstNoZero(n)
-        val secondNoZero = n - firstNoZero
-        return intArrayOf(firstNoZero, secondNoZero)
-    }
+        var a = 0
+        var remain = n
+        var unit = 1
 
-    private fun constructFirstNoZero(n: Int): Int {
-        val digits = n.getDigits()
-        for (index in digits.indices.reversed()) {
-            if (digits[index] < 2 && index != 0) {
-                digits[index] = 5
-                digits[index - 1] -= 1
+        while (remain > 1) {
+            val digit = remain % 10
+            remain /= 10
+
+            if (digit < 2) {
+                a += unit shl 1
+                remain--
             } else {
-                digits[index] = digits[index] shr 1
+                a += unit
             }
-        }
-        return digits.fold(0) { acc, i -> acc * 10 + i }
-    }
 
-    private fun Int.getDigits(): IntArray = this
-        .toString()
-        .map { it - '0' }
-        .toIntArray()
+            unit *= 10
+        }
+        return intArrayOf(a, n - a)
+    }
 }
