20609481cc7bb3d272d13984863755e651a20ec4
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-11 14:34:05 +0800

    Refactor the implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2785/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2785/Solution.kt
@@ -4,25 +4,32 @@ package com.hj.leetcode.kotlin.problem2785
  * LeetCode page: [2785. Sort Vowels in a String](https://leetcode.com/problems/sort-vowels-in-a-string/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(N) where N is the length of s;
-     */
+    // Complexity:
+    // Time O(N) and Auxiliary Space O(1) where N is the length
+    // of s. We treat the number of different vowels (i.e., 1) as
+    // a constant.
     fun sortVowels(s: String): String {
         val vowels = charArrayOf('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
-        val vowelsCount = IntArray(vowels.size) { index ->
-            s.count { char -> char == vowels[index] }
+
+        val cntVowels = IntArray(vowels.size)
+        for (c in s) {
+            val i = vowels.indexOf(c)
+            if (i >= 0) {
+                cntVowels[i]++
+            }
         }
 
-        val result = StringBuilder(s)
-        var vowelIndex = 0
-
-        for ((index, char) in result.withIndex()) {
-            if (char in vowels) {
-                while (vowelsCount[vowelIndex] == 0) {
-                    vowelIndex++
+        val result = StringBuilder(s.length)
+        var j = 0 // index of vowels
+        for (c in s) {
+            if (c in vowels) {
+                while (cntVowels[j] == 0) {
+                    j++
                 }
-                result[index] = vowels[vowelIndex]
-                vowelsCount[vowelIndex]--
+                result.append(vowels[j])
+                cntVowels[j]--
+            } else {
+                result.append(c)
             }
         }
         return result.toString()
