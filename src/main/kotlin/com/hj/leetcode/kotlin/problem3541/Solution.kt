0a2808233bba2f0637f36a946c36751f10916813
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-13 19:02:07 +0800

    Add a solution to problem 3541 (#840)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3541/Solution.kt
@@ -0,0 +1,25 @@
+package com.hj.leetcode.kotlin.problem3541
+
+/**
+ * LeetCode page: [3541. Find Most Frequent Vowel and Consonant](https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N+M) and Space O(M), where N is the length of s
+    // and M is the number of possible lowercase (i.e., 26).
+    fun maxFreqSum(s: String): Int {
+        val freqs = IntArray(26)
+        for (c in s) {
+            freqs[c - 'a']++
+        }
+
+        val maxVowelFreq = maxOf(freqs[0], freqs[4], freqs[8], freqs[14], freqs[20])
+        freqs[0] = 0
+        freqs[4] = 0
+        freqs[8] = 0
+        freqs[14] = 0
+        freqs[20] = 0
+
+        return maxVowelFreq + freqs.max()
+    }
+}
