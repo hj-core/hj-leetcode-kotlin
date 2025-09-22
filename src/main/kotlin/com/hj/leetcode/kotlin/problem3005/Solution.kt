8241b91492ef99f1d026525cb8ab642c8135ef64
hj-core <88603338+hj-core@users.noreply.github.com>
2025-09-22 21:55:00 +0800

    Update problem 3005: Refactor implementation (#849)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3005/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3005/Solution.kt
@@ -4,12 +4,23 @@ package com.hj.leetcode.kotlin.problem3005
  * LeetCode page: [3005. Count Elements With Maximum Frequency](https://leetcode.com/problems/count-elements-with-maximum-frequency/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(N) where N is the size of nums;
-     */
+    // Complexity:
+    // Time O(N+M) and Space O(M) where N is the length of
+    // nums and M is the maximum possible number.
     fun maxFrequencyElements(nums: IntArray): Int {
-        val counter = nums.asList().groupingBy { it }.eachCount()
-        val maxCount = counter.values.max()
-        return maxCount * counter.values.count { it == maxCount }
+        val freqs = IntArray(101)
+        var maxFreq = 0
+        var count = 0
+
+        for (elem in nums) {
+            freqs[elem]++
+            if (freqs[elem] > maxFreq) {
+                maxFreq = freqs[elem]
+                count = 1
+            } else if (freqs[elem] == maxFreq) {
+                count++
+            }
+        }
+        return maxFreq * count
     }
 }
