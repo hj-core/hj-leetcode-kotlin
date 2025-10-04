3c03188c2a137175c77d97998c263fc0de5314e9
hj-core <88603338+hj-core@users.noreply.github.com>
2025-10-04 16:08:48 +0800

    Update problem 11: Refactor implementation (#861)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem11/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem11/Solution.kt
@@ -4,27 +4,21 @@ package com.hj.leetcode.kotlin.problem11
  * LeetCode page: [11. Container With Most Water](https://leetcode.com/problems/container-with-most-water/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(1) where N is the size of height;
-     */
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of height.
     fun maxArea(height: IntArray): Int {
-        var maxArea = 0
-        var leftBoundIndex = 0
-        var rightBoundIndex = height.lastIndex
+        var result = 0
+        var iL = 0
+        var iR = height.lastIndex
 
-        while (leftBoundIndex < rightBoundIndex) {
-            val area = calculateArea(leftBoundIndex, rightBoundIndex, height)
-            maxArea = maxOf(maxArea, area)
+        while (iL < iR) {
+            val w = iR - iL
+            val h = minOf(height[iL], height[iR])
+            result = maxOf(result, w * h)
 
-            val shouldMoveTowardsRight = height[leftBoundIndex] < height[rightBoundIndex]
-            if (shouldMoveTowardsRight) leftBoundIndex++ else rightBoundIndex--
+            while (iL < iR && height[iL] <= h) iL++
+            while (iL < iR && height[iR] <= h) iR--
         }
-        return maxArea
-    }
-
-    private fun calculateArea(leftBoundIndex: Int, rightBoundIndex: Int, heights: IntArray): Int {
-        val effectiveHeight = minOf(heights[leftBoundIndex], heights[rightBoundIndex])
-        val width = rightBoundIndex - leftBoundIndex
-        return width * effectiveHeight
+        return result
     }
 }
