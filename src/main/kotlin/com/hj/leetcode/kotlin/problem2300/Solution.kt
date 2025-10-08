3aa12d5a2a7ab414526921f387ec98ab8afc6a5c
hj-core <88603338+hj-core@users.noreply.github.com>
2025-10-08 14:03:29 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2300/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2300/Solution.kt
@@ -4,30 +4,38 @@ package com.hj.leetcode.kotlin.problem2300
  * LeetCode page: [2300. Successful Pairs of Spells and Potions](https://leetcode.com/problems/successful-pairs-of-spells-and-potions/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(NLogM+MLogM) and Space O(M) where N and M are the size of spells and potions;
-     */
-    fun successfulPairs(spells: IntArray, potions: IntArray, success: Long): IntArray {
+    // Complexity:
+    // Time O(NLogM+MLogM) and Space O(M) where N and M are
+    // the size of spells and potions, respectively.
+    fun successfulPairs(
+        spells: IntArray,
+        potions: IntArray,
+        success: Long,
+    ): IntArray {
         val sortedPotions = potions.sorted()
         return IntArray(spells.size) { index ->
-            numSuccessfulPairs(spells[index], sortedPotions, success)
+            countSuccessfulPairs(spells[index], sortedPotions, success)
         }
     }
 
-    private fun numSuccessfulPairs(spell: Int, sortedPotions: List<Int>, success: Long): Int {
+    private fun countSuccessfulPairs(
+        spell: Int,
+        sortedPotions: List<Int>,
+        success: Long,
+    ): Int {
+        val target = (success + spell - 1) / spell
+        // Binary search on the number of potions that are
+        // less than target.
         var left = 0
-        var right = sortedPotions.lastIndex
-        while (left <= right) {
+        var right = sortedPotions.size
+        while (left < right) {
             val mid = (left + right) ushr 1
-            val midPotion = sortedPotions[mid]
-            val isSuccess = spell.toLong() * midPotion >= success
-            if (isSuccess) {
-                right = mid - 1
-            } else {
+            if (sortedPotions[mid] < target) {
                 left = mid + 1
+            } else {
+                right = mid
             }
         }
-        // left is the first index in sortedPotions that forms a successful pair
         return sortedPotions.size - left
     }
 }
