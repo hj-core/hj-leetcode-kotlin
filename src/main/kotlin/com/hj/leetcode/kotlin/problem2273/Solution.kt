fc7006bb9f501fb815abe9f61b5aef0ab830abdb
hj-core <88603338+hj-core@users.noreply.github.com>
2025-10-13 09:48:40 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2273/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2273/Solution.kt
@@ -4,27 +4,32 @@ package com.hj.leetcode.kotlin.problem2273
  * LeetCode page: [2273. Find Resultant Array After Removing Anagrams](https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Aux_Space O(1) where N is the flat length of words;
-     */
+    // Complexity:
+    // Time O(L) and Space O(L) where L is the flattened length
+    // of words.
     fun removeAnagrams(words: Array<String>): List<String> {
-        val ans = mutableListOf(words[0])
-        var prevCharFrequency = getCharFrequency(words[0])
-        for (index in 1..words.lastIndex) {
-            val currCharFrequency = getCharFrequency(words[index])
-            if (!currCharFrequency.contentEquals(prevCharFrequency)) {
-                ans.add(words[index])
-                prevCharFrequency = currCharFrequency
+        val result = mutableListOf(words[0])
+        val hash = hashWord(words[0])
+
+        for (i in 1..<words.size) {
+            val newHash = hashWord(words[i])
+            if (newHash.contentEquals(hash)) {
+                continue
             }
+            result.add(words[i])
+            newHash.copyInto(hash)
         }
-        return ans
+        return result
     }
 
-    private fun getCharFrequency(lowercaseString: String): IntArray {
-        val charFrequency = IntArray(26)
-        for (char in lowercaseString) {
-            charFrequency[char - 'a']++
+    // Returns a collision-free hash for checking anagrams
+    // consisting of up to 15 lowercase English letters.
+    fun hashWord(word: String): LongArray {
+        val result = LongArray(2)
+        for (c in word) {
+            val idx = c - 'a'
+            result[idx and 1] += 1L shl (idx shl 1)
         }
-        return charFrequency
+        return result
     }
 }
