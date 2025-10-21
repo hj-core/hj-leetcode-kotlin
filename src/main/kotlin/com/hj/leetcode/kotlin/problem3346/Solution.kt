8f27c1809edd7936033d6ff81b2ec303552094ff
hj-core <88603338+hj-core@users.noreply.github.com>
2025-10-21 18:11:21 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3346/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3346/Solution.kt
@@ -17,33 +17,30 @@ class Solution {
         newNums[newNums.lastIndex] = Int.MAX_VALUE
         newNums.sort()
         // Ensure the extra number does not affect the result
-        check(newNums.last() - 2 * k > newNums[nums.lastIndex]) {
+        check(newNums.last() - newNums[nums.lastIndex] > k) {
             "The extra number may lead to incorrect result"
         }
 
-        // We greedily limit the target value to match nums[i]-k,
-        // nums[i], and nums[i]+k, and use three sliding windows
-        // to determine the corresponding frequencies.
+        // We greedily limit the target value to match nums[i]-k
+        // and nums[i], and use two sliding windows to determine
+        // the corresponding frequencies.
         var i = 0
         var result = 0
+
+        // Left window contains all numbers in [nums[i]-2k, nums[i]]
+        var leftStart = 0
 
         // Mid window contains all numbers in [nums[i]-k, nums[i]+k]
         var midStart = 0
         var midEnd = 0
 
-        // Left window contains all numbers in [nums[i]-2k, nums[i]]
-        var leftStart = 0
-
-        // Right window contains all numbers in [nums[i], nums[i]+2k]
-        var rightEnd = 0
-
         while (i < nums.size) {
-            while (newNums[leftStart] < newNums[i] - 2 * k) {
+            while (2 * k < newNums[i] - newNums[leftStart]) {
                 leftStart++
             }
 
             midStart = maxOf(midStart, leftStart)
-            while (newNums[midStart] < newNums[i] - k) {
+            while (k < newNums[i] - newNums[midStart]) {
                 midStart++
             }
 
@@ -58,17 +55,11 @@ class Solution {
                 midEnd++
             }
 
-            rightEnd = maxOf(rightEnd, midEnd)
-            while (newNums[rightEnd] - newNums[oldI] <= 2 * k) {
-                rightEnd++
-            }
-
             result =
                 maxOf(
                     result,
                     minOf(midEnd - midStart, i - oldI + numOperations),
                     minOf(i - leftStart, numOperations),
-                    minOf(rightEnd - oldI, numOperations),
                 )
         }
         return result
