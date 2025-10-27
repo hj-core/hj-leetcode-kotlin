f17b668faa5c932555e0705ac73048f997e25aaa
hj-core <88603338+hj-core@users.noreply.github.com>
2025-10-27 19:24:05 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2125/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2125/Solution.kt
@@ -4,19 +4,14 @@ package com.hj.leetcode.kotlin.problem2125
  * LeetCode page: [2125. Number of Laser Beams in a Bank](https://leetcode.com/problems/number-of-laser-beams-in-a-bank/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(1) where N is the flattened length of bank;
-     */
-    fun numberOfBeams(bank: Array<String>): Int {
-        var result = 0
-        var numSenders = 0
-        for (row in bank) {
-            val numReceivers = row.count { it == '1' }
-            if (numReceivers != 0) {
-                result += numSenders * numReceivers
-                numSenders = numReceivers
-            }
-        }
-        return result
-    }
+    // Complexity:
+    // Time O(MN) and Space O(1) where M and N are the number of
+    // rows and columns in bank, respectively.
+    fun numberOfBeams(bank: Array<String>): Int =
+        bank
+            .asSequence()
+            .map { it.count { c -> c == '1' } }
+            .filter { it > 0 }
+            .zipWithNext()
+            .sumOf { (sendCnt, recvCnt) -> sendCnt * recvCnt }
 }
