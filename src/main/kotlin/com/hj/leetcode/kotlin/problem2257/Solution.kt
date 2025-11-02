4bd22af2878d1b96abfbec3eb40073f5bae1217e
hj-core <88603338+hj-core@users.noreply.github.com>
2025-11-02 09:55:47 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2257/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2257/Solution.kt
@@ -6,95 +6,61 @@ private typealias Grid = Array<IntArray>
  * LeetCode page: [2257. Count Unguarded Cells in the Grid](https://leetcode.com/problems/count-unguarded-cells-in-the-grid/);
  */
 class Solution {
-    private val free = 0
-    private val wall = 1
-    private val guard = 2
-    private val guardedHorizontally = 3
-    private val guardedVertically = 4
-
-    /* Complexity:
-     * Time O(mn) and Space O(mn).
-     */
+    // Complexity:
+    // Time O(mn) and Space O(mn).
     fun countUnguarded(
         m: Int,
         n: Int,
         guards: Array<IntArray>,
         walls: Array<IntArray>,
-    ): Int =
-        newGrid(m, n).run {
-            installWalls(walls)
-            installGuards(guards)
-            markGuardedCells(guards)
-            countFreeCells()
+    ): Int {
+        // grid[r][c]:=
+        // 0 if it is free,
+        // 7 if there is a guard or wall,
+        // bit 1 is set if it is watched from row direction,
+        // bit 2 is set if it is watched from column direction.
+        val grid = Array(m) { IntArray(n) }
+
+        for ((r0, c0) in walls) {
+            grid[r0][c0] = 7
         }
 
-    private fun newGrid(
-        m: Int,
-        n: Int,
-    ): Grid = Array(m) { IntArray(n) { free } }
+        for ((r0, c0) in guards) {
+            grid[r0][c0] = 7
 
-    private fun Grid.installWalls(walls: Array<IntArray>) {
-        for ((r, c) in walls) {
-            this[r][c] = wall
+            for (r in r0 - 1 downTo 0) {
+                if (grid[r][c0] and 1 != 0) {
+                    break
+                } else {
+                    grid[r][c0] = grid[r][c0] xor 1
+                }
+            }
+
+            for (r in r0 + 1..<m) {
+                if (grid[r][c0] and 1 != 0) {
+                    break
+                } else {
+                    grid[r][c0] = grid[r][c0] xor 1
+                }
+            }
+
+            for (c in c0 - 1 downTo 0) {
+                if (grid[r0][c] and 2 != 0) {
+                    break
+                } else {
+                    grid[r0][c] = grid[r0][c] xor 2
+                }
+            }
+
+            for (c in c0 + 1..<n) {
+                if (grid[r0][c] and 2 != 0) {
+                    break
+                } else {
+                    grid[r0][c] = grid[r0][c] xor 2
+                }
+            }
         }
+
+        return grid.sumOf { it.count { v -> v == 0 } }
     }
-
-    private fun Grid.installGuards(guards: Array<IntArray>) {
-        for ((r, c) in guards) {
-            this[r][c] = guard
-        }
-    }
-
-    private fun Grid.markGuardedCells(guards: Array<IntArray>) {
-        for (guard in guards) {
-            markHorizontallyGuarded(guard)
-        }
-        for (guard in guards) {
-            markVerticallyGuarded(guard)
-        }
-    }
-
-    private fun Grid.markHorizontallyGuarded(guard: IntArray) {
-        val (r, c) = guard
-        for (r2 in r - 1 downTo 0) {
-            if (!tryGuardedHorizontally(r2, c)) break
-        }
-        for (r2 in r + 1..<this.size) {
-            if (!tryGuardedHorizontally(r2, c)) break
-        }
-    }
-
-    private fun Grid.tryGuardedHorizontally(
-        row: Int,
-        col: Int,
-    ): Boolean {
-        if (this[row][col] == free || this[row][col] == guardedVertically) {
-            this[row][col] = guardedHorizontally
-            return true
-        }
-        return false
-    }
-
-    private fun Grid.markVerticallyGuarded(guard: IntArray) {
-        val (r, c) = guard
-        for (c2 in c - 1 downTo 0) {
-            if (!tryGuardedVertically(r, c2)) break
-        }
-        for (c2 in c + 1..<this[r].size) {
-            if (!tryGuardedVertically(r, c2)) break
-        }
-    }
-
-    private fun Grid.tryGuardedVertically(
-        row: Int,
-        col: Int,
-    ): Boolean {
-        if (this[row][col] == free || this[row][col] == guardedHorizontally) {
-            this[row][col] = guardedVertically
-            return true
-        }
-        return false
-    }
-
-    private fun Grid.countFreeCells(): Int = sumOf { row -> row.count { it == free } }
 }
