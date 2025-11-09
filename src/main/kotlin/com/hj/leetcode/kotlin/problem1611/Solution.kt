1e680b145c5b6831c00887138ad19bd25fd2075c
hj-core <88603338+hj-core@users.noreply.github.com>
2025-11-09 09:38:04 +0800

    Add a solution to problem 1611 (#895)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1611/Solution.kt
@@ -0,0 +1,22 @@
+package com.hj.leetcode.kotlin.problem1611
+
+/**
+ * LeetCode page: [1611. Minimum One Bit Operations to Make Integers Zero](https://leetcode.com/problems/minimum-one-bit-operations-to-make-integers-zero/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(Log n) and Space O(1).
+    fun minimumOneBitOperations(n: Int): Int {
+        // Think about it in the inverted direction: make 0 into
+        // the MSB.
+        var result = 0
+        var bit = 1
+        while (bit <= n) {
+            if (bit and n != 0) {
+                result = (bit shl 1) - 1 - result
+            }
+            bit = bit shl 1
+        }
+        return result
+    }
+}
