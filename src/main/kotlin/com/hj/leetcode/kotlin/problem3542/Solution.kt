6f91e67b3015901161622a2f97e864365cd2d879
hj-core <88603338+hj-core@users.noreply.github.com>
2025-11-10 18:23:24 +0800

    Add a solution to problem 3542

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3542/Solution.kt
@@ -0,0 +1,25 @@
+package com.hj.leetcode.kotlin.problem3542
+
+/**
+ * LeetCode page: [3542. Minimum Operations to Convert All Elements to Zero](https://leetcode.com/problems/minimum-operations-to-convert-all-elements-to-zero/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of nums.
+    fun minOperations(nums: IntArray): Int {
+        val monoStack = mutableListOf(0)
+        var result = 0
+
+        for (num in nums) {
+            while (num < monoStack.last()) {
+                monoStack.removeLast()
+                result++
+            }
+            if (num > monoStack.last()) {
+                monoStack.add(num)
+            }
+        }
+        result += monoStack.size - 1
+        return result
+    }
+}
