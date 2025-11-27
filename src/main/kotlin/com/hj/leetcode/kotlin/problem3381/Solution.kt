166350058751cfc75279e545f0af324abaa33118
hj-core <88603338+hj-core@users.noreply.github.com>
2025-11-27 15:59:01 +0800

    Fix incorrect comment

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3381/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3381/Solution.kt
@@ -11,7 +11,7 @@ class Solution {
         k: Int,
     ): Long {
         // minPrefixSum[r]:= the min prefix sum such that
-        // (length - 1) % k equals (r + 1) % k. The 1-shift is
+        // (length - 1) % k equals r % k. The 1-shift is
         // introduced to eliminate the index-to-length conversion.
         val minPrefixSum = LongArray(k)
         var prefixSum = 0L
