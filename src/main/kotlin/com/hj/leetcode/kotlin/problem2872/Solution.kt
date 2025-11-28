079ef7d541b1ad7a42e68612805c8f2ee788e245
hj-core <88603338+hj-core@users.noreply.github.com>
2025-11-28 15:05:51 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2872/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2872/Solution.kt
@@ -4,32 +4,31 @@ package com.hj.leetcode.kotlin.problem2872
  * LeetCode page: [2872. Maximum Number of K-Divisible Components](https://leetcode.com/problems/maximum-number-of-k-divisible-components/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(n) and Space O(n) where M is the size of edges.
-     */
+    // Complexity:
+    // Time O(n) and Space O(n).
     fun maxKDivisibleComponents(
         n: Int,
         edges: Array<IntArray>,
         values: IntArray,
         k: Int,
-    ): Int = dfs(0, -1, adjacencyList(edges), values, k)[1]
+    ): Int = dfs(0, -1, adjacencyList(n, edges), values, k)[1]
 
     // Return the (sumPendingNodes, validSplits).
     private fun dfs(
         node: Int,
         parentNode: Int,
-        adjacencyList: Map<Int, List<Int>>,
+        adjacencyList: List<List<Int>>,
         values: IntArray,
         k: Int,
     ): IntArray {
         val result = intArrayOf(values[node] % k, 0)
-        for (child in adjacencyList[node] ?: emptyList()) {
-            if (child == parentNode) {
-                continue
+        for (child in adjacencyList[node]) {
+            if (child != parentNode) {
+                val subResult =
+                    dfs(child, node, adjacencyList, values, k)
+                result[0] = (result[0] + subResult[0]) % k
+                result[1] += subResult[1]
             }
-            val subResult = dfs(child, node, adjacencyList, values, k)
-            result[0] = (result[0] + subResult[0]) % k
-            result[1] += subResult[1]
         }
         if (result[0] == 0) {
             result[1] += 1
@@ -37,11 +36,14 @@ class Solution {
         return result
     }
 
-    private fun adjacencyList(edges: Array<IntArray>): Map<Int, List<Int>> {
-        val result = mutableMapOf<Int, MutableList<Int>>()
+    private fun adjacencyList(
+        n: Int,
+        edges: Array<IntArray>,
+    ): List<List<Int>> {
+        val result = List(n) { mutableListOf<Int>() }
         for ((u, v) in edges) {
-            result.computeIfAbsent(u) { mutableListOf() }.add(v)
-            result.computeIfAbsent(v) { mutableListOf() }.add(u)
+            result[u].add(v)
+            result[v].add(u)
         }
         return result
     }
