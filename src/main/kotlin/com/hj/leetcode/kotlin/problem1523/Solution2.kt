37f407701892351d7670ddfefea41d4766f9f533
hj-core <88603338+hj-core@users.noreply.github.com>
2025-12-07 19:32:46 +0800

    Update problem 1523: Improve implementation (#923)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1523/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1523/Solution2.kt
@@ -3,11 +3,14 @@ package com.hj.leetcode.kotlin.problem1523
 /**
  * LeetCode page: [1523. Count Odd Numbers in an Interval Range](https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/);
  */
-class Solution {
+class Solution2 {
     // Complexity:
     // Time O(1) and Space O(1).
     fun countOdds(
         low: Int,
         high: Int,
-    ): Int = (high - low + 1 + (low and 1)) / 2
+    ): Int {
+        // countOdds(0,high) - countOdds(0,low-1)
+        return (high + 1) / 2 - low / 2
+    }
 }
