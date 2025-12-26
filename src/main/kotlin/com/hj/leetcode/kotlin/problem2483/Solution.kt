862904fe8d9a0955a3c5007b9ed978580b3811cf
hj-core <88603338+hj-core@users.noreply.github.com>
2025-12-26 21:24:02 +0800

    Update problem 2483: Refactor implementations (#942)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2483/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2483/Solution.kt
@@ -4,27 +4,28 @@ package com.hj.leetcode.kotlin.problem2483
  * LeetCode page: [2483. Minimum Penalty for a Shop](https://leetcode.com/problems/minimum-penalty-for-a-shop/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(1) where N is the length of customers;
-     */
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of customers.
     fun bestClosingTime(customers: String): Int {
-        val totalY = customers.count { it == 'Y' }
-        var nCountBeforeStop = 0
-        var result = 0
-        var minPenalty = totalY
+        var penaltyY = customers.count { it == 'Y' }
+        var penaltyN = 0
+        var bestHour = 0
+        var minPenalty = penaltyY
 
-        for (stop in 0..customers.length) {
-            val yCountBeforeStop = stop - nCountBeforeStop
-            val penalty = nCountBeforeStop + (totalY - yCountBeforeStop)
+        for ((t, c) in customers.withIndex()) {
+            if (c == 'Y') {
+                penaltyY--
+            } else {
+                penaltyN++
+            }
+
+            val penalty = penaltyY + penaltyN
             if (penalty < minPenalty) {
                 minPenalty = penalty
-                result = stop
-            }
-
-            if (stop < customers.length && customers[stop] == 'N') {
-                nCountBeforeStop++
+                bestHour = t + 1
             }
         }
-        return result
+
+        return bestHour
     }
 }
