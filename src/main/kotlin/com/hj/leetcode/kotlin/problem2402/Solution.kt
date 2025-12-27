bcaaa2d8e7c52c43fc4e104a7986ef0f2181c1f4
hj-core <hj.forbetterfuture@gmail.com>
2025-12-27 11:52:27 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2402/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2402/Solution.kt
@@ -7,38 +7,71 @@ import java.util.*
  */
 class Solution {
     // Complexity:
-    // Time O(nLog(n)+MLogM+MLog(n)) and Space O(n+M) where M is the length
-    // of meetings.
+    // Time O(MLogM+(n+M)Log(n)) and Space O(n+M) where M is the
+    // length of meetings.
     fun mostBooked(
         n: Int,
         meetings: Array<IntArray>,
     ): Int {
-        val freeRooms = PriorityQueue<Int>()
+        val freeRooms = PriorityQueue<Room>(compareBy(Room::number))
         for (i in 0..<n) {
-            freeRooms.add(i)
+            freeRooms.add(
+                Room(number = i, useUntil = 0L, useCount = 0),
+            )
         }
 
-        val usedRooms = PriorityQueue<Pair<Int, Long>>(compareBy { (room, end) -> end })
-        val bookCount = IntArray(n)
-        var newStart = 0L
-        val sortedMeetings = meetings.sortedBy { (start, _) -> start }
+        val usedRooms =
+            PriorityQueue<Room>(compareBy(Room::useUntil))
+
+        val sortedMeetings =
+            meetings.sortedArrayWith(compareBy { it[0] })
+
+        var minStart = 0L // the earliest start of the next meeting
 
         for (meeting in sortedMeetings) {
-            newStart = maxOf(newStart, meeting[0].toLong())
+            minStart = maxOf(minStart, meeting[0].toLong())
             if (freeRooms.isEmpty()) {
-                newStart = maxOf(newStart, usedRooms.peek().second)
+                minStart =
+                    maxOf(minStart, usedRooms.peek().useUntil)
             }
 
-            while (usedRooms.isNotEmpty() && usedRooms.peek().second <= newStart) {
-                val room = usedRooms.poll().first
-                freeRooms.add(room)
+            while (usedRooms.isNotEmpty() &&
+                usedRooms.peek().useUntil <= minStart
+            ) {
+                freeRooms.add(usedRooms.poll())
             }
 
             val room = freeRooms.poll()
-            val end = newStart + meeting[1] - meeting[0]
-            usedRooms.add(Pair(room, end))
-            bookCount[room]++
+            room.useUntil = minStart + (meeting[1] - meeting[0])
+            room.useCount++
+            usedRooms.add(room)
         }
-        return bookCount.withIndex().maxBy { it.value }.index
+
+        var mostBookedRoom = usedRooms.peek()
+        for (rooms in arrayOf(usedRooms, freeRooms)) {
+            for (room in rooms.iterator()) {
+                if (isMoreBooked(room, mostBookedRoom)) {
+                    mostBookedRoom = room
+                }
+            }
+        }
+
+        return mostBookedRoom.number
+    }
+
+    private class Room(
+        val number: Int,
+        var useUntil: Long,
+        var useCount: Int,
+    )
+
+    private fun isMoreBooked(
+        roomA: Room,
+        roomB: Room,
+    ): Boolean {
+        if (roomA.useCount == roomB.useCount) {
+            return roomA.number < roomB.number
+        }
+        return roomA.useCount > roomB.useCount
     }
 }
