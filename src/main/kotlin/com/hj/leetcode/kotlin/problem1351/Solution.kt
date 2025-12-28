1993198a9b00dac97ae3d98cedaa16cca97038ac
hj-core <hj.forbetterfuture@gmail.com>
2025-12-28 09:45:53 +0800

    Refactor implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1351/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1351/Solution.kt
@@ -4,28 +4,24 @@ package com.hj.leetcode.kotlin.problem1351
  * LeetCode page: [1351. Count Negative Numbers in a Sorted Matrix](https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(M+N) and Space O(1) where M and N are the number of rows and columns in grid;
-     */
+    // Complexity:
+    // Time O(M+N) and Space O(1) where M and N are the number of
+    // rows and columns of grid, respectively.
     fun countNegatives(grid: Array<IntArray>): Int {
-        val hasNoNegative = grid.last().last() >= 0
-        if (hasNoNegative) {
-            return 0
-        }
-
-        val isAllNegative = grid.first().first() < 0
-        if (isAllNegative) {
-            return grid.size * grid[0].size
-        }
+        val m = grid.size
+        val n = grid[0].size
 
         var result = 0
-        var columnFirstNegative = grid[0].size
-        for (rowValues in grid) {
-            while (columnFirstNegative > 0 && rowValues[columnFirstNegative - 1] < 0) {
-                columnFirstNegative--
+        var c = 0
+
+        for (r in m - 1 downTo 0) {
+            while (c < n && 0 <= grid[r][c]) {
+                c++
             }
-            result += rowValues.size - columnFirstNegative
+
+            result += n - c
         }
+
         return result
     }
 }
