5a5d1e7c06a720eb001f76baa37a6ddd4b2c5a09
hj-core <hj.forbetterfuture@gmail.com>
2026-01-02 11:55:01 +0800

    Add a step-2 sieve to solution 1

--- src/main/kotlin/com/hj/leetcode/kotlin/problem961/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem961/Solution.kt
@@ -7,12 +7,33 @@ class Solution {
     // Complexity:
     // Time O(N) and Space O(1) where N is the length of nums.
     fun repeatedNTimes(nums: IntArray): Int {
-        for (i in 1..<nums.size) {
-            if (nums[i] == nums[i - 1]) {
+        for (i in nums.indices step 2) {
+            if (nums[i] == nums[i + 1]) {
                 return nums[i]
             }
         }
 
         return if (nums[1] == nums[3]) nums[1] else nums[0]
+    }
+
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of nums.
+    //
+    // A step of 2 improves the worst case over a step of 1, though
+    // its impact on common cases is unverified.
+    fun repeatedNTimesStep2(nums: IntArray): Int {
+        for (i in nums.indices step 2) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i]
+            }
+        }
+
+        return if (
+            nums[0] == nums[2] || nums[0] == nums[3]
+        ) {
+            nums[0]
+        } else {
+            nums[1]
+        }
     }
 }
