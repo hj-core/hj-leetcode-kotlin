4722edad8e92756e42a54dacc921643b1c535dab
hj-core <88603338+hj-core@users.noreply.github.com>
2026-01-05 20:11:21 +0800

    Update problem 1975: Improve readability (#952)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1975/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1975/Solution.kt
@@ -1,31 +1,37 @@
 package com.hj.leetcode.kotlin.problem1975
 
 import kotlin.math.abs
-import kotlin.math.min
 
 /**
  * LeetCode page: [1975. Maximum Matrix Sum](https://leetcode.com/problems/maximum-matrix-sum/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N^2) and Space O(1) where N is the number of rows and columns in matrix.
-     */
+    // Complexity:
+    // Time O(N^2) and Space O(1) where N is the number of rows and
+    // columns in matrix.
     fun maxMatrixSum(matrix: Array<IntArray>): Long {
-        // We can change any value pair (a, b) to (-a, -b) without affecting others
-        var countNegatives = 0
+        // We can negate any pair of cells without affecting others
+        // by applying operations along a path connecting those two
+        // cells. The total number of changed cells is always even.
         var sumAbs = 0L
         var minAbs = abs(matrix[0][0])
+        var countNegatives = 0
 
         for (row in matrix) {
             for (value in row) {
+                val absValue = abs(value)
+                sumAbs += absValue
+                minAbs = minOf(minAbs, absValue)
+
                 if (value < 0) {
                     countNegatives++
                 }
-                val absValue = abs(value)
-                sumAbs += absValue
-                minAbs = min(minAbs, absValue)
             }
         }
-        return if (countNegatives % 2 == 0) sumAbs else sumAbs - (2 * minAbs)
+
+        if (countNegatives and 1 == 1) {
+            sumAbs -= 2 * minAbs
+        }
+        return sumAbs
     }
 }
