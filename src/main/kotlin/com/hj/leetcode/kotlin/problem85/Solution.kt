19b0cb9cbba760abbc8d7e8e17946e42f0d90c8d
hj-core <hj.forbetterfuture@gmail.com>
2026-01-11 12:30:15 +0800

    Add a solution to problem 85

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem85/Solution.kt
@@ -0,0 +1,51 @@
+package com.hj.leetcode.kotlin.problem85
+
+/**
+ * LeetCode page: [85. Maximal Rectangle](https://leetcode.com/problems/maximal-rectangle/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(MN) and Space O(N), where M and N are the number of rows
+    // and columns of the matrix.
+    fun maximalRectangle(matrix: Array<CharArray>): Int {
+        val histogram = IntArray(matrix[0].size)
+        var maximal = 0
+        for (row in matrix) {
+            for ((i, value) in row.withIndex()) {
+                histogram[i] = if (value == '1') histogram[i] + 1 else 0
+            }
+
+            maximal =
+                maxOf(maximal, maximalRectangleInHistogram(histogram))
+        }
+
+        return maximal
+    }
+
+    private fun maximalRectangleInHistogram(histogram: IntArray): Int {
+        // Store strictly increasing (left, height) pairs, where
+        // histogram[..<left] < height and histogram[left..] >= height.
+        val monoStack = mutableListOf(Pair(-1, 0))
+        var maximal = 0
+
+        for ((i, height) in histogram.withIndex()) {
+            var left = i
+            while (
+                monoStack.isNotEmpty() &&
+                monoStack.last().second >= height
+            ) {
+                val (lastIndex, lastHeight) = monoStack.removeLast()
+                maximal = maxOf(maximal, (i - lastIndex) * lastHeight)
+                left = lastIndex
+            }
+
+            monoStack.add(Pair(left, height))
+        }
+
+        for ((left, height) in monoStack) {
+            maximal = maxOf(maximal, (histogram.size - left) * height)
+        }
+
+        return maximal
+    }
+}
