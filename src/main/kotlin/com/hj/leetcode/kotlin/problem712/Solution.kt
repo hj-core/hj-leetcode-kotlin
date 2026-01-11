e6705b74f09b8bf3cf7e3a63bb90c22d958f4e03
hj-core <88603338+hj-core@users.noreply.github.com>
2026-01-11 12:11:43 +0800

    Update problem 712: Refactor implementation (#957)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem712/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem712/Solution.kt
@@ -4,42 +4,47 @@ package com.hj.leetcode.kotlin.problem712
  * LeetCode page: [712. Minimum ASCII Delete Sum for Two Strings](https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(MN) and Space O(MN) where M and N are the length of s1 and s2 respectively;
-     */
-    fun minimumDeleteSum(s1: String, s2: String): Int {
-        // dp[lengthPrefix1][lengthPrefix2] ::= minimumDeleteSum(s1[:lengthPrefix1], s2[:lengthPrefix2])
-        val dp = Array(s1.length + 1) { IntArray(s2.length + 1) }
-
-        // Base cases that all characters of s1[:lengthPrefix1] are deleted since s2[:0] is empty
-        for (lengthPrefix1 in 1..s1.length) {
-            dp[lengthPrefix1][0] = s1[lengthPrefix1 - 1].ascii() + dp[lengthPrefix1 - 1][0]
-        }
-        // Base cases that all characters of s2[:lengthPrefix2] are deleted since s1[:0] is empty
-        for (lengthPrefix2 in 1..s2.length) {
-            dp[0][lengthPrefix2] = s2[lengthPrefix2 - 1].ascii() + dp[0][lengthPrefix2 - 1]
+    // Complexity:
+    // Time O(MN) and Space O(min(M,N)) where M and N are the lengths of
+    // s1 and s2, respectively.
+    fun minimumDeleteSum(
+        s1: String,
+        s2: String,
+    ): Int {
+        if (s1.length < s2.length) {
+            return minimumDeleteSum(s2, s1)
         }
 
-        for (lengthPrefix1 in 1..s1.length) {
-            for (lengthPrefix2 in 1..s2.length) {
-                /* If s1[lengthPrefix1-1] and s2[lengthPrefix2-1] are the same, there is a minimumDeleteSum
-                 * case that keeps both characters; otherwise, one of the two characters must be deleted.
-                 */
-                dp[lengthPrefix1][lengthPrefix2] = if (s1[lengthPrefix1 - 1] == s2[lengthPrefix2 - 1]) {
-                    dp[lengthPrefix1 - 1][lengthPrefix2 - 1]
-                } else {
-                    minOf(
-                        s1[lengthPrefix1 - 1].ascii() + dp[lengthPrefix1 - 1][lengthPrefix2],
-                        s2[lengthPrefix2 - 1].ascii() + dp[lengthPrefix1][lengthPrefix2 - 1]
-                    )
-                }
+        val m = s1.length
+        val n = s2.length
+
+        // dp[j]@i:= minimumDeleteSum(s1[i..], s2[j..])
+        val dp = IntArray(n + 1)
+
+        // Base cases where i equals m
+        for (j in n - 1 downTo 0) {
+            dp[j] = s2[j].code + dp[j + 1]
+        }
+
+        for (i in m - 1 downTo 0) {
+            var oldNext: Int // dp[i+1][j+1]
+
+            dp[n] = (s1[i].code + dp[n]).also { oldNext = dp[n] }
+            for (j in n - 1 downTo 0) {
+                dp[j] =
+                    if (s1[i] == s2[j]) {
+                        oldNext
+                    } else {
+                        minOf(
+                            // s1[i].code + dp[i+1][j]
+                            s1[i].code + dp[j],
+                            // s2[j].code + dp[i][j+1]
+                            s2[j].code + dp[j + 1],
+                        )
+                    }.also { oldNext = dp[j] }
             }
         }
-        return dp[s1.length][s2.length]
-    }
 
-    private fun Char.ascii(): Int {
-        require(this in 'a'..'z')
-        return 97 + (this - 'a')
+        return dp[0]
     }
 }
