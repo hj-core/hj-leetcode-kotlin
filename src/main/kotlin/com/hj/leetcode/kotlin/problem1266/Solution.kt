2aef22ee1e3a2f5eeef15b256b7d1f3cc2f944bc
hj-core <88603338+hj-core@users.noreply.github.com>
2026-01-12 22:29:13 +0800

    Update problem 1266: Improve implementation (#959)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1266/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1266/Solution.kt
@@ -7,19 +7,23 @@ import kotlin.math.max
  * LeetCode page: [1266. Minimum Time Visiting All Points](https://leetcode.com/problems/minimum-time-visiting-all-points/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(1) where N is the size of points;
-     */
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the size of points.
     fun minTimeToVisitAllPoints(points: Array<IntArray>): Int {
-        return points
-            .asSequence()
-            .zipWithNext()
-            .sumOf { (from, to) -> minVisitingTime(from, to) }
+        var minTime = 0
+        for (i in 1..<points.size) {
+            minTime += minTravelTime(points[i - 1], points[i])
+        }
+
+        return minTime
     }
 
-    private fun minVisitingTime(fromPoint: IntArray, toPoint: IntArray): Int {
-        val xDistance = abs(toPoint[0] - fromPoint[0])
-        val yDistance = abs(toPoint[1] - fromPoint[1])
+    private fun minTravelTime(
+        pt1: IntArray,
+        pt2: IntArray,
+    ): Int {
+        val xDistance = abs(pt2[0] - pt1[0])
+        val yDistance = abs(pt2[1] - pt1[1])
         return max(xDistance, yDistance)
     }
 }
