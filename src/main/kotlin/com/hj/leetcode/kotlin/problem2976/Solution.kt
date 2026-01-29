ee487f69cf380248e60e22dc6108f19fa6cb0dc1
hj-core <88603338+hj-core@users.noreply.github.com>
2026-01-29 22:31:41 +0800

    Update problem 2976: Improve implementation (#974)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2976/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2976/Solution.kt
@@ -1,15 +1,13 @@
 package com.hj.leetcode.kotlin.problem2976
 
-import kotlin.math.min
-
 /**
  * LeetCode page: [2976. Minimum Cost to Convert String I](https://leetcode.com/problems/minimum-cost-to-convert-string-i/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(M+N+C^3) and Space O(C^2) where M is the size of source and target,
-     * N is the size of original, changed and cost, and C is the size of charset.
-     */
+    // Complexity:
+    // Time O(M+N+C^3) and Space O(C^2) where M is the size of source
+    // and target, N is the size of original, changed and cost, and C is
+    // the size of charset.
     fun minimumCost(
         source: String,
         target: String,
@@ -23,11 +21,13 @@ class Solution {
         for (i in source.indices) {
             val from = source[i] - 'a'
             val to = target[i] - 'a'
+
             if (minCostMatrix[from][to] == Int.MAX_VALUE) {
                 return -1
             }
             result += minCostMatrix[from][to]
         }
+
         return result
     }
 
@@ -36,36 +36,44 @@ class Solution {
         changed: CharArray,
         cost: IntArray,
     ): Array<IntArray> {
-        val result = getAdjacencyMatrix(original, changed, cost)
+        val costMatrix = adjacencyMatrix(original, changed, cost)
         for (k in 0..<26) {
             for (i in 0..<26) {
+                if (costMatrix[i][k] == Int.MAX_VALUE) {
+                    continue
+                }
+
                 for (j in 0..<26) {
-                    if (result[i][k] == Int.MAX_VALUE || result[k][j] == Int.MAX_VALUE) {
-                        continue
+                    if (costMatrix[k][j] != Int.MAX_VALUE) {
+                        costMatrix[i][j] =
+                            minOf(
+                                costMatrix[i][j],
+                                costMatrix[i][k] + costMatrix[k][j],
+                            )
                     }
-                    result[i][j] = min(result[i][j], result[i][k] + result[k][j])
                 }
             }
         }
-        return result
+        return costMatrix
     }
 
-    private fun getAdjacencyMatrix(
+    private fun adjacencyMatrix(
         original: CharArray,
         changed: CharArray,
         cost: IntArray,
     ): Array<IntArray> {
-        val result = Array(26) { IntArray(26) { Int.MAX_VALUE } }
+        val matrix = Array(26) { IntArray(26) { Int.MAX_VALUE } }
         // Set cost for self loop
         for (i in 0..<26) {
-            result[i][i] = 0
+            matrix[i][i] = 0
         }
         // Set cost for edges
         for (i in original.indices) {
             val from = original[i] - 'a'
             val to = changed[i] - 'a'
-            result[from][to] = min(result[from][to], cost[i])
+            matrix[from][to] = minOf(matrix[from][to], cost[i])
         }
-        return result
+
+        return matrix
     }
 }
