95859c86c296d19937bbcf9dda1cace2736ebbbb
hj-core <88603338+hj-core@users.noreply.github.com>
2026-02-03 13:27:33 +0800

    Add a solution to problem 3013 (#977)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3013/Solution.kt
@@ -0,0 +1,67 @@
+package com.hj.leetcode.kotlin.problem3013
+
+import java.util.*
+
+/**
+ * LeetCode page: [3013. Divide an Array Into Subarrays With Minimum Cost II](https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-ii/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(NLogD) and Space O(D) where N is the length of nums and D
+    // is dist.
+    fun minimumCost(
+        nums: IntArray,
+        k: Int,
+        dist: Int,
+    ): Long {
+        // We slide a window of size dist, tracking the sum of the k-1
+        // smallest numbers within it. We use a TreeSet to store the k-1
+        // smallest indexed values in the window, while using a
+        // PriorityQueue for the rest.
+        val small = TreeSet<IntArray>(compareBy({ -it[1] }, { it[0] }))
+        val big =
+            PriorityQueue<IntArray>(compareBy({ it[1] }, { it[0] }))
+
+        var smallSum = 0L
+        for (i in 1..dist + 1) {
+            small.add(intArrayOf(i, nums[i]))
+            smallSum += nums[i]
+        }
+        while (small.size >= k) {
+            val firstSmall = checkNotNull(small.pollFirst())
+            smallSum -= firstSmall[1]
+            big.offer(firstSmall)
+        }
+
+        var minSmallSum = smallSum
+        for (i in dist + 2..<nums.size) {
+            val popIndex = i - dist - 1
+            if (nums[popIndex] <= small.first()[1] &&
+                small.remove(intArrayOf(popIndex, nums[popIndex]))
+            ) {
+                smallSum -= nums[popIndex]
+            }
+
+            big.offer(intArrayOf(i, nums[i]))
+            while (big.isNotEmpty() && big.first()[0] <= popIndex) {
+                big.poll()
+            }
+
+            if (small.size < k - 1) {
+                val firstBig = checkNotNull(big.poll())
+                smallSum += firstBig[1]
+                small.add(firstBig)
+            } else if (small.first()[1] > big.first()[1]) {
+                smallSum += big.first()[1] - small.first()[1]
+                val firstSmall = checkNotNull(small.pollFirst())
+                val firstBig = checkNotNull(big.poll())
+                small.add(firstBig)
+                big.add(firstSmall)
+            }
+
+            minSmallSum = minOf(minSmallSum, smallSum)
+        }
+
+        return nums[0] + minSmallSum
+    }
+}
---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3013/Solution2.kt
@@ -0,0 +1,88 @@
+package com.hj.leetcode.kotlin.problem3013
+
+import java.util.*
+
+class Solution2 {
+    // Complexity:
+    // Time O(NLogN) and Space O(N) where N is the length of nums.
+    fun minimumCost(
+        nums: IntArray,
+        k: Int,
+        dist: Int,
+    ): Long {
+        // Pqs of (i, nums[i]), sorted by different orders. They may
+        // contain numbers that are out of the current window.
+        val small =
+            PriorityQueue<IntArray>(compareBy({ -it[1] }, { it[0] }))
+        val big =
+            PriorityQueue<IntArray>(compareBy({ it[1] }, { -it[0] }))
+
+        var smallSum = 0L // The sum of valid numbers in small
+        for (i in 1..dist + 1) {
+            small.offer(intArrayOf(i, nums[i]))
+            smallSum += nums[i]
+        }
+        while (small.size > k - 1) {
+            val firstSmall = checkNotNull(small.poll())
+            smallSum -= firstSmall[1]
+            big.offer(firstSmall)
+        }
+
+        // The number of valid numbers in small
+        var smallSize = k - 1
+        // The largest number in the last size k-1 smallSum
+        var threshold = small.first()[1]
+        var minSmallSum = smallSum
+
+        for (i in dist + 2..<nums.size) {
+            val popIndex = i - dist - 1
+            if (nums[popIndex] < threshold) {
+                smallSum -= nums[popIndex]
+                smallSize--
+            } else if (small.isNotEmpty() &&
+                small.first()[0] == popIndex
+            ) {
+                smallSum -= small.first()[1]
+                smallSize--
+                small.poll()
+            }
+
+            // Only a nums[i] < threshold may improve the last size k-1
+            // smallSum.
+            if (nums[i] >= threshold) {
+                big.offer(intArrayOf(i, nums[i]))
+            } else {
+                smallSum += nums[i]
+                smallSize++
+                small.offer(intArrayOf(i, nums[i]))
+                while (smallSize > k - 1) {
+                    val firstSmall = checkNotNull(small.poll())
+                    if (firstSmall[0] > popIndex) {
+                        smallSum -= firstSmall[1]
+                        smallSize--
+                        big.offer(firstSmall)
+                    }
+                }
+                while (smallSize < k - 1) {
+                    val firstBig = checkNotNull(big.poll())
+                    if (firstBig[0] > popIndex) {
+                        smallSum += firstBig[1]
+                        smallSize++
+                        small.offer(firstBig)
+                    }
+                }
+
+                while (small.first()[0] <= popIndex) {
+                    small.poll()
+                }
+                threshold = small.first()[1]
+
+                if (smallSum < minSmallSum) {
+                    minSmallSum = smallSum
+                }
+            }
+        }
+
+        return nums[0] + minSmallSum
+    }
+}
