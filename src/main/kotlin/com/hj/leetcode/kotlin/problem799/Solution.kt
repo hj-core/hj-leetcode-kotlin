14e69d5004ad3744d48781c4e57aa333e83babca
hj-core <hj.forbetterfuture@gmail.com>
2026-02-14 13:58:57 +0800

    Improve implementation with symmetry

--- src/main/kotlin/com/hj/leetcode/kotlin/problem799/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem799/Solution.kt
@@ -11,18 +11,20 @@ class Solution {
         query_row: Int,
         query_glass: Int,
     ): Double {
+        val queryGlass = minOf(query_glass, query_row - query_glass)
+
         // dp[c]@r:= total champagne received by glass (r, c)
-        val dp = DoubleArray(query_glass + 2)
+        val dp = DoubleArray(queryGlass + 2)
         dp[0] = poured.toDouble()
 
         for (r in 0..<query_row) {
-            for (c in minOf(query_glass, r) downTo 0) {
+            for (c in minOf(queryGlass, r) downTo 0) {
                 val toNext = maxOf(0.0, (dp[c] - 1.0)) / 2
                 dp[c] = toNext
                 dp[c + 1] += toNext
             }
         }
 
-        return minOf(1.0, dp[query_glass])
+        return minOf(1.0, dp[queryGlass])
     }
 }
