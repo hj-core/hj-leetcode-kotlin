6f38fbbe27c8fba7f2eb81a5d4686365feb32403
hj-core <88603338+hj-core@users.noreply.github.com>
2026-03-11 12:42:45 +0800

    Update problem 1582: improve implementation (#990)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1582/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1582/Solution.kt
@@ -4,49 +4,27 @@ package com.hj.leetcode.kotlin.problem1582
  * LeetCode page: [1582. Special Positions in a Binary Matrix](https://leetcode.com/problems/special-positions-in-a-binary-matrix/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(MN) and Space O(N) where M and N are the number of rows
-     * and columns of mat;
-     */
+    // Complexity:
+    // Time O(MN) and Space O(M+N) where M and N are the number of rows
+    // and columns of mat, respectively.
     fun numSpecial(mat: Array<IntArray>): Int {
-        val memoization = hashMapOf<Int, Boolean>() // entry=(column, containsSingleOne)
-        return candidateColumns(mat).count { containsSingleOne(mat, it, memoization) }
-    }
+        val m = mat.size
+        val n = mat[0].size
+        val rowOnes = Array(m) { intArrayOf(0, 0) }
+        val colOnes = IntArray(n)
 
-    private fun candidateColumns(mat: Array<IntArray>): List<Int> {
-        val result = mutableListOf<Int>()
-        for (row in mat) {
-            val firstOneColumn = row.indexOfFirst { it == 1 }
-            if (firstOneColumn == -1) {
-                continue
-            }
-
-            val lastOneColumn = row.indexOfLast { it == 1 }
-            if (firstOneColumn == lastOneColumn) {
-                result.add(firstOneColumn)
+        for ((r, row) in mat.withIndex()) {
+            for ((c, num) in row.withIndex()) {
+                if (num == 1) {
+                    rowOnes[r][0]++
+                    rowOnes[r][1] = c
+                    colOnes[c]++
+                }
             }
         }
-        return result
-    }
 
-    private fun containsSingleOne(
-        mat: Array<IntArray>,
-        column: Int,
-        memoization: MutableMap<Int, Boolean>,
-    ): Boolean {
-
-        if (column in memoization) {
-            return checkNotNull(memoization[column])
+        return rowOnes.count { (cnt, lastOne) ->
+            cnt == 1 && colOnes[lastOne] == 1
         }
-
-        val firstOneRow = mat.indexOfFirst { it[column] == 1 }
-        if (firstOneRow == -1) {
-            memoization[column] = false
-            return false
-        }
-
-        val lastOneRow = mat.indexOfLast { it[column] == 1 }
-        memoization[column] = firstOneRow == lastOneRow
-        return checkNotNull(memoization[column])
     }
 }
