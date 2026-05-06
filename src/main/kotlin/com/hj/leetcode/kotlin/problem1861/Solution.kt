db50b949f2fc31ac1ee8f335e7f24deb178d5ada
hj-core <88603338+hj-core@users.noreply.github.com>
2026-05-06 22:23:14 +0800

    Update problem 1861: Improve readability (#1022)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1861/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1861/Solution.kt
@@ -4,41 +4,42 @@ package com.hj.leetcode.kotlin.problem1861
  * LeetCode page: [1861. Rotating the Box](https://leetcode.com/problems/rotating-the-box/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(MN) and Auxiliary Space O(1) where M is the number of rows in box
-     * and N is the number of columns in box.
-     */
+    // Complexity:
+    // Time O(MN) and Auxiliary Space O(1) where M is the number of rows in box
+    // and N is the number of columns in box.
     fun rotateTheBox(box: Array<CharArray>): Array<CharArray> {
         val (stone, obstacle, empty) = charArrayOf('#', '*', '.')
         val m = box.size
         val n = box[0].size
-        val result = Array(n) { CharArray(m) { empty } }
+        val rotatedBox = Array(n) { CharArray(m) { empty } }
 
-        for (colAfter in result[0].indices) {
-            var baseRow = result.size
-            for (rowAfter in result.indices.reversed()) {
-                when (beforeRotate(box, rowAfter, colAfter)) {
-                    empty -> continue
+        for (c in rotatedBox[0].indices) {
+            var base = n // the row index to support the next stone
+
+            for (r in rotatedBox.indices.reversed()) {
+                val oldContent = box[m - 1 - c][r]
+                when (oldContent) {
+                    empty -> {
+                        continue
+                    }
+
                     stone -> {
-                        baseRow -= 1
-                        result[baseRow][colAfter] = stone
+                        base -= 1
+                        rotatedBox[base][c] = stone
                     }
 
                     obstacle -> {
-                        baseRow = rowAfter
-                        result[baseRow][colAfter] = obstacle
+                        base = r
+                        rotatedBox[base][c] = obstacle
                     }
 
-                    else -> throw IllegalArgumentException("Unexpected item.")
+                    else -> {
+                        throw IllegalArgumentException("Unexpected item.")
+                    }
                 }
             }
         }
-        return result
-    }
 
-    private fun beforeRotate(
-        box: Array<CharArray>,
-        rowAfter: Int,
-        colAfter: Int,
-    ): Char = box[box.lastIndex - colAfter][rowAfter]
+        return rotatedBox
+    }
 }
