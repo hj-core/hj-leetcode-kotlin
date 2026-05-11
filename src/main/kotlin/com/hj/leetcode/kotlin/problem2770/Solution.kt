ff4960ed5e5abe0ee67dfbe82d9e56d10a49c27d
hj-core <88603338+hj-core@users.noreply.github.com>
2026-05-11 21:49:18 +0800

    Add a solution to problem 2770 (#1026)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2770/Solution.kt
@@ -0,0 +1,31 @@
+package com.hj.leetcode.kotlin.problem2770
+
+import kotlin.math.abs
+
+/**
+ * LeetCode page: [2770. Maximum Number of Jumps to Reach the Last Index](https://leetcode.com/problems/maximum-number-of-jumps-to-reach-the-last-index/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N^2) and Space O(N) where N is the length of nums.
+    fun maximumJumps(
+        nums: IntArray,
+        target: Int,
+    ): Int {
+        // dp[i]:= the maximum jumps starting from index i
+        val dp = IntArray(nums.size) { -1 }
+
+        // We can use a segment tree with coordinate compression to improve the
+        // time complexity.
+        dp[nums.lastIndex] = 0
+        for (i in nums.size - 2 downTo 0) {
+            for (j in i + 1..<nums.size) {
+                if (abs(nums[i] - nums[j]) <= target && 0 <= dp[j]) {
+                    dp[i] = maxOf(dp[i], 1 + dp[j])
+                }
+            }
+        }
+
+        return dp[0]
+    }
+}
