bcfd295844ad1d90c6b280e43ce225da2882a8cc
hj-core <hj.forbetterfuture@gmail.com>
2026-05-17 09:44:04 +0800

    Add a solution to problem 1306

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1306/Solution.kt
@@ -0,0 +1,32 @@
+package com.hj.leetcode.kotlin.problem1306
+
+/**
+ * LeetCode page: [1306. Jump Game III](https://leetcode.com/problems/jump-game-iii/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of arr.
+    fun canReach(
+        arr: IntArray,
+        start: Int,
+    ): Boolean {
+        val visited = BooleanArray(arr.size)
+        return dfs(start, arr, visited)
+    }
+
+    private fun dfs(
+        start: Int,
+        arr: IntArray,
+        visited: BooleanArray,
+    ): Boolean {
+        if (start !in arr.indices || visited[start]) {
+            return false
+        }
+        if (arr[start] == 0) {
+            return true
+        }
+
+        visited[start] = true
+        return dfs(start - arr[start], arr, visited) || dfs(start + arr[start], arr, visited)
+    }
+}
