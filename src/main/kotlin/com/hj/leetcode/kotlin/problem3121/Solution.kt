1e128b7b72e52831e0b0e17085016e21701b470a
hj-core <88603338+hj-core@users.noreply.github.com>
2026-05-27 22:32:50 +0800

    Add a solution to problem 3121 (#1042)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3121/Solution.kt
@@ -0,0 +1,23 @@
+package com.hj.leetcode.kotlin.problem3121
+
+/**
+ * LeetCode page: [3121. Count the Number of Special Characters II](https://leetcode.com/problems/count-the-number-of-special-characters-ii/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of word.
+    fun numberOfSpecialChars(word: String): Int {
+        // visited >> (c - 'A') & 1 = 1 if c in word
+        var visited = 0L
+        // inOrder >> (lower(c) - 'a') & 1 = 1 if no lower(c) after upper(c)
+        var inOrder = 0xFFFF_FFFF
+
+        for (c in word) {
+            val bit = 1L shl (c - 'A')
+            visited = visited or bit
+            inOrder = (bit shr 32 and visited).inv() and inOrder
+        }
+
+        return (visited shr 32 and visited and inOrder).countOneBits()
+    }
+}
