fec83e2725c55312e180b089376896e0272055bd
hj-core <hj.forbetterfuture@gmail.com>
2026-06-08 10:15:47 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2161/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2161/Solution.kt
@@ -5,30 +5,21 @@ package com.hj.leetcode.kotlin.problem2161
  */
 class Solution {
     // Complexity:
-    // Time O(N) and Space O(N) where N is the length of `nums`.
+    // Time O(N) and Space O(N) where N is the length of nums.
     fun pivotArray(
         nums: IntArray,
         pivot: Int,
     ): IntArray {
-        var i = 0 // The index in `nums` to place the next number
-        val largerNums = mutableListOf<Int>() // The larger numbers in their original order
-
-        // Handle the smaller numbers and collect the larger numbers
+        val result = IntArray(nums.size) { pivot }
+        var i = 0
+        var j = nums.size - nums.count { it > pivot }
         for (num in nums) {
-            if (num < pivot) {
-                nums[i++] = num
-            } else if (num > pivot) {
-                largerNums.add(num)
+            when {
+                num < pivot -> result[i++] = num
+                num > pivot -> result[j++] = num
             }
         }
-        // Handle the pivots
-        repeat(nums.size - largerNums.size - i) {
-            nums[i++] = pivot
-        }
-        // Handle the larger numbers
-        for (num in largerNums) {
-            nums[i++] = num
-        }
-        return nums
+
+        return result
     }
 }
