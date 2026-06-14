7d9567367d1d518ef335657da85a18939a73db69
hj-core <hj.forbetterfuture@gmail.com>
2026-06-14 09:56:21 +0800

    Simplify implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2130/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2130/Solution.kt
@@ -6,46 +6,21 @@ import com.hj.leetcode.kotlin.common.model.ListNode
  * LeetCode page: [2130. Maximum Twin Sum of a Linked List](https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(N) where N is the size of head;
-     */
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of head.
     fun pairSum(head: ListNode?): Int {
-        // Collect all values in the linkedList then compute all twin sums and return the maximum
-        return maxTwinSum(head.values())
+        val values = head.values()
+        return (0..<values.size / 2).maxOf { values[it] + values[values.lastIndex - it] }
     }
 
     private fun ListNode?.values(): List<Int> {
-        val result = mutableListOf<Int>()
-        linearTraversal { node -> result.add(node.`val`) }
-        return result
-    }
-
-    private fun ListNode?.linearTraversal(onEachNode: (node: ListNode) -> Unit) {
-        var currentNode = this
-
-        while (currentNode != null) {
-            onEachNode(currentNode)
-            currentNode = currentNode.next
+        val values = mutableListOf<Int>()
+        var node = this
+        while (node != null) {
+            values.add(node.`val`)
+            node = node.next
         }
+
+        return values
     }
-
-    private fun maxTwinSum(values: List<Int>): Int {
-        require(values.isNotEmpty())
-        require(values.size.isEven())
-
-        // Compute all twin sums and return the maximum
-        var maxTwinSum = values[0] + values[values.lastIndex]
-        for (index in 0 until (values.size / 2)) {
-            val value = values[index]
-            val twinValue = values[values.lastIndex - index]
-            val twinSum = value + twinValue
-
-            if (twinSum > maxTwinSum) {
-                maxTwinSum = twinSum
-            }
-        }
-        return maxTwinSum
-    }
-
-    private fun Int.isEven(): Boolean = this and 1 == 0
 }
