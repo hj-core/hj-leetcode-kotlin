ab00234afa9dc37b9ae24c32ef97fd893cb1724f
hj-core <hj.forbetterfuture@gmail.com>
2026-06-21 18:48:20 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1833/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1833/Solution.kt
@@ -4,18 +4,20 @@ package com.hj.leetcode.kotlin.problem1833
  * LeetCode page: [1833. Maximum Ice Cream Bars](https://leetcode.com/problems/maximum-ice-cream-bars/);
  */
 class Solution {
-    /* Complexity:
-     * Time (NLogN) and Space O(N) where N is the size of costs;
-     */
-    fun maxIceCream(costs: IntArray, coins: Int): Int {
-        val sortedCosts = costs.sorted()
-        var maxIceCream = 0
-        var remainingCoins = coins
-        for (cost in sortedCosts) {
-            if (remainingCoins < cost) break
-            maxIceCream++
-            remainingCoins -= cost
+    // Complexity:
+    // Time (NLogN) and Space O(N) where N is the size of costs.
+    fun maxIceCream(
+        costs: IntArray,
+        coins: Int,
+    ): Int {
+        val costs = costs.sortedArray()
+        var coins = coins
+        for ((i, cost) in costs.withIndex()) {
+            coins -= cost
+            if (coins < 0) {
+                return i
+            }
         }
-        return maxIceCream
+        return costs.size
     }
 }
