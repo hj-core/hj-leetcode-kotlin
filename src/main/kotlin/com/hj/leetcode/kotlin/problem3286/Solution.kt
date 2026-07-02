ad544dc2d0f5482014b266457f4ccd7220f76242
hj-core <hj.forbetterfuture@gmail.com>
2026-07-02 11:54:54 +0800

    Add a solution to problem 3286

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3286/Solution.kt
@@ -0,0 +1,44 @@
+package com.hj.leetcode.kotlin.problem3286
+
+/**
+ * LeetCode page: [3286. Find a Safe Walk Through a Grid](https://leetcode.com/problems/find-a-safe-walk-through-a-grid/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(mn) and Space O(mn) where m and n are the number of rows and
+    // columns of grid, respectively.
+    fun findSafeWalk(
+        grid: List<List<Int>>,
+        health: Int,
+    ): Boolean {
+        val m = grid.size
+        val n = grid[0].size
+        val dirs = intArrayOf(-1, 0, 1, 0, -1)
+        val minHealth = Array(m) { IntArray(n) { Int.MAX_VALUE } }
+        val queue = ArrayDeque<Pair<Int, Int>>()
+        minHealth[0][0] = grid[0][0] + 1
+        queue.add(Pair(0, 0))
+        // 0-1 BFS
+        while (queue.isNotEmpty()) {
+            val (r, c) = queue.removeFirst()
+            for (i in 0..<4) {
+                val nextR = r + dirs[i]
+                val nextC = c + dirs[i + 1]
+                if (nextR !in grid.indices || nextC !in grid[nextR].indices) {
+                    continue
+                }
+                if (minHealth[nextR][nextC] != Int.MAX_VALUE) {
+                    continue
+                }
+                if (grid[nextR][nextC] == 0) {
+                    minHealth[nextR][nextC] = minHealth[r][c]
+                    queue.addFirst(Pair(nextR, nextC))
+                } else {
+                    minHealth[nextR][nextC] = minHealth[r][c] + 1
+                    queue.add(Pair(nextR, nextC))
+                }
+            }
+        }
+        return minHealth[m - 1][n - 1] <= health
+    }
+}
