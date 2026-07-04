bdd9cf329644761d85b8b9a662eb7df1f72d613e
hj-core <hj.forbetterfuture@gmail.com>
2026-07-04 19:20:49 +0800

    Improve readability of the uf solution

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2492/Solution2.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2492/Solution2.kt
@@ -4,60 +4,83 @@ package com.hj.leetcode.kotlin.problem2492
  * LeetCode page: [2492. Minimum Score of a Path Between Two Cities](https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/);
  */
 class Solution2 {
-    /* Complexity:
-     * Time O(n+E) and Space O(n) where E is the size of roads;
-     */
-    fun minScore(n: Int, roads: Array<IntArray>): Int {
-        val cityUnionFind = buildUnionFindOfCity(n, roads)
-        check(cityUnionFind.inSameUnion(1, n))
+    // Complexity:
+    // Time O(n+E) and Space O(n) where E is the length of roads.
+    fun minScore(
+        n: Int,
+        roads: Array<IntArray>,
+    ): Int {
+        val cityUnionFind = newCityUnionFind(n, roads)
+        check(cityUnionFind.inConnected(1, n))
         return minDistanceAmongReachableRoads(1, roads, cityUnionFind)
     }
 
-    private fun buildUnionFindOfCity(maxLabel: Int, roads: Array<IntArray>): UnionFind {
-        val unionFind = UnionFind(maxLabel + 1)
+    private fun newCityUnionFind(
+        n: Int,
+        roads: Array<IntArray>,
+    ): UnionFind {
+        val uf = UnionFind(n + 1)
         for ((u, v, _) in roads) {
-            unionFind.union(u, v)
+            uf.union(u, v)
         }
-        return unionFind
+        return uf
     }
 
-    private class UnionFind(size: Int) {
-
+    private class UnionFind(
+        size: Int,
+    ) {
         private val parent = IntArray(size) { it }
         private val rank = IntArray(size)
 
-        fun union(u: Int, v: Int) {
-            val uParent = find(u)
-            val vParent = find(v)
-            if (uParent == vParent) return
+        fun union(
+            u: Int,
+            v: Int,
+        ) {
+            val parent1 = find(u)
+            val parent2 = find(v)
+            if (parent1 == parent2) {
+                return
+            }
 
-            val uRank = rank[uParent]
-            val vRank = rank[vParent]
+            val rank1 = rank[parent1]
+            val rank2 = rank[parent2]
             when {
-                uRank > vRank -> parent[vParent] = uParent
-                uRank < vRank -> parent[uParent] = vParent
+                rank1 > rank2 -> {
+                    parent[parent2] = parent1
+                }
+
+                rank1 < rank2 -> {
+                    parent[parent1] = parent2
+                }
+
                 else -> {
-                    parent[vParent] = uParent
-                    rank[uParent]++
+                    parent[parent2] = parent1
+                    rank[parent1]++
                 }
             }
         }
 
         private fun find(city: Int): Int {
-            return if (city == parent[city]) city else find(parent[city])
+            if (parent[city] != city) {
+                parent[city] = find(parent[city])
+            }
+            return parent[city]
         }
 
-        fun inSameUnion(u: Int, v: Int): Boolean = find(u) == find(v)
+        fun inConnected(
+            u: Int,
+            v: Int,
+        ): Boolean = find(u) == find(v)
     }
 
     private fun minDistanceAmongReachableRoads(
-        origin: Int,
+        start: Int,
         roads: Array<IntArray>,
-        cityUnionFind: UnionFind
+        cityUnionFind: UnionFind,
     ): Int {
         var minDistance = Int.MAX_VALUE
         for ((u, _, distance) in roads) {
-            if (cityUnionFind.inSameUnion(u, origin) && distance < minDistance) {
+            if (cityUnionFind.inConnected(u, start) && distance < minDistance) {
                 minDistance = distance
             }
         }
