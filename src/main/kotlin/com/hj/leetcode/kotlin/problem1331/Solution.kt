4f8320f54dd508b4663c14a0c54b8921d60c31a5
hj-core <hj.forbetterfuture@gmail.com>
2026-07-12 18:25:06 +0800

    Improve readability

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1331/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1331/Solution.kt
@@ -4,24 +4,21 @@ package com.hj.leetcode.kotlin.problem1331
  * LeetCode page: [1331. Rank Transform of an Array](https://leetcode.com/problems/rank-transform-of-an-array/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(NLogN) and Space O(N) where N is the length of arr.
-     */
+    // Complexity:
+    // Time O(NLogN) and Space O(N) where N is the length of arr.
     fun arrayRankTransform(arr: IntArray): IntArray {
         if (arr.isEmpty()) {
             return intArrayOf()
         }
-        val sortedIndices = arr.indices.sortedBy { arr[it] }
-        val result = IntArray(arr.size)
-        var prevNum = arr[sortedIndices[0]] - 1
-        var rank = 0
-        for (index in sortedIndices) {
-            if (arr[index] != prevNum) {
-                rank += 1
-            }
-            result[index] = rank
-            prevNum = arr[index]
+
+        val indices = arr.indices.sortedBy { arr[it] }
+        val rank = IntArray(arr.size)
+        rank[indices[0]] = 1
+        for (i in 1..<indices.size) {
+            rank[indices[i]] =
+                rank[indices[i - 1]] + (if (arr[indices[i]] == arr[indices[i - 1]]) 0 else 1)
         }
-        return result
+
+        return rank
     }
 }
