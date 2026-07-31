f6625363b495ae2f7a74759aa47245358ee7f4b7
hj-core <hj.forbetterfuture@gmail.com>
2026-07-31 09:56:33 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem3016/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3016/Solution.kt
@@ -4,23 +4,15 @@ package com.hj.leetcode.kotlin.problem3016
  * LeetCode page: [3016. Minimum Number of Pushes to Type Word II](https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(1) where N is the length of word;
-     */
+    // Complexity:
+    // Time O(N) and Space O(1) where N is the length of word.
     fun minimumPushes(word: String): Int {
-        val countChar = MutableList(26) { 0 }
+        val charFreq = IntArray(26)
         for (c in word) {
-            countChar[c - 'a']++
+            charFreq[c - 'a']++
         }
+        charFreq.sortDescending()
 
-        val pushes = buildList {
-            repeat(8) { add(1) }
-            repeat(8) { add(2) }
-            repeat(8) { add(3) }
-            repeat(2) { add(4) }
-        }
-
-        countChar.sortBy { -it }
-        return (0..<26).sumOf { countChar[it] * pushes[it] }
+        return charFreq.withIndex().sumOf { (i, freq) -> freq * (i / 8 + 1) }
     }
 }
