ff93204cc86101664ac200eaead9767a7ccab74b
hj-core <hj.forbetterfuture@gmail.com>
2026-08-02 09:45:44 +0800

    Add a solution to problem 877

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem877/Solution.kt
@@ -0,0 +1,10 @@
+package com.hj.leetcode.kotlin.problem877
+
+/**
+ * LeetCode page: [877. Stone Game](https://leetcode.com/problems/stone-game/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(1) and Space O(1).
+    fun stoneGame(piles: IntArray): Boolean = true
+}
