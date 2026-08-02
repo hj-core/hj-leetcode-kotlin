47375e7fc016456eec0dd0c7a3095317d2f6c527
hj-core <88603338+hj-core@users.noreply.github.com>
2026-08-02 10:00:45 +0800

    Add a solution to problem 877 (#1093)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem877/Solution.kt
@@ -0,0 +1,14 @@
+package com.hj.leetcode.kotlin.problem877
+
+/**
+ * LeetCode page: [877. Stone Game](https://leetcode.com/problems/stone-game/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(1) and Space O(1).
+    fun stoneGame(piles: IntArray): Boolean {
+        // Alice can choose to pick either all the odd-indexed piles or all the
+        // even-indexed piles; thus, Alice can always win.
+        return true
+    }
+}
