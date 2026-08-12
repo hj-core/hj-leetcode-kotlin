c0d30750ec546cf685c5cc8cb782dd51830b9e64
hj-core <hj.forbetterfuture@gmail.com>
2026-08-12 10:44:30 +0800

    Improve implementation

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2958/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2958/Solution.kt
@@ -6,23 +6,33 @@ import kotlin.math.max
  * LeetCode page: [2958. Length of Longest Subarray With at Most K Frequency](https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N) and Space O(N) where N is the size of nums;
-     */
-    fun maxSubarrayLength(nums: IntArray, k: Int): Int {
-        var result = 0
-        var start = 0
-        val counter = hashMapOf<Int, Int>() // number to its count
+    // Complexity:
+    // Time O(N) and Space O(N) where N is the length of nums.
+    fun maxSubarrayLength(
+        nums: IntArray,
+        k: Int,
+    ): Int {
+        val freq = hashMapOf<Int, Int>()
+        var left = 0
+        var breachCount = 0
 
-        for (end in nums.indices) {
-            counter.compute(nums[end]) { _, count -> 1 + (count ?: 0) }
-
-            while (counter[nums[end]]?.let { it > k } == true) {
-                counter.compute(nums[start]) { _, count -> checkNotNull(count) - 1 }
-                start += 1
+        for (right in nums.indices) {
+            freq.compute(nums[right]) { _, v -> (v ?: 0) + 1 }?.let {
+                if (it == k + 1) {
+                    breachCount++
+                }
             }
-            result = max(result, end - start + 1)
+
+            if (breachCount > 0) {
+                freq.compute(nums[left]) { _, v -> (v ?: 0) - 1 }?.let {
+                    if (it == k) {
+                        breachCount--
+                    }
+                }
+                left++
+            }
         }
-        return result
+
+        return nums.size - left
     }
 }
