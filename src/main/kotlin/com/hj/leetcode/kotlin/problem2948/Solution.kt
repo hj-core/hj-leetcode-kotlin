d09a6bef68cd6243f27001357430725c2b574cd5
hj-core <88603338+hj-core@users.noreply.github.com>
2026-08-29 22:26:37 +0800

    Update problem 2948: Improve implementation (#1114)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem2948/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem2948/Solution.kt
@@ -4,36 +4,48 @@ package com.hj.leetcode.kotlin.problem2948
  * LeetCode page: [2948. Make Lexicographically Smallest Array by Swapping Elements](https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(NLogN) and Space O(N) where N is the length of nums.
-     */
+    // Complexity:
+    // Time O(NLogN) and Space O(N) where N is the length of nums.
     fun lexicographicallySmallestArray(
         nums: IntArray,
         limit: Int,
     ): IntArray {
-        val sortedNums = nums.withIndex().toMutableList()
-        val dummy = IndexedValue(index = -1, value = Int.MAX_VALUE)
-        sortedNums.add(dummy)
-        sortedNums.sortBy { it.value }
+        val valueIndices = LongArray(nums.size) { valueIndex(nums[it], it) }
+        valueIndices.sort()
 
-        // Partition the sorted numbers into the minimum number of groups where the
-        // difference between adjacent numbers in each group doesn't exceed the limit.
-        // Swap the numbers in `nums` group by group according to the sorted order.
         val result = IntArray(nums.size)
-        val groupIndices = mutableListOf<Int>()
-        var prevValue = sortedNums[0].value
-
-        for (i in sortedNums.indices) {
-            if (prevValue + limit < sortedNums[i].value) {
-                groupIndices.sortDescending()
-                repeat(groupIndices.size) {
-                    result[groupIndices[it]] = sortedNums[i - 1 - it].value
-                }
-                groupIndices.clear()
+        var start = 0
+        for (end in 1..<nums.size) {
+            val isNewGroup = getValue(valueIndices[end]) - getValue(valueIndices[end - 1]) > limit
+            if (isNewGroup) {
+                assignResult(valueIndices, start, end, result)
+                start = end
             }
-            prevValue = sortedNums[i].value
-            groupIndices.add(sortedNums[i].index)
         }
+        assignResult(valueIndices, start, nums.size, result)
+
         return result
+    }
+
+    private fun valueIndex(
+        value: Int,
+        index: Int,
+    ): Long = value.toLong() shl 32 or index.toLong()
+
+    private fun getValue(valueIndex: Long): Int = (valueIndex ushr 32).toInt()
+
+    private fun getIndex(valueIndex: Long): Int = (valueIndex and 0xFFFF_FFFF).toInt()
+
+    private fun assignResult(
+        valueIndices: LongArray,
+        start: Int,
+        end: Int,
+        result: IntArray,
+    ) {
+        val idxAssignOrder = IntArray(end - start) { getIndex(valueIndices[start + it]) }
+        idxAssignOrder.sort()
+        for (i in start..<end) {
+            result[idxAssignOrder[i - start]] = getValue(valueIndices[i])
+        }
     }
 }
