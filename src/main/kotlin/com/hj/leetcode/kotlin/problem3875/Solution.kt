c04283b23198384d61e5d64cfa26b47e00e7da45
hj-core <88603338+hj-core@users.noreply.github.com>
2026-09-02 19:07:55 +0800

    Add a solution to problem 3875 (#1117)

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3875/Solution.kt
@@ -0,0 +1,10 @@
+package com.hj.leetcode.kotlin.problem3875
+
+/**
+ * LeetCode page: [3875. Construct Uniform Parity Array I](https://leetcode.com/problems/construct-uniform-parity-array-i/);
+ */
+class Solution {
+    // Complexity:
+    // Time O(1) and Space O(1)
+    fun uniformArray(nums1: IntArray): Boolean = true
+}
