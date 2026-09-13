285558ca4f2b0cc41bf561873291146e6ffb396d
hj-core <88603338+hj-core@users.noreply.github.com>
2026-09-13 17:51:20 +0800

    Update problem 835: Improve implementation (#1126)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem835/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem835/Solution.kt
@@ -4,76 +4,48 @@ package com.hj.leetcode.kotlin.problem835
  * LeetCode page: [835. Image Overlap](https://leetcode.com/problems/image-overlap/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(N^4) and Space O(N^2) where N is the size of img1 and img2;
-     */
-    fun largestOverlap(img1: Array<IntArray>, img2: Array<IntArray>): Int {
-        val numOnesInImg1 = countOnes(img1)
-        val numOnesInImg2 = countOnes(img2)
-        val minNumOnes = minOf(numOnesInImg1, numOnesInImg2)
-        if (minNumOnes == 0 || minNumOnes == 1) return minNumOnes
+    // Complexity:
+    // Time O(N^3) and Space O(N) where N is the size of img1 and img2.
+    fun largestOverlap(
+        img1: Array<IntArray>,
+        img2: Array<IntArray>,
+    ): Int {
+        val n = img1.size
+        val img1 = toIntArray(img1)
+        val img2 = toIntArray(img2)
 
-        val (lessContent, moreContent) = if (numOnesInImg1 < numOnesInImg2) img1 to img2 else img2 to img1
-        val coordinatesOfOnes = findCoordinatesOfOnes(lessContent)
-
-        val size = img1.size
-        val possibleShift = -(size - 1) until size
-        var largestOverlap = 0
-
-        for (rowShift in possibleShift) {
-            for (columnShift in possibleShift) {
-                val overlap = countOverlap(moreContent, rowShift, columnShift, coordinatesOfOnes)
-                largestOverlap = maxOf(largestOverlap, overlap)
-
-                val reachLargestPossible = largestOverlap == minNumOnes
-                if (reachLargestPossible) return largestOverlap
+        var maxOverlap = 0
+        for (dr in -(n - 1)..<n) {
+            for (dc in -(n - 1)..<n) {
+                maxOverlap = maxOf(maxOverlap, countOverlap(img1, img2, dr, dc))
             }
         }
 
-        return largestOverlap
+        return maxOverlap
     }
 
-    private fun countOnes(image: Array<IntArray>): Int {
-        var count = 0
-
-        for (row in image.indices) {
-            count += image[row].count { it == 1 }
-        }
-
-        return count
-    }
-
-    private fun findCoordinatesOfOnes(image: Array<IntArray>): List<Coordinates> {
-        val coordinates = mutableListOf<Coordinates>()
-
-        for (row in image.indices) {
-            for (column in image[row].indices) {
-                if (image[row][column] == 1) coordinates.add(Coordinates(row, column))
+    private fun toIntArray(img: Array<IntArray>): IntArray =
+        IntArray(img.size) {
+            val row = img[it]
+            row.foldIndexed(0) { index, acc, bit ->
+                bit shl (row.lastIndex - index) or acc
             }
         }
-
-        return coordinates
-    }
-
-    private data class Coordinates(val row: Int, val column: Int)
 
     private fun countOverlap(
-        imageToShift: Array<IntArray>,
-        rowShift: Int,
-        columnShift: Int,
-        coordinatesOfOnesInBaseImage: List<Coordinates>
+        img1: IntArray,
+        img2: IntArray,
+        dr: Int,
+        dc: Int,
     ): Int {
+        val n = img1.size
+        val r1Range = if (dr < 0) -dr..<n else 0..<(n - dr)
         var overlap = 0
-
-        for (coordinate in coordinatesOfOnesInBaseImage) {
-            val isOverlap = with(coordinate) {
-                1 == imageToShift
-                    .getOrNull(row - rowShift)
-                    ?.getOrNull(column - columnShift)
-            }
-            if (isOverlap) overlap++
+        for (r1 in r1Range) {
+            val newRow1 = if (dc < 0) img1[r1] shl -dc else img1[r1] ushr dc
+            val row2 = img2[r1 + dr]
+            overlap += (newRow1 and row2).countOneBits()
         }
-
         return overlap
     }
 }
