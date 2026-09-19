2551e5d8b0af36c681815f523cb55e9ffa4bfd95
hj-core <88603338+hj-core@users.noreply.github.com>
2026-09-19 19:06:17 +0800

    Update problem 1401: Improve readability (#1132)

--- src/main/kotlin/com/hj/leetcode/kotlin/problem1401/Solution.kt
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem1401/Solution.kt
@@ -4,26 +4,19 @@ package com.hj.leetcode.kotlin.problem1401
  * LeetCode page: [1401. Circle and Rectangle Overlapping](https://leetcode.com/problems/circle-and-rectangle-overlapping/);
  */
 class Solution {
-    /* Complexity:
-     * Time O(1) and Space O(1);
-     */
-    fun checkOverlap(radius: Int, xCenter: Int, yCenter: Int, x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-        val rangeOfX = x1..x2
-        val xClosestToCenter = rangeOfX.findClosest(xCenter)
-
-        val rangeOfY = y1..y2
-        val yClosestToCenter = rangeOfY.findClosest(yCenter)
-
-        return square(xClosestToCenter - xCenter) + square(yClosestToCenter - yCenter) <= square(radius)
+    // Complexity:
+    // Time O(1) and Space O(1).
+    fun checkOverlap(
+        radius: Int,
+        xCenter: Int,
+        yCenter: Int,
+        x1: Int,
+        y1: Int,
+        x2: Int,
+        y2: Int,
+    ): Boolean {
+        val dx = xCenter.coerceIn(x1, x2) - xCenter
+        val dy = yCenter.coerceIn(y1, y2) - yCenter
+        return dx * dx <= radius * radius - dy * dy
     }
-
-    private fun IntRange.findClosest(target: Int): Int {
-        return when {
-            target < first -> first
-            target > last -> last
-            else -> target
-        }
-    }
-
-    private fun square(i: Int) = i * i
 }
