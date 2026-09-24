03ab1baf74bba3026279e632e7bf482e84fa814e
hj-core <hj.forbetterfuture@gmail.com>
2026-09-24 09:50:48 +0800

    Add a solution to problem 3550

---
+++ src/main/kotlin/com/hj/leetcode/kotlin/problem3550/Solution.kt
@@ -0,0 +1,30 @@
+package com.hj.leetcode.kotlin.problem3550
+
+/**
+ * LeetCode page: [3550. Smallest Index With Digit Sum Equal to Index](https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/);
+ */
+class Solution {
+    private val digitSum = precomputeDigitSum()
+
+    private fun precomputeDigitSum(): IntArray {
+        val digitSum = IntArray(1001) // the maximum num is 1000
+        var headUnit = 1
+        while (headUnit < 1000) {
+            for (headDigit in 1..<10) {
+                val head = headDigit * headUnit
+                for (tail in 0..<headUnit) {
+                    digitSum[head + tail] = headDigit + digitSum[tail]
+                }
+            }
+            headUnit *= 10
+        }
+        digitSum[1000] = 1
+        return digitSum
+    }
+
+    fun smallestIndex(nums: IntArray): Int {
+        val maxDigitSum = 27
+        val maxIndex = minOf(nums.size - 1, maxDigitSum)
+        return (0..maxIndex).firstOrNull { it == digitSum[nums[it]] } ?: -1
+    }
+}
